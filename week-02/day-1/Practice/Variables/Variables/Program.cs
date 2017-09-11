0a6cb15b6d7b3b6f36545332e8719d0f6c07c598
Lorant Lajos <trojanosall@gmail.com>
2017-09-11 14:36:18 +0200

    Practise - Variables

---
+++ week-02/day-1/Practice/Variables/Variables/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Variables
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // string
+            string greeting = "Hello my friend";
+            Console.WriteLine(greeting);
+
+            // boolean
+            bool running = true;
+            Console.WriteLine(running);
+
+            // whole number
+            int a = 11;
+            Console.WriteLine(a);
+
+            // floating point number
+            double pi = 3.141592;
+            Console.WriteLine(pi);
+
+            // declaring a variable
+            int number;
+
+            // assigning a variable, (define its value)
+            number = 12;
+
+            // mutate a variable, (redefine its value)
+            number = 23;
+
+            Console.ReadLine();
+        }
+    }
+}
