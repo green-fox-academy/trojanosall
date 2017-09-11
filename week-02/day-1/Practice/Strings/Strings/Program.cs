cf9a6022dbaeb4b7e30b364c381667d888b4b661
Lorant Lajos <trojanosall@gmail.com>
2017-09-11 13:57:27 +0200

    Practise - Strings

---
+++ week-02/day-1/Practice/Strings/Strings/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Strings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // string and special characters
+            Console.WriteLine("hello");
+            Console.WriteLine("don't");
+            Console.WriteLine("She said, \"you deserva a treat\" ");
+
+            // string Operators
+            // Concatination
+            Console.WriteLine("hand" + "some");
+
+            // concat string with number
+            Console.WriteLine("My favourite number is: " + 8);
+
+            Console.ReadLine();
+        }
+    }
+}
