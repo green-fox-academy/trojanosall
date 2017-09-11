541af22108fa115989fae7e9fff8fc4ef6099254
Lorant Lajos <trojanosall@gmail.com>
2017-09-11 17:40:55 +0200

    Exercise 15 - MileToKmConverter

---
+++ week-02/day-1/exercise-15-MileToKmConverter/MileToKmConverter/MileToKmConverter/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MileToKmConverter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Write a program that asks for an integer that is a distance in kilometers,
+            // then it converts that value to miles and prints it
+
+            Console.WriteLine("Distance in kilometer: ");
+            int userInput = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Your distance in miles: " + userInput * 1.60934);
+
+            Console.ReadLine();
+
+        }
+    }
+}
