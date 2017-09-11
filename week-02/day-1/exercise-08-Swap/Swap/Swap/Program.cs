6c6f6b246b84e3120b48a67a08f3570009bd9a17
Lorant Lajos <trojanosall@gmail.com>
2017-09-11 15:15:59 +0200

    Exercise 08 - Swap

---
+++ week-02/day-1/exercise-08-Swap/Swap/Swap/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Swap
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Swap the values of the wariables
+            int a = 123;
+            int b = 526;
+            int temp;
+            temp = a;
+            a = b;
+            b = temp;
+            Console.WriteLine(a);
+            Console.WriteLine(b);
+
+            Console.ReadLine();
+        }
+    }
+}
