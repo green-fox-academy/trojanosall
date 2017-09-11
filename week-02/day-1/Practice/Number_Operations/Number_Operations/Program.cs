e95e83bfa3ebf7551ca84a90d1ebce0b61b6da37
Lorant Lajos <trojanosall@gmail.com>
2017-09-11 13:50:56 +0200

    Number_Operations

---
+++ week-02/day-1/Practice/Number_Operations/Number_Operations/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Number_Operations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(2 + 2);
+            Console.WriteLine(7 - 2);
+            Console.WriteLine(3 * 2);
+            Console.WriteLine(5 / 2);
+            Console.WriteLine(7 % 2);
+            Console.WriteLine(7 / 2.0);
+
+            Console.ReadLine();
+        }
+    }
+}
