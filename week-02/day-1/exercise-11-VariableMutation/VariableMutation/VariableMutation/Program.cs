6b03962c6ffef688f57b70acb251ed842f5b1742
Lorant Lajos <trojanosall@gmail.com>
2017-09-11 16:53:58 +0200

    Exercise 11 - VariableMutation

---
+++ week-02/day-1/exercise-11-VariableMutation/VariableMutation/VariableMutation/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VariableMutation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int a = 3;
+            // make it bigger by 10
+            a += 10;
+            Console.WriteLine(a);
+
+            int b = 100;
+            // make it smaller by 7
+            b -= 7;
+            Console.WriteLine(b);
+
+            int c = 44;
+            // please double c's value
+            c = c * 2;
+            Console.WriteLine(c);
+
+            int d = 125;
+            // please divide by 5 d's value
+            d = d / 5;
+            Console.WriteLine(d);
+
+            int e = 8;
+            // please cube of e's value
+            e *= e * e;     
+            Console.WriteLine(e);
+
+            int f1 = 123;
+            int f2 = 345;
+            // tell if f1 is bigger than f2 (print as a boolean)
+            Console.WriteLine("f1 > f2: " + (f1 >f2));
+
+            int g1 = 350;
+            int g2 = 200;
+            // tell if the double of g2 is bigger than g1 (print as a boolean)
+            Console.WriteLine("g2 * 2 > g1: " + (g2 * 2 > g1));
+         
+            long h = 1357988018575474;
+            // tell if it has 11 as a divisor (print as a boolean)
+            Console.WriteLine("11 as a divisor: " + ((h % 11 == 0)));
+     
+            int i1 = 10;
+            int i2 = 3;
+            // tell if i1 is higher than i2 squared and smaller than i2 cubed (print as a boolean)
+            Console.WriteLine("i1 is higher than i2 squared and smaller than i2 cubed: " + ((i1 > i2 * i2) && (i1 < i2 * i2 * i2)));
+            
+      
+            int j = 1521;
+            // tell if j is dividable by 3 or 5 (print as a boolean)
+            Console.WriteLine("dividable by 3 or 5: " + ((j % 3 == 0) || (j % 5 == 0)));
+   
+            string k = "Apple";
+            //fill the k variable with its cotnent 4 times
+            k = k + k + k + k;
+            Console.WriteLine(k);
+
+            Console.ReadKey();
+        }
+    }
+}
