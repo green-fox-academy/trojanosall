534bf264acfa661b5b2a00d772a30846864c0fad
Lorant Lajos <trojanosall@gmail.com>
2017-09-11 14:56:47 +0200

    Exercise 07 - FavoriteNumber

---
+++ week-02/day-1/exercise-07-FavoriteNumbers/FavoriteNumbers/FavoriteNumbers/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FavoriteNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Store your favorite number in a variable (as a number)
+            // And print it like this: "My favorite number is: 8"
+
+            int a = 30;
+            Console.WriteLine("My favorite number is:" + a );
+
+            Console.ReadLine();
+        }
+    }
+}
