de97313b45ca0f0c201982e38417f95ab5b9ed7c
Lorant Lajos <trojanosall@gmail.com>
2017-09-11 13:42:05 +0200

    Practise exercise - Type + and other not added files

---
+++ week-02/day-1/Practice/Types/Types/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Types
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            Console.WriteLine(42);
+
+            Console.Write(3.1415);
+
+            Console.Read();
+        }
+    }
+}
