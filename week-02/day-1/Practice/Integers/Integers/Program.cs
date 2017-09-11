1ecdc9aec305bdfa720eec89615b775bb9b7ecc7
Lorant Lajos <trojanosall@gmail.com>
2017-09-11 13:45:48 +0200

    Practise exercise - Type

---
+++ week-02/day-1/Practice/Integers/Integers/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Integers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            Console.WriteLine(42);
+
+            Console.Write(3.1415);
+
+            // maximum and minimum value
+            // a simple integer is represented in 4 bytes (32bit)
+
+            // maximum value:  2 147 483 647
+            Console.WriteLine(2147483647);
+
+            // minimum value: -2 147 483 648
+            Console.WriteLine(-2147483648);
+
+            Console.ReadLine();
+        }
+    }
+}
