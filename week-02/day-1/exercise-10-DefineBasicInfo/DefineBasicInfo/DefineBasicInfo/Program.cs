bde6a74615ead624af6a07bb4350981e35d7fd91
Lorant Lajos <trojanosall@gmail.com>
2017-09-11 15:36:50 +0200

    Exercise 10 - DefineBasicInfo

---
+++ week-02/day-1/exercise-10-DefineBasicInfo/DefineBasicInfo/DefineBasicInfo/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DefineBasicInfo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Define several things as a variable than print their values
+            // Your name as a string
+            // Your age as an integer
+            // Your height in meters as a double
+            // Wether you are married or not as a boolean
+
+            string name = "Lorant Lajos";
+            int age = 34;
+            double height = 1.78;
+            bool married = true;
+
+            Console.WriteLine(name);
+            Console.WriteLine(age);
+            Console.WriteLine(height);
+            Console.WriteLine(married);
+
+            Console.ReadLine();
+            
+        }
+    }
+}
