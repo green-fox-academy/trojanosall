5823df9dd56d37ca27440a6e2734614e3a0f106f
Lorant Lajos <trojanosall@gmail.com>
2017-09-11 12:47:44 +0200

    Exercise 1 - HelloMe

---
+++ week-02/day-1/exercise-01-HelloMe/HelloMe/HelloMe/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloMe
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Modify this program to greet you instead of the World!
+            Console.WriteLine("Hello Me");
+            Console.ReadLine();
+        }
+    }
+}
