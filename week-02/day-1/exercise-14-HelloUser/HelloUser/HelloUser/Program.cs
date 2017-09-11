09b1052913613e3251a548361e8c7930159ffab1
Lorant Lajos <trojanosall@gmail.com>
2017-09-11 17:21:08 +0200

    Exercise 14 - Hello User

---
+++ week-02/day-1/exercise-14-HelloUser/HelloUser/HelloUser/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloUser
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Modify this program to greet user instead of the World!
+            // The program should ask for the name of the user
+            Console.WriteLine("Hello, User!");
+
+            Console.WriteLine("What is your name?");
+            string name = Console.ReadLine();
+
+            Console.WriteLine("Hello, " + name);
+
+            Console.ReadLine();
+        }
+    }
+}
