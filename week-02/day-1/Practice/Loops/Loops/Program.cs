ff458d3a9be372386b67d067cba6a0f75c3b9945
Lorant Lajos <trojanosall@gmail.com>
2017-09-11 23:56:01 +0200

    Practice - Loops

---
+++ week-02/day-1/Practice/Loops/Loops/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Loops
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("while loop");
+            int a = 0;
+            while (a < 10)
+            {
+                Console.WriteLine(a++);
+            }
+
+            Console.WriteLine("\n\n do while loop:");
+            int b = 0;
+            do
+            {
+                Console.WriteLine(b++);
+            } while (b < 10);
+
+            Console.WriteLine("\n\n for loop:");
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine(i);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
