0b6cad70292bbaf6bb075001c984a4dfa5f92f97
Lorant Lajos <trojanosall@gmail.com>
2017-09-11 17:13:52 +0200

    Practice - User input (scanner)

---
+++ week-02/day-1/Practice/UserInputScanner/UserInputScanner/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UserInputScanner
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello user what is your name?");
+            string name = Console.ReadLine();
+
+            Console.WriteLine("Hello, " + name);
+            Console.WriteLine("How old are you? ");
+
+            string age = Console.ReadLine();
+            Console.WriteLine(age + " is a nice age");
+
+            Console.ReadLine();
+        }
+    }
+}
