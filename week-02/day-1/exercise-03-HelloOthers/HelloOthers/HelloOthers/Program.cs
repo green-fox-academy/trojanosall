1920fe241c1fe073a13b08b588ca9dc8f7f05714
Lorant Lajos <trojanosall@gmail.com>
2017-09-11 13:33:06 +0200

    Exercise 3 - Hello Others

---
+++ week-02/day-1/exercise-03-HelloOthers/HelloOthers/HelloOthers/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloOthers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Greet 3 of your classmates with this program, in three separate lines
+            // like:
+            //
+            // Hello, Esther!
+            // Hello, Mary!
+            // Hello, Joe!
+
+            Console.WriteLine("Helloooo Lori");
+            Console.WriteLine("Heloooooo Arnold");
+            Console.WriteLine("Helooooooo Gyuszi");
+            Console.ReadLine();
+        }
+    }
+}
