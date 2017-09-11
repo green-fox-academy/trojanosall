f84282ff0f1e1ed28d2ca03786a8dce0bf911cb9
Lorant Lajos <trojanosall@gmail.com>
2017-09-11 14:04:00 +0200

    Exercise - 04 - Introduce Yourself

---
+++ week-02/day-1/exercise-04-IntroduceYourself/IntroduceYourself/IntroduceYourself/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IntroduceYourself
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Write a program that prints a few details to the terminal window about you
+            // It should print each detail to a new line.
+            //  - Your name
+            //  - Your age
+            //  - Your height in meters (as a decimal fraction)
+            //
+            //  Example output:
+            //  John Doe
+            //  31
+            //  1.87
+
+            Console.WriteLine("Lajos Lorant");
+            Console.WriteLine("34");
+            Console.WriteLine("1.71");
+
+
+            Console.ReadLine();
+        }
+    }
+}
