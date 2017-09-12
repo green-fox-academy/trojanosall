b965d034af06ffa9d72bf2d943db9ed271e6f133
Lorant Lajos <trojanosall@gmail.com>
2017-09-12 21:43:17 +0200

    Starting of Exercise 03 - AppendA

---
+++ week-02/day-2/exercise-03-AppendA/append-a/append-a/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace append_a
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // - Create a string variable named `am` and assign the value `kuty` to it
+            // - Write a function called `appendA` that gets a string as an input
+            //   and appends an 'a' character to its end
+            // - Print the result of `appendA(am)`
+            Console.ReadLine();
+        }
+    }
+}
