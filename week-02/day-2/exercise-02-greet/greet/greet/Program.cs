11041aeb892475b44bd755b50d2fb3be06ebaca7
Lorant Lajos <trojanosall@gmail.com>
2017-09-12 17:52:41 +0200

    Exercise 02 - Greet

---
+++ week-02/day-2/exercise-02-greet/greet/greet/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace greet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // - Create a string variable named `al` and assign the value `Greenfox` to it
+            // - Create a function called `greet` that greets it's input parameter
+            //     - Greeting is printing e.g. `Greetings dear, Greenfox`
+            // - Greet `al`
+
+            string al = "Greenfox";
+            Console.WriteLine(Greet(al));
+            Console.ReadKey();
+        }
+
+        public static string Greet(string al)
+
+        {
+            return al = "Greetings dear, " + al;
+
+        }
+    }
+}
