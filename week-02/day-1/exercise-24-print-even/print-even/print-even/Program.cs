5b2e88fd621da0525fcf6b3cf10defb8152001e7
Lorant Lajos <trojanosall@gmail.com>
2017-09-12 14:05:42 +0200

    Exercise 24 - print-even

---
+++ week-02/day-1/exercise-24-print-even/print-even/print-even/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace print_even
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Create a program that prints all the even numbers between 0 and 500
+            
+            for (int i = 0; i < 500; i += 2)
+            {
+                Console.WriteLine(i);
+            }
+            Console.ReadKey();
+
+        }
+    }
+}
