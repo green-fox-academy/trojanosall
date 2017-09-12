0fcc86f2d90100d7f1a42fb9147fe7c01c20b1b4
Lorant Lajos <trojanosall@gmail.com>
2017-09-12 16:37:42 +0200

    Prepare Practice of Funcion

---
+++ week-02/day-2/Practice/Functions/Functions/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Functions
+{
+    class Program
+    {
+        public static void GreetingFunction()
+        {
+            Console.WriteLine("hello");
+        }
+
+        static void Main(string[] args)
+        {
+            GreetingFunction();
+            Console.ReadLine();
+        }
+    }
+}
---
+++ week-02/day-2/exercise-01-Doubling/Doubling/Doubling/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Doubling
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // - Create an integer variable named `ak` and assign the value `123` to it
+            // - Create a function called `doubling` that doubles it's input parameter
+            // - Print the result of `doubling(ak)`
+
+            int ak = 123;
+
+
+            Console.ReadKey();
+        }
+    }
+}
