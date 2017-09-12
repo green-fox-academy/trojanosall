c84fc11b67979ca66d2b2eab49a3d102b57f3bde
Lorant Lajos <trojanosall@gmail.com>
2017-09-13 00:40:49 +0200

    Starting of Exercise 06 - Print Argument

---
+++ week-02/day-2/exercise-06-PrintArgument/PrintArgument/PrintArgument/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrintArgument
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // - Create a function called `printer`
+            //   which prints the input String parameters
+            // - It can have any number of parameters
+
+            // Examples
+            // printer("first")
+            // printer("first", "second")
+            // printer("first", "second", "third", "fourh")
+            // ...
+            Console.ReadLine();
+
+        }
+    }
+}
