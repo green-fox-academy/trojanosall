994007cd5f4dac9b9850977c6c2c5a05cd9c15a2
Lorant Lajos <trojanosall@gmail.com>
2017-09-12 09:33:08 +0200

    Exercise 18 - Odd or Even

---
+++ week-02/day-1/exercise-18-OddEven/OddEven/OddEven/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OddEven
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Write a program that reads a number form the standard input,
+            // Than prints "Odd" if the number is odd, or "Even" it it is even.
+
+            Console.WriteLine("If you give me a number I tell you that this number is ODD or EVEN. Enter your number: ");
+            string userInput = Console.ReadLine();
+            int userNum = Int32.Parse(userInput);
+
+            if ((userNum & 1) == 0)
+                Console.WriteLine("EVEN!\n");
+            else
+                Console.WriteLine("ODD!\n");
+
+            Console.ReadKey();
+        }
+    }
+}
