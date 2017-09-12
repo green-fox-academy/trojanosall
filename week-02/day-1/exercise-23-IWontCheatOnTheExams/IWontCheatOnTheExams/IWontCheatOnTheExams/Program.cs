aaddb7f82121079f35188f6dc774e1186783cea4
Lorant Lajos <trojanosall@gmail.com>
2017-09-12 13:43:07 +0200

    Exercise 23 - IWontCheatOnTheExams

---
+++ week-02/day-1/exercise-23-IWontCheatOnTheExams/IWontCheatOnTheExams/IWontCheatOnTheExams/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IWontCheatOnTheExams
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Create a program that writes this line 100 times:
+            // "I won't cheat on the exam!"
+
+            string sorry = "I won't cheat on the exame";
+
+            for (int i = 0; i < 100; i++)
+            {
+                Console.WriteLine(sorry);
+            }
+
+            Console.ReadKey();
+
+        }
+    }
+}
