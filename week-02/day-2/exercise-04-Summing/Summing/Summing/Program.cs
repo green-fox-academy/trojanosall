2f095bebaff387f0049066f4641ae71e4c5e9593
Lorant Lajos <trojanosall@gmail.com>
2017-09-13 00:02:38 +0200

    Exercise 04 - Summing

---
+++ week-02/day-2/exercise-04-Summing/Summing/Summing/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Summing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Please give me a number (INTEGER): ");
+            int UserNumber = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Sum is " + Sum(UserNumber));
+            Console.ReadKey();
+        }
+
+        public static int Sum(int number)
+        {
+            int result = 0;
+            for (int i = number; i > 0; i--)
+            {
+                result += i;
+            }
+            return result;
+        }
+    }
+}
