f66277b7d6ca9c3768bd9c37da080a31ca334b18
Lorant Lajos <trojanosall@gmail.com>
2017-09-13 00:46:14 +0200

    Exercise 07 - Third

---
+++ week-02/day-2/exercise-07-Third/Third/Third/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Third
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // - Create an array variable named `q`
+            //   with the following content: `[4, 5, 6, 7]`
+            // - Print the third element of `q`
+
+            int[] q = { 4, 5, 6, 7 };
+            Console.WriteLine(q[2]);
+            Console.ReadKey();
+            
+        }
+    }
+}
