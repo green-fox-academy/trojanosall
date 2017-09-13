88f2f9d318f0665b1261914ac88052b89df8f4a8
Lorant Lajos <trojanosall@gmail.com>
2017-09-14 00:17:23 +0200

    Day01 - Excercise 20 - Print Bigger - Firsr Step

---
+++ week-02/day-1/exercise-20-PrintBigger/PrintBigger/PrintBigger/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrintBigger
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Write a program that asks for two numbers and prints the bigger one
+        }
+    }
+}
