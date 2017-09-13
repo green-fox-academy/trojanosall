09050ce6e49e165582564626dbb15159fbc4ca9e
Lorant Lajos <trojanosall@gmail.com>
2017-09-13 15:00:18 +0200

    Done Exercise 2 - URL Fixer

---
+++ week-02/day-3/exercise-02-strings/Ex-02-strings-UrlFixer/Ex-02-strings-simplereplace/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex_02_strings_simplereplace
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string url = "https//www.reddit.com/r/nevertellmethebots";
+
+            // Accidentally I got the wrong URL for a funny subreddit. It's probably "odds" and not "bots"
+            // Also, the URL is missing a crutial component, find out what it is and insert it too!
+            // Try to solve it more than once using different string functions!
+
+            string CorrectedURL = url.Replace("s/", "s:/").Replace("bots", "odds");
+
+            Console.WriteLine(CorrectedURL);
+
+            Console.ReadKey();
+        }
+    }
+}
