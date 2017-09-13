039769a8ce613d66394cc255d5b87b753a49cf68
Lorant Lajos <trojanosall@gmail.com>
2017-09-13 13:56:47 +0200

    Done Exercise 14 - Double Items

--- week-02/day-2/exercise-14-DoubleItems/Double items/Double items/Program.cs
+++ week-02/day-2/exercise-14-DoubleItems/Double items/Double items/Program.cs
@@ -13,6 +13,12 @@ namespace Double_items
             // - Create an array variable named `ag`
             //   with the following content: `[3, 4, 5, 6, 7]`
             // - Double all the values in the array
+            int[] ag = { 3, 4, 5, 6, 7 };
+            for (int i = 0; i < 5; i++)
+            {
+                ag[i] = ag[i] * 2;
+                Console.WriteLine(ag[i]);
+            }
             Console.ReadKey();
         }
     }
