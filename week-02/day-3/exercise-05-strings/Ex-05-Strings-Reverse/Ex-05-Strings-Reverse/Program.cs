a112d809aa5d1aebc5ef2e7c26bff13b12906233
Lorant Lajos <trojanosall@gmail.com>
2017-09-13 16:35:13 +0200

    Starting of Exercise 05 - String

---
+++ week-02/day-3/exercise-05-strings/Ex-05-Strings-Reverse/Ex-05-Strings-Reverse/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex_05_Strings_Reverse
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string reversed = ".eslaf eb t'ndluow ecnetnes siht ,dehctiws erew eslaf dna eurt fo sgninaem eht fI";
+
+            // Create a function that can reverse a String, which is passed as the parameter
+            // Use it on this reversed string to check it!
+
+            Console.WriteLine(reversed);
+
+            Console.ReadKey();
+        }
+    }
+}
