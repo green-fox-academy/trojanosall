43b8ad54a2713711fc540393687f7abbe5795977
Lorant Lajos <trojanosall@gmail.com>
2017-09-13 13:01:39 +0200

    Done Exercise 12 - Print All

--- week-02/day-2/exercise-12-PrintElements/PrintElements/PrintElements/Program.cs
+++ week-02/day-2/exercise-12-PrintElements/PrintElements/PrintElements/Program.cs
@@ -13,6 +13,12 @@ namespace PrintElements
             // - Create an array variable named `af`
             //   with the following content: `[4, 5, 6, 7]`
             // - Print all the elements of `af`
+
+            int[] af = { 4, 5, 6, 7 };
+            for (int i = 0; i < af.Length; i++)
+            {
+                Console.WriteLine(af[i]);
+            }
             Console.ReadKey();
         }
     }
