29a8516240cbd145f3f412bbe7938af0591686bd
Lorant Lajos <trojanosall@gmail.com>
2017-09-13 12:58:20 +0200

    Starting Exercise 12 - Print All

---
+++ week-02/day-2/exercise-12-PrintElements/PrintElements/PrintElements/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrintElements
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // - Create an array variable named `af`
+            //   with the following content: `[4, 5, 6, 7]`
+            // - Print all the elements of `af`
+            Console.ReadKey();
+        }
+    }
+}
