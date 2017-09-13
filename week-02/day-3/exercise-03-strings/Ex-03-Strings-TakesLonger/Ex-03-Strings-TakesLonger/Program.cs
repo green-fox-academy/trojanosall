0649c8d5052082343888e6aef2d745f789123bf0
Lorant Lajos <trojanosall@gmail.com>
2017-09-13 15:03:42 +0200

    Starting of Exercise 03 - String - TakesLonger

---
+++ week-02/day-3/exercise-03-strings/Ex-03-Strings-TakesLonger/Ex-03-Strings-TakesLonger/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex_03_Strings_TakesLonger
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string quote = "Hofstadter's Law: It you expect, even when you take into account Hofstadter's Law.";
+
+            // When saving this quote a disk error has occured. Please fix it.
+            // Add "always takes longer than" to the StringBuilder (quote) between the words "It" and "you"
+            // Using pieces of the quote variable (instead of just redefining the string)
+
+            Console.WriteLine(quote;
+        }
+    }
+}
