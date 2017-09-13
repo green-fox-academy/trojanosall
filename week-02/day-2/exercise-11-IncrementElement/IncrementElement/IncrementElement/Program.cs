6e4b5214d9d65d34bd2ef52cbb0adb3a74af72e4
Lorant Lajos <trojanosall@gmail.com>
2017-09-13 12:55:06 +0200

    Done of Exercise 11 - IncrementElement

--- week-02/day-2/exercise-11-IncrementElement/IncrementElement/IncrementElement/Program.cs
+++ week-02/day-2/exercise-11-IncrementElement/IncrementElement/IncrementElement/Program.cs
@@ -14,7 +14,10 @@ namespace IncrementElement
             //  with the following content: `[1, 2, 3, 4, 5]`
             //- Increment the third element
             //- Print the third element
-            Console.ReadLine();
+            int[] t = { 1, 2, 3, 4, 5 };
+            t[2] += 1;
+            Console.WriteLine(t[2]);
+            Console.ReadKey();
         }
     }
 }
