1f5aef60a1abb62da97784271a9d447ba03f7783
Lorant Lajos <trojanosall@gmail.com>
2017-09-13 12:51:11 +0200

    Starting of Exercise 11 - IncrementElement

---
+++ week-02/day-2/exercise-11-IncrementElement/IncrementElement/IncrementElement/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IncrementElement
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //- Create an array variable named `t`
+            //  with the following content: `[1, 2, 3, 4, 5]`
+            //- Increment the third element
+            //- Print the third element
+            Console.ReadLine();
+        }
+    }
+}
