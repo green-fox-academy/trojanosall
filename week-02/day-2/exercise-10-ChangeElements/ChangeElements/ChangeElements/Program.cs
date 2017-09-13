84d3c6dd8a64910765a13cc10961d79634a38296
Lorant Lajos <trojanosall@gmail.com>
2017-09-13 12:48:53 +0200

    Done - Exercis 10 - Change Element

--- week-02/day-2/exercise-10-ChangeElements/ChangeElements/ChangeElements/Program.cs
+++ week-02/day-2/exercise-10-ChangeElements/ChangeElements/ChangeElements/Program.cs
@@ -14,6 +14,9 @@ namespace ChangeElements
             //   with the following content: `[1, 2, 3, 8, 5, 6]`
             // - Change the 8 to 4
             // - Print the fourth element
+            int[] s = { 1, 2, 3, 8, 5, 6 };
+            s[3] = 4;
+            Console.WriteLine(s[3]);
             Console.ReadKey();
         }
     }
