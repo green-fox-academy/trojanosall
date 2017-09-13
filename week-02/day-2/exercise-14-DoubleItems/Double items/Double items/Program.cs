8f5af20f90605269dbb1f11378ee8c9c990edc52
Lorant Lajos <trojanosall@gmail.com>
2017-09-13 14:00:00 +0200

    Better Solution for Exercise 14 - Double Items

--- week-02/day-2/exercise-14-DoubleItems/Double items/Double items/Program.cs
+++ week-02/day-2/exercise-14-DoubleItems/Double items/Double items/Program.cs
@@ -13,13 +13,24 @@ namespace Double_items
             // - Create an array variable named `ag`
             //   with the following content: `[3, 4, 5, 6, 7]`
             // - Double all the values in the array
+
+            //BETTER SOLUTION
             int[] ag = { 3, 4, 5, 6, 7 };
-            for (int i = 0; i < 5; i++)
+            for (int i = 0; i < ag.Length; i++)
             {
-                ag[i] = ag[i] * 2;
-                Console.WriteLine(ag[i]);
+                Console.WriteLine(ag[i]*2);
             }
             Console.ReadKey();
+
+
+            // FIRST SOLUTION
+            //int[] ag = { 3, 4, 5, 6, 7 };
+            //for (int i = 0; i < 5; i++)
+            //{
+            //    ag[i] = ag[i] * 2;
+            //   Console.WriteLine(ag[i]);
+            //}
+            //Console.ReadKey();
         }
     }
 }
