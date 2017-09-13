5f2bdf20e82d990e0dd4eb64e86ca5f134467fa3
Lorant Lajos <trojanosall@gmail.com>
2017-09-14 00:38:32 +0200

    Day01 - Excercise 20 - Print Bigger - Firsr Step"

--- week-02/day-1/exercise-20-PrintBigger/PrintBigger/PrintBigger/Program.cs
+++ week-02/day-1/exercise-20-PrintBigger/PrintBigger/PrintBigger/Program.cs
@@ -11,6 +11,36 @@ namespace PrintBigger
         static void Main(string[] args)
         {
             // Write a program that asks for two numbers and prints the bigger one
+
+            //LONGER DECLARATION
+            // Console.WriteLine("Please give me a number (INTEGER): ");
+            // string UserInput1 = Console.ReadLine();
+            // int UserNum1 = Int32.Parse(UserInput1);
+
+            //Console.WriteLine("Please give me another number (INTEGER): ");
+            //string UserInput2 = Console.ReadLine();
+            //int UserNum2 = Int32.Parse(UserInput2);
+
+            //SHORTER METHOD
+            Console.WriteLine("Please give me a number(INTEGER): ");
+            int UserNum1 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Please give me another number (INTEGER): ");
+            int UserNum2 = int.Parse(Console.ReadLine());
+
+            if (UserNum2 > UserNum1)
+            {
+                Console.WriteLine("The bigger one is " + UserNum2);
+            }
+            else if (UserNum2 == UserNum1)
+            {
+                Console.WriteLine("The numbers are equals");
+            }
+            else
+            {
+                Console.WriteLine("The bigger one is " + UserNum1);
+            }
+            Console.ReadKey();
         }
     }
 }
