b43b62f633141aa40c38bc7f4c4afe163908adf9
Lorant Lajos <trojanosall@gmail.com>
2017-09-13 11:44:49 +0200

    Practice - Arrays

---
+++ week-02/day-2/Practice/Arrays/Arrays/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Arrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = { 1, 2, 3, 8 };
+
+            // Print the second element of the array
+            Console.WriteLine(array[1]);
+
+            // Print all the array elements
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.WriteLine(array[i]);
+            }
+            Console.ReadLine();
+        }
+    }
+}
