cf9cbf9127f2bb92519ed2504f43b971ba57841d
Lorant Lajos <trojanosall@gmail.com>
2017-09-13 14:04:43 +0200

    Starting excercise 01 - Strings

---
+++ week-02/day-3/exercise-01-strings/Ex-01-strings/Ex-01-strings/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex_01_strings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            example.replace("dishwasher", "galaxy");
+
+            // I would like to replace "dishwasher" with "galaxy" in this example, but it has a problem.
+            // Please fix it for me!
+            // Expected ouput: In a galaxy far far away
+
+            Console.WriteLine(example);
+        }
+    }
+}
