f1a817a139ce93a42ab8c05fcd2a7e3d055b29f0
Lorant Lajos <trojanosall@gmail.com>
2017-09-13 15:58:14 +0200

    Starting of Exercise 4 - String -ToDoPrint

---
+++ week-02/day-3/exercise-04-strings/Ex-04-String-ToDoPrint/Ex-04-String-ToDoPrint/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex_04_String_ToDoPrint
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string todoText = " - Buy milk\n";
+            // Add "My todo:" to the beginning of the todoText
+            // Add " - Download games" to the end of the todoText
+            // Add " - Diablo" to the end of the todoText but with indention
+
+            // Expected output:
+
+            // My todo:
+            //  - Buy milk
+            //  - Download games
+            //      - Diablo
+
+            Console.WriteLine(todoText);
+        }
+    }
+}
