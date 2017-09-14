a9f7d0738a49199550b02de4d52d436b80b827e9
Lorant Lajos <trojanosall@gmail.com>
2017-09-14 13:52:58 +0200

    Week-02 - Day-01 - Exercise-27 - Done

--- week-02/day-1/exercise-27-FizzBuzz/FizzBuzz/FizzBuzz/Program.cs
+++ week-02/day-1/exercise-27-FizzBuzz/FizzBuzz/FizzBuzz/Program.cs
@@ -17,8 +17,22 @@ namespace FizzBuzz
 
             for (int i = 1; i < 101; i++)
             {
-                Console.WriteLine(i);
-
+                if (i % 3 == 0 && i % 5 == 0)
+                {
+                    Console.WriteLine("FizzBuzz");
+                }
+                else if (i % 3 == 0)
+                {
+                    Console.WriteLine("Fizz");
+                }
+                else if (i % 5 == 0)
+                {
+                    Console.WriteLine("Buzz");
+                }
+                else
+                {
+                    Console.WriteLine(i);
+                }
             }
 
             Console.ReadKey();
