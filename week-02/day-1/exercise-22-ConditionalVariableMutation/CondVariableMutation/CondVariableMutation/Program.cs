19ffbbdd1e2c5f1414619a360a4360eadd4dc726
Lorant Lajos <trojanosall@gmail.com>
2017-09-14 10:59:41 +0200

    Week-02 - Day-01 - Exercise-22 ConditionalVariableMutation - 2nd task

--- week-02/day-1/exercise-22-ConditionalVariableMutation/CondVariableMutation/CondVariableMutation/Program.cs
+++ week-02/day-1/exercise-22-ConditionalVariableMutation/CondVariableMutation/CondVariableMutation/Program.cs
@@ -19,6 +19,8 @@ namespace CondVariableMutation
                 Console.WriteLine(out1+=1);
             }
 
+
+
             // if b is between 10 and 20 set out2 to "Sweet!"
             // if less than 10 set out2 to "More!",
             // if more than 20 set out2 to "Less!"
@@ -26,29 +28,44 @@ namespace CondVariableMutation
             int b = 13;
             string out2 = "";
 
-
+            if (10 < b && b < 20)
+            {
+                out2 = "Sweet";
+            }
+            else if (b < 10)
+            {
+                out2 = "More";
+            }
+            else 
+            {
+                out2 = "Less";
+            }
             Console.WriteLine(out2);
 
-            int c = 123;
-            int credits = 100;
-            bool isBonus = false;
+
+
             // if credits are at least 50,
             // and isBonus is false decrement c by 2
             // if credits are smaller than 50,
             // and isBonus is false decrement c by 1
             // if isBonus is true c should remain the same
+            int c = 123;
+            int credits = 100;
+            bool isBonus = false;
+            
 
             Console.WriteLine(c);
 
-            int d = 8;
-            int time = 120;
-            string out3 = "";
             // if d is dividable by 4
             // and time is not more than 200
             // set out3 to "check"
             // if time is more than 200
             // set out3 to "Time out"
             // otherwise set out3 to "Run Forest Run!"
+            int d = 8;
+            int time = 120;
+            string out3 = "";
+
 
             Console.WriteLine(out3);
 
