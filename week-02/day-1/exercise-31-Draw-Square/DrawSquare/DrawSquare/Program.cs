bda2f00ac4b7b6198dbe6eb11d37ede7baa9a9d8
Lorant Lajos <trojanosall@gmail.com>
2017-09-14 18:01:12 +0200

    Week-01 Day-01 Exercise 31 - Draw Square

---
+++ week-02/day-1/exercise-31-Draw-Square/DrawSquare/DrawSquare/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DrawSquare
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Height of your square: ");
+            int height = int.Parse(Console.ReadLine());
+
+            Console.WriteLine();
+            Console.WriteLine("Here is your square!");
+
+            for (int j = 0; j < height; j++)
+            {
+                Console.Write("%");
+            }
+
+            Console.WriteLine();
+
+            for (int k = 0; k < height - 2; k++)
+
+            {
+                Console.Write("%");
+
+            for (int l = 0; l < height - 2; l++)
+            {
+                 Console.Write(" ");
+            }
+
+            Console.Write("%");
+            Console.WriteLine();
+
+            }
+
+            for (int j = 0; j < height; j++)
+
+            {
+                Console.Write("%");
+            }
+
+        Console.WriteLine();
+
+        Console.ReadKey();
+        }
+    }
+}
