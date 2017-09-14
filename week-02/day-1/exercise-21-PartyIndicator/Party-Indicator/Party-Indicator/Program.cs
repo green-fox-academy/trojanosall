fd224f34fb2c615862a8c26f7c9390f1f0915216
Lorant Lajos <trojanosall@gmail.com>
2017-09-14 10:07:41 +0200

    Week-02 - Day-01 - Exercise-21 PartyIndicator - First Steps

---
+++ week-02/day-1/exercise-21-PartyIndicator/Party-Indicator/Party-Indicator/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Party_Indicator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Write a program that asks for two numbers
+            // Thw first number represents the number of girls that comes to a party, the
+            // second the boys
+            // It should print: The party is exellent!
+            // If the the number of girls and boys are equal and there are more people coming than 20
+            //
+            // It should print: Quite cool party!
+            // It there are more than 20 people coming but the girl - boy ratio is not 1-1
+            //
+            // It should print: Average party...
+            // If there are less people coming than 20
+            //
+            // It should print: Sausage party
+            // If no girls are coming, regardless the count of the people
+        }
+    }
+}
