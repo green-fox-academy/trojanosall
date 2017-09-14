d42513c18f3cb3f700a9eb91dfb24bbb32d3b1d6
Lorant Lajos <trojanosall@gmail.com>
2017-09-14 16:53:44 +0200

    Draw Dyamond

---
+++ week-02/day-1/exercise-30-DrawDiamond/DrawDyamiond/DrawDyamiond/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DrawDyamiond
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Write a program that reads a number from the standard input, then draws a
+            // diamond like this:
+            //
+            //
+            //    *
+            //   ***
+            //  *****
+            // *******
+            //  *****
+            //   ***
+            //    *
+            //
+            // The diamond should have as many lines as the number was
+
+        }
+    }
+}
