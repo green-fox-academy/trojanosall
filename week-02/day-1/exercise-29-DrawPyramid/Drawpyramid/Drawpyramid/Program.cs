2205fc313a603d1b6f8bf75a2dd128ac5e2f9e8d
Lorant Lajos <trojanosall@gmail.com>
2017-09-14 14:53:41 +0200

    Week-02 - Day-01 - Exercise-29 - DrawPyramid - First Step

---
+++ week-02/day-1/exercise-29-DrawPyramid/Drawpyramid/Drawpyramid/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Drawpyramid
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // Write a program that reads a number from the standard input, then draws a
+            // pyramid like this:
+            //
+            //
+            //    *
+            //   ***
+            //  *****
+            // *******
+            //
+            // The pyramid should have as many lines as the number was
+
+        }
+    }
+}
