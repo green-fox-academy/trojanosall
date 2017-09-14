b77982a897b6bf77968d167365945e40dd9c679a
Lorant Lajos <trojanosall@gmail.com>
2017-09-14 13:06:26 +0200

    Week-02 - Day-01 - Exercise-26 - Count From to - First Step

---
+++ week-02/day-1/exercise-26-CountFromTo/CountFromTo/CountFromTo/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CountFromTo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Create a program that asks for two numbers
+            // If the second number is not bigger than the first one it should print:
+            // "The second number should be bigger"
+            //
+            // If it is bigger it should count from the first number to the second by one
+            //
+            // example:
+            //
+            // first number: 3, second number: 6, should print:
+            //
+            // 3
+            // 4
+            // 5
+
+
+
+        }
+    }
+}
