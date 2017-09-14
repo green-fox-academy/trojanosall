0a1fc5182c3fb47302d987d7ccf4afb60a327d52
Lorant Lajos <trojanosall@gmail.com>
2017-09-14 17:16:14 +0200

    Week-02 Day-01 Exercise 30 - Draw a dyamond - half dyamond prepoared

--- week-02/day-1/exercise-30-DrawDiamond/DrawDyamiond/DrawDyamiond/Program.cs
+++ week-02/day-1/exercise-30-DrawDiamond/DrawDyamiond/DrawDyamiond/Program.cs
@@ -24,6 +24,23 @@ namespace DrawDyamiond
             //
             // The diamond should have as many lines as the number was
 
+            Console.Write("Please give me a number and based on this number I will build a dyamond for you: ");
+            int UserInput = int.Parse(Console.ReadLine());
+
+
+            for (int i = 0; i <= UserInput; i++)
+            {
+                for (int j = 0; j < UserInput - i; j++)
+                {
+                    Console.Write(" ");
+                }
+                for (int k = 0; k < 2 * i - 1; k++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }
+            Console.ReadKey();
         }
     }
 }
