4e82e1a1367dc8c4595ce0d644801ca2281f3dce
Lorant Lajos <trojanosall@gmail.com>
2017-09-14 10:48:47 +0200

    Week-02 - Day-01 - Exercise-22 ConditionalVariableMutation - 1t task

--- week-02/day-1/exercise-22-ConditionalVariableMutation/CondVariableMutation/CondVariableMutation/Program.cs
+++ week-02/day-1/exercise-22-ConditionalVariableMutation/CondVariableMutation/CondVariableMutation/Program.cs
@@ -10,18 +10,22 @@ namespace CondVariableMutation
     {
         static void Main(string[] args)
         {
-
+            // if a is even increment out by one
             double a = 24;
-            int out = 0;
-            // if w is even increment out by one
+            int out1 = 0;
+                        
+            if (a%2==0)
+            {
+                Console.WriteLine(out1+=1);
+            }
 
-            Console.WriteLine(out);
-
-            int b = 13;
-            string out2 = "";
             // if b is between 10 and 20 set out2 to "Sweet!"
             // if less than 10 set out2 to "More!",
             // if more than 20 set out2 to "Less!"
+
+            int b = 13;
+            string out2 = "";
+
 
             Console.WriteLine(out2);
 
@@ -48,6 +52,7 @@ namespace CondVariableMutation
 
             Console.WriteLine(out3);
 
+            Console.ReadKey();
         }
     }
 }
