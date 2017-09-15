f0122d732a068fbb2b8b7f240799c59e1ebd31c7
Lorant Lajos <trojanosall@gmail.com>
2017-09-15 11:16:27 +0200

    week 02 - Day 05 - Guess My Number - First Step

---
+++ week-02/day-5/GuessMyNumber/GuessMyNumber/GuessMyNumber/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GuessMyNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // GUESS MY NUMBER
+            // Write a program where the program chooses a number between 1 and 100. 
+            // The player is then asked to enter a guess. If the player guesses wrong, then the program gives feedback 
+            // and ask to enter an other guess until the guess is correct.
+
+        }
+    }
+}
