0b1ca4009a433751dd4d91c8be666d054baa7e65
Lorant Lajos <trojanosall@gmail.com>
2017-09-15 11:48:54 +0200

    week 02 - Day 05 - Guess My Number - The initial parameters of the game

--- week-02/day-5/GuessMyNumber/GuessMyNumber/GuessMyNumber/Program.cs
+++ week-02/day-5/GuessMyNumber/GuessMyNumber/GuessMyNumber/Program.cs
@@ -14,6 +14,31 @@ namespace GuessMyNumber
             // Write a program where the program chooses a number between 1 and 100. 
             // The player is then asked to enter a guess. If the player guesses wrong, then the program gives feedback 
             // and ask to enter an other guess until the guess is correct.
+            // Make the range customizable (ask for it before starting the guessing).
+            // You can add lives. (optional)
+
+            // Make the range
+            Console.WriteLine("GUESS MY NUMBER \n \n");
+            Console.WriteLine("Please give me the parameter of the range.");
+
+            Console.WriteLine("Lower limit:");
+            int LowerLimit = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Upper limit:");
+            int UpperLimit = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("\nBased on your request the limits are the following:\n");
+            Console.WriteLine("Lower limit is: " + LowerLimit);
+            Console.WriteLine("Upper limit is: " + UpperLimit);
+
+            Console.ReadKey();
+
+            // Generate random number
+
+            //Random rnd = new Random();
+            //int ComputerNumber = rnd.Next(1, 101);
+            //Console.WriteLine(ComputerNumber);
+            //Console.ReadKey();
 
         }
     }
