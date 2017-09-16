f6e38a93c3cab52cdb7e8755d97b0eec35ab1902
Lorant Lajos <trojanosall@gmail.com>
2017-09-17 00:04:20 +0200

    Week-02 Day-2 exercise-17-SwapElements - Arrays - Done

--- week-02/day-2/exercise-17-SwapElements/SwapElements/SwapElements/Program.cs
+++ week-02/day-2/exercise-17-SwapElements/SwapElements/SwapElements/Program.cs
@@ -14,6 +14,23 @@ namespace SwapElements
             // - Create an array variable named `abc`
             //   with the following content: `["first", "second", "third"]`
             // - Swap the first and the third element of `abc`
+
+            string[] abc = new string[]
+            {
+                "first", "second", "third"
+            };
+
+            // Swap the first and the third element of `abc
+            string temp = abc[0];
+            abc[0] = abc[2];
+            abc[2] = temp;
+
+            //Display the element of the array
+            foreach (string i in abc)
+            {
+                Console.Write("{0} ", i);
+            }
+            
             Console.ReadKey();
 
         }
