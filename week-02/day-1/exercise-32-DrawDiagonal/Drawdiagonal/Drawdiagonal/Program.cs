a67373b340d4b19c2625af2d7911c16199dbdf0b
Lorant Lajos <trojanosall@gmail.com>
2017-09-16 16:30:02 +0200

    Week-02 Day-1 exercise-32-DrawDiagonal - First Step

---
+++ week-02/day-1/exercise-32-DrawDiagonal/Drawdiagonal/Drawdiagonal/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Drawdiagonal
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // Write a program that reads a number from the standard input, then draws a
+            // square like this:
+            //
+            //
+            // %%%%%
+            // %%  %
+            // % % %
+            // %  %%
+            // %%%%%
+            //
+            // The square should have as many lines as the number was
+
+        }
+    }
+}
