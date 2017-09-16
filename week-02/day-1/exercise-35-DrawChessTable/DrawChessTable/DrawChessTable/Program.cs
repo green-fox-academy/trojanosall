f5323bf8ba4dce50f44e1ffe020faa9c8c9b7f94
Lorant Lajos <trojanosall@gmail.com>
2017-09-16 20:58:18 +0200

    Week-02 Day-1 exercise-35-DrawChessTable - First Step

---
+++ week-02/day-1/exercise-35-DrawChessTable/DrawChessTable/DrawChessTable/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DrawChessTable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Crate a program that draws a chess table like this
+            //
+            // % % % %
+            //  % % % %
+            // % % % %
+            //  % % % %
+            // % % % %
+            //  % % % %
+            // % % % %
+            //  % % % %
+            //
+
+        }
+    }
+}
