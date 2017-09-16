86625a829d575f4bb5f482a6137087a5244d3bf6
Lorant Lajos <trojanosall@gmail.com>
2017-09-16 15:15:32 +0200

    Week-02 Day-1 exercise-12-Cuboid - Done

--- week-02/day-1/exercise-12-Cuboid/Cuboid/Cuboid/Program.cs
+++ week-02/day-1/exercise-12-Cuboid/Cuboid/Cuboid/Program.cs
@@ -16,6 +16,23 @@ namespace Cuboid
             // Surface Area: 600
             // Volume: 1000
 
+            Console.WriteLine("CALCULATOR FOR SURFACE AND VOLUME OF A CUBOID\n\n");
+            Console.WriteLine("Please give me the length of the cuboid: ");
+            int length = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Please give me the width of the cuboid: ");
+            int width = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Please give me the height of the cuboid: ");
+            int height = int.Parse(Console.ReadLine());
+
+            int SurfaceArea, Volume;
+                        
+            Console.WriteLine("\nSURFACE AREA OF THE CUBOID: " + (SurfaceArea = 2 * length * width + 2 * length * height + 2 * width * height) + "\n");
+            Console.WriteLine("VOLUME OF THE CUBOID: " + (Volume = length * width * height));
+
+            Console.ReadKey();
+
         }
     }
 }
