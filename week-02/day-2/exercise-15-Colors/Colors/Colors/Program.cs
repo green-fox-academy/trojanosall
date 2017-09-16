c4425fa793f8f0b61e5bb638d1a946353afc08a7
Lorant Lajos <trojanosall@gmail.com>
2017-09-16 23:27:39 +0200

    Week-02 Day-2 exercise-15-Colors - Arrays - Done

--- week-02/day-2/exercise-15-Colors/Colors/Colors/Program.cs
+++ week-02/day-2/exercise-15-Colors/Colors/Colors/Program.cs
@@ -20,10 +20,30 @@ namespace Colors
             // - In `colors[2]` store the shades of pink:
             //   `"orchid", "violet", "pink", "hot pink"`
 
+            // JAGGED ARRAYS. The jagged array is an array whose elements are arrays. The elements of a jagged array can be of different dimensions and sizes. A jagged array is sometimes called an "array of arrays."
+            // https://docs.microsoft.com/en-us/dotnet/csharp/programming-guide/arrays/jagged-arrays
+
+            //Initialize the array upon declaration like this:
+
+            string[][] colors = new string[][]
+            {
+                new string[] {"lime", "forest green", "olive", "pale green", "spring green"},
+                new string[] {"orange red", "red", "tomato" },
+                new string[] {"orchid", "violet", "pink", "hot pink" }
+            };
 
 
+            //Display the array elements:
+            for (int i = 0; i < colors.Length; i++)
+            {
+                for (int j = 0; j < colors[i].Length; j++)
+                {
+                    Console.Write(colors[i][j] + "  ");
+                }
+                Console.WriteLine();
+            }
+            
             Console.ReadKey();
-
         }
     }
 }
