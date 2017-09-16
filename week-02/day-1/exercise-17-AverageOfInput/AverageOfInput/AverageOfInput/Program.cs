f5c7324a216288a502566c27df5142d328650093
Lorant Lajos <trojanosall@gmail.com>
2017-09-16 15:42:09 +0200

    Week-02 Day-1 exercise-16-AverageOfInput - First Steps

---
+++ week-02/day-1/exercise-17-AverageOfInput/AverageOfInput/AverageOfInput/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AverageOfInput
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // Write a program that asks for 5 integers in a row,
+            // then it should print the sum and the average of these numbers like:
+            //
+            // Sum: 22, Average: 4.4
+
+        }
+    }
+}
