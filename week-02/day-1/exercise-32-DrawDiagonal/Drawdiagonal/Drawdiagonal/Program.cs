d6aed4083121cd70c45de59831e1c86af651238c
Lorant Lajos <trojanosall@gmail.com>
2017-09-16 19:00:05 +0200

    Week-02 Day-1 exercise-32-DrawDiagonal - Done

--- week-02/day-1/exercise-32-DrawDiagonal/Drawdiagonal/Drawdiagonal/Program.cs
+++ week-02/day-1/exercise-32-DrawDiagonal/Drawdiagonal/Drawdiagonal/Program.cs
@@ -23,6 +23,53 @@ namespace Drawdiagonal
             //
             // The square should have as many lines as the number was
 
+            Console.Write("Give me the height of your square: ");
+            int height = int.Parse(Console.ReadLine());
+
+            Console.WriteLine();
+            Console.WriteLine("Here is your square with a diagonal line!");
+
+
+            // PREPARING THE TOP OF THE SQUARE
+            for (int i = 0; i < height; i++)
+            {
+                Console.Write("%");
+            }
+
+            // PREPARING THE MIDDLE OF THE SQUARE
+            int counter = 0;
+
+            Console.WriteLine();
+            for (int j = 0; j < height - 2; j++)
+            {
+                Console.Write("%");
+                for (int k = 0; k < height - 2; k++)
+                {
+                    if (k == counter)
+                    {
+                        Console.Write("%");
+                    }
+                    else
+                    {
+                        Console.Write(" ");
+                    }
+                }
+                counter++;
+                Console.Write("%");
+                Console.WriteLine();
+            }
+
+            // PREPARING THE BOTTOM OF THE SQUARE
+            for (int i = 0; i < height; i++)
+            {
+                Console.Write("%");
+            }
+            Console.WriteLine();
+
+
+            Console.ReadKey();
+
+
         }
     }
 }
