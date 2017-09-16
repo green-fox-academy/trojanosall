fa5884f2885c325f575911f8b99e72cf915823f4
Lorant Lajos <trojanosall@gmail.com>
2017-09-16 15:38:17 +0200

    Week-02 Day-1 exercise-16-AnimalsAndLegs - Done

--- week-02/day-1/exercise-16-AnimalsAndLegs/AnimalsAndLegs/AnimalsAndLegs/Program.cs
+++ week-02/day-1/exercise-16-AnimalsAndLegs/AnimalsAndLegs/AnimalsAndLegs/Program.cs
@@ -16,8 +16,18 @@ namespace AnimalsAndLegs
             // The seconf represents the number of pigs the farmer has
             // It should print how many legs all the animals have
 
-            Console.WriteLine();
+            Console.WriteLine("ALL LEGS CALCULATOR\n\n");
 
+            Console.WriteLine("Please give me how many chickens are had by the farm: ");
+            int ChickensNumber = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("\nPlease give me how many pigs are had by the farm: ");
+            int PigsNumber = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("\nHow many legs all the animals have: " + (ChickensNumber * 2 + PigsNumber * 4));
+
+            Console.ReadKey();
+            
         }
     }
 }
