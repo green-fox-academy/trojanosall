61b0868c9fa185daec1b2e4342a445a681423763
Lorant Lajos <trojanosall@gmail.com>
2017-09-16 23:30:53 +0200

    Week-02 Day-2 exercise-16-AppendA - Arrays - First Steps

---
+++ week-02/day-2/exercise-16-AppendA/AppendA/AppendA/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppendA
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // - Create an array variable named `nimals`
+            //   with the following content: `["kuty", "macsk", "cic"]`
+            // - Add all elements an `"a"` at the end
+            Console.ReadKey();
+
+        }
+    }
+}
