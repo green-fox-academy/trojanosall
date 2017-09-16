f518133838dfc2ae3f62339dc57102772768ab4a
Lorant Lajos <trojanosall@gmail.com>
2017-09-16 14:53:53 +0200

    Week-02 Day-1 exercise-12-Cuboid - First Steps

---
+++ week-02/day-1/exercise-12-Cuboid/Cuboid/Cuboid/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cuboid
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Write a program that stores 3 sides of a cuboid as variables (doubles)
+            // The program should write the surface area and volume of the cuboid like:
+            //
+            // Surface Area: 600
+            // Volume: 1000
+
+        }
+    }
+}
