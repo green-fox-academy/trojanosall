a37bff43dc7b99a8933c73491822377a96d0d4cb
Lorant Lajos <trojanosall@gmail.com>
2017-09-16 23:43:06 +0200

    Week-02 Day-2 exercise-16-AppendA - Arrays - Done

--- week-02/day-2/exercise-16-AppendA/AppendA/AppendA/Program.cs
+++ week-02/day-2/exercise-16-AppendA/AppendA/AppendA/Program.cs
@@ -14,6 +14,22 @@ namespace AppendA
             // - Create an array variable named `nimals`
             //   with the following content: `["kuty", "macsk", "cic"]`
             // - Add all elements an `"a"` at the end
+
+            string[] nimals = new string[]
+                {
+                    "kuty", "macsk", "cic"
+                };
+
+            nimals[0] += "a";
+            nimals[1] += "a";
+            nimals[2] += "a";
+
+            //Display the element of the array
+            foreach (string i in nimals)
+            {
+                Console.Write("{0} ", i);
+            }
+
             Console.ReadKey();
 
         }
