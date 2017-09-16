37dafde018d0d8ae2abdf9895daee0e40a9518a1
Lorant Lajos <trojanosall@gmail.com>
2017-09-16 23:46:34 +0200

    Week-02 Day-2 exercise-17-SwapElements - Arrays - First Steps

---
+++ week-02/day-2/exercise-17-SwapElements/SwapElements/SwapElements/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SwapElements
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // - Create an array variable named `abc`
+            //   with the following content: `["first", "second", "third"]`
+            // - Swap the first and the third element of `abc`
+            Console.ReadKey();
+
+        }
+    }
+}
