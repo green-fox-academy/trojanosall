7b4b394160f5b2918a02ac814e43dffd4618a616
Lorant Lajos <trojanosall@gmail.com>
2017-09-16 15:31:33 +0200

    Week-02 Day-1 exercise-16-AnimalsAndLegs - First Step

---
+++ week-02/day-1/exercise-16-AnimalsAndLegs/AnimalsAndLegs/AnimalsAndLegs/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AnimalsAndLegs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // Write a program that asks for two integers
+            // The first represents the number of chickens the farmer has
+            // The seconf represents the number of pigs the farmer has
+            // It should print how many legs all the animals have
+
+            Console.WriteLine();
+
+        }
+    }
+}
