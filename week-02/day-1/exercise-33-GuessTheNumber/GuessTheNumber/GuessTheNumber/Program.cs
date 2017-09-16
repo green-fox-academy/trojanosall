5cbeb4f41228e6fa19ba3c80fa9765d3fb7c6061
Lorant Lajos <trojanosall@gmail.com>
2017-09-16 19:48:54 +0200

    Week-02 Day-1 exercise-33-GuessTheNumber - Done

--- week-02/day-1/exercise-33-GuessTheNumber/GuessTheNumber/GuessTheNumber/Program.cs
+++ week-02/day-1/exercise-33-GuessTheNumber/GuessTheNumber/GuessTheNumber/Program.cs
@@ -19,6 +19,35 @@ namespace GuessTheNumber
             // The stried number is lower
             // You found the number: 8
 
+            Console.WriteLine("Lets play My Friend. I have a number between 1 and 100. Try to figure it out.");
+
+            int UserTip;
+            Random RandomNumber = new Random();
+            int ComputerNumber = RandomNumber.Next(1,100);
+                       
+
+            do
+            {
+                Console.WriteLine("\nWhat do you think what is my number?");
+                UserTip = int.Parse(Console.ReadLine());
+
+                if (UserTip > ComputerNumber)
+                {
+                    Console.WriteLine("The stored number is lower.");
+                }
+                else if (UserTip < ComputerNumber)
+                {
+                    Console.WriteLine("The stored number is higher");
+                }
+                else
+                {
+                     Console.WriteLine("YOU WIN!!!");
+                }
+            }
+
+            while (UserTip != ComputerNumber);                     
+
+            Console.ReadKey();
         }
     }
 }
