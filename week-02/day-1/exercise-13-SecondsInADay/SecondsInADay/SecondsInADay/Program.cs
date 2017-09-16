8eb15023ed9d337653f6b976de06a6d28619bf7d
Lorant Lajos <trojanosall@gmail.com>
2017-09-16 15:20:29 +0200

    Week-02 Day-1 exercise-13-SecondsInADay - First Step

---
+++ week-02/day-1/exercise-13-SecondsInADay/SecondsInADay/SecondsInADay/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SecondsInADay
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int currentHours = 14;
+            int currentMinutes = 34;
+            int currentSeconds = 42;
+
+            // Write a program that prints the remaining seconds (as an integer) from a
+            // day if the current time is represented bt the variables
+
+
+        }
+    }
+}
