687ff9d6699f669fcbf83fc95be4a9ccaca9a849
Lorant Lajos <trojanosall@gmail.com>
2017-09-17 10:48:31 +0200

    Week 02 - Day 03 - Exercise-03-List-AppendLetter - First Steps

---
+++ week-02/day-3/exercise-03-list/AppendLetter/AppendLetter/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppendLetter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var far = new List<string> { "kuty", "macsk", "kacs", "rók", "halacsk" };
+            // Add "a" to every string in the far list.
+
+            Console.WriteLine(far);
+
+        }
+    }
+}
