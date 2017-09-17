53687ebf0c646418f03f4c8635665618506ff37a
Lorant Lajos <trojanosall@gmail.com>
2017-09-18 00:55:27 +0200

    Practice - Funcions - SpaceCounter

---
+++ week-02/Practice/Funcions/CountSpaces/CountSpaces/CountSpaces/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CountSpaces
+{
+    class Program
+    {
+        public static int SpaceCount (string userInput)
+        { 
+            int countSpaces = userInput.Count(Char.IsWhiteSpace);
+            Console.WriteLine(countSpaces);
+
+            return countSpaces;
+        }
+        static void Main(string[] args)
+        {
+
+            // Write a program in C# Sharp to create a function to input a string and count number of spaces are in the string.
+
+            string userInput = "This is a test string.";
+
+            SpaceCount(userInput);
+
+            Console.WriteLine(SpaceCount(userInput));
+
+            Console.ReadKey();
+        }
+    }
+}
