a723cbe90787988d99552c2e226b86bd1746814f
Lorant Lajos <trojanosall@gmail.com>
2017-09-17 15:41:11 +0200

    Week 02 - Day 03 - Exercise-06-List-IsInList - Done2 - with an other solution

--- week-02/day-3/exercise-06-list/IsInList/IsInList/Program.cs
+++ week-02/day-3/exercise-06-list/IsInList/IsInList/Program.cs
@@ -29,6 +29,30 @@ namespace IsInList
                 Console.WriteLine("False");
             }
 
+            // OTHER SOLUTION FOR THE SAME PROBLEM
+            // 
+            // int n1 = 4;
+            // int n2 = 8;
+            // int n3 = 12;
+            // int n4 = 16;
+            // int contains = 0;
+            // 
+            // for (int i = 0; i < list.Count; i++)
+            // {
+            // if (list[i] == n1 || list[i] == n2 || list[i] == n3 || list[i] == n4)
+            // {
+            // contains++;
+            // }
+            // }
+            // if (contains == 4)
+            // {
+            // Console.WriteLine("True");
+            // }
+            // else
+            // {
+            // Console.WriteLine("False");
+            // }
+
             Console.ReadKey();
         }
     }
