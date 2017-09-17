2ee06cfa802b5c479d49044d9fcf04e336539be5
Lorant Lajos <trojanosall@gmail.com>
2017-09-17 15:55:34 +0200

    Week 02 - Day 03 - Exercise-07-List-QuoteSwap - First Step

---
+++ week-02/day-3/exercise-7-list/QuoteSwap/QuoteSwap/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuoteSwap
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var list = new List<string> { "What", "I", "do", "create,", "I", "cannot", "not", "understand." };
+
+            // Accidentally I messed up this quote from Richard Feynman.
+            // Two words are out of place
+            // Your task is to fix it by swapping the right words with code
+
+            // Also, print the sentence to the output with spaces in between.
+
+        }
+    }
+}
