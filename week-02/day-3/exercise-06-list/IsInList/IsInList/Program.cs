cc26b56c6518fc87184ebad8d9d70ab034731ea3
Lorant Lajos <trojanosall@gmail.com>
2017-09-17 15:00:11 +0200

    Week 02 - Day 03 - Exercise-06-List-IsInList - Done

---
+++ week-02/day-3/exercise-06-list/IsInList/IsInList/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IsInList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var list = new List<int> { 2, 4, 6, 8, 10, 12, 14, 16 };
+
+            // Check if list contains all of the following elements: 4,8,12,16
+            // Print "true" if it contains all, otherwise print "false"
+            // Can you do both the different approaches you tried in the previous one?
+
+
+        }
+    }
+}
