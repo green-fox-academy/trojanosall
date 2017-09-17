74b045d1e8e39eb61615fa44eb8ed42988c5a8d1
Lorant Lajos <trojanosall@gmail.com>
2017-09-17 18:10:48 +0200

    Week 02 - Day 3 - Exercise-01-Dictionary - Done

--- week-02/day-3/exercise-01-Dictionary/HeWillNever/HeWillNever/Program.cs
+++ week-02/day-3/exercise-01-Dictionary/HeWillNever/HeWillNever/Program.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace HeWillNever
+namespace HeWillnever
 {
     class Program
     {
@@ -14,7 +14,7 @@ namespace HeWillNever
             string output = "";
             int[] notSoCrypticMessage = { 1, 12, 1, 2, 11, 1, 7, 11, 1, 49, 1, 3, 11, 1, 50, 11 };
 
-            var map = new Dictionary<string, string>();
+            var map = new Dictionary<int, string>();
 
             map.Add(7, "run around and desert you");
             map.Add(50, "tell a lie and hurt you");
@@ -29,7 +29,14 @@ namespace HeWillNever
             // Your job is to decode the notSoCrypticMessage by using the hashmap as a look up table
             // Assemble the fragments into the out variable
 
+
+            for (int i = 0; i < notSoCrypticMessage.Length; i++)
+            {
+                output += map[notSoCrypticMessage[i]] + " ";
+            }
             Console.WriteLine(output);
+
+            Console.ReadLine();
 
         }
     }
--- week-02/day-3/exercise-01-Dictionary/HeWillNever/HeWillNever/Properties/AssemblyInfo.cs
+++ week-02/day-3/exercise-01-Dictionary/HeWillNever/HeWillNever/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("HeWillNever")]
+[assembly: AssemblyTitle("HeWillnever")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("HeWillNever")]
+[assembly: AssemblyProduct("HeWillnever")]
 [assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("ec7c67fe-573c-4637-b609-0ef74c937f92")]
+[assembly: Guid("ddce2204-56bb-4c39-9e2d-d356bd5a9ed7")]
 
 // Version information for an assembly consists of the following four values:
 //
