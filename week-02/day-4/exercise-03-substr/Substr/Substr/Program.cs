f8973ca4141043ad615dfb03021221d8784ed2aa
Lorant Lajos <trojanosall@gmail.com>
2017-09-17 23:12:56 +0200

    Week 02 - Day 4 - Exercise-03-Substr - First Steps

---
+++ week-02/day-4/exercise-03-substr/Substr/Substr/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Substr
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // Find part of an integer
+            // Create a function that takes two strings as a parameter
+            // Returns the starting index where the second one is starting in the first one
+            // Returns -1 if the second string is not in the first one
+            //
+            // Example
+            // input: "this is what I'm searching in", "searching"
+            // output: 17
+
+
+
+        }
+    }
+}
