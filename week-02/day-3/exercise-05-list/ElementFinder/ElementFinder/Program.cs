2e764d20eb209b4fe22b662db5bd873cbe3cfba2
Lorant Lajos <trojanosall@gmail.com>
2017-09-17 14:50:00 +0200

    Week 02 - Day 03 - Exercise-05-List-ElementFinder - First Step

---
+++ week-02/day-3/exercise-05-list/ElementFinder/ElementFinder/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ElementFinder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var List = new List<int> { 1, 2, 3, 4, 5 };
+
+            // Check if the arrayList contains "7" if it contains print "Hoorray" otherwise print "Noooooo"
+            // Do this again with a different solution using different list functions!
+
+        }
+    }
+}
