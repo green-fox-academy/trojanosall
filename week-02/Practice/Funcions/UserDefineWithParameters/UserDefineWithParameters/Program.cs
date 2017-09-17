d3f8f7678b3ed12b96aa7163b342cc5f5162b24f
Lorant Lajos <trojanosall@gmail.com>
2017-09-18 00:19:09 +0200

    Practice - Funcions - UserDefineWithParameter

---
+++ week-02/Practice/Funcions/UserDefineWithParameters/UserDefineWithParameters/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UserDefineWithParameters
+{
+    class Program
+    {
+        public static void WelcomeWithParameter(string UserName)
+        {
+            Console.WriteLine("Please input your name: ");            
+            Console.WriteLine("Welcome friend " + Console.ReadLine() + "\nHave a nice day!");
+        }
+
+        static void Main(string[] args)
+        {
+
+            // Write a program in C# Sharp to create a user define function with parameters. 
+
+            string UserName=Console.ReadLine();
+            WelcomeWithParameter(UserName);
+
+            Console.ReadKey();
+
+        }
+    }
+}
