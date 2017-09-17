e9d010b0583b7261d824076e356eafe9221ba75b
Lorant Lajos <trojanosall@gmail.com>
2017-09-18 00:07:30 +0200

    Practice - Funcions - UserDefine

---
+++ week-02/Practice/Funcions/UserDefine/UserDefine/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UserDefine
+{
+    class Program
+    {
+        public static void UserDefine()
+        {
+            Console.WriteLine("Welcome Friends!\nHave a nice day!");
+        }
+
+        static void Main(string[] args)
+        {
+
+            // Write a program in C# Sharp to create a user define function.
+
+            UserDefine();
+            Console.ReadKey();
+        }
+    }
+}
