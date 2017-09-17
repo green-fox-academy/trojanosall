0613b2e389b8e527fc53e9e9e811a04b52f0a46c
Lorant Lajos <trojanosall@gmail.com>
2017-09-17 20:25:49 +0200

    Week 02 - Day 4 - Exercise-02-Bubble - First Steps

---
+++ week-02/day-4/exercise-02-Bubble/Bubble/Bubble/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bubble
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // Sort that list
+            // Create a function that takes a list of numbers as parameter
+            // Returns a list where the elements are sorted in ascending numerical order
+            // Make a second boolean parameter, if it's true sort that list descending
+            // Example
+            // input [34, 12, 24, 9, 5]
+            // output [5, 9, 12, 24, 34]
+
+        }
+    }
+}
