6ca0efa9a31a54af975a68ce0eedf5532cb0b618
Lorant Lajos <trojanosall@gmail.com>
2017-09-17 18:48:33 +0200

    Week 02 - Day 4 - Exercise-01-Unique - First Step

---
+++ week-02/day-4/exercise-01-Unique/Unique/Unique/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Unique
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // Unique - remove the duplicates
+            // Create a function that takes a list of numbers as a parameter
+            // Returns a list of numbers where every number in the list occurs only once
+
+            // Example
+            // Input: [1, 11, 34, 11, 52, 61, 1, 34]
+            // Output: [1, 11, 34, 52, 61]
+
+
+
+        }
+    }
+}
