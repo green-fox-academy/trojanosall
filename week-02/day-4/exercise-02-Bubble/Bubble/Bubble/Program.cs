68157f0a340987583adba4e2c07b84e9575c0332
Lorant Lajos <trojanosall@gmail.com>
2017-09-17 23:09:30 +0200

    Week 02 - Day 4 - Exercise-02-Bubble - Done

--- week-02/day-4/exercise-02-Bubble/Bubble/Bubble/Program.cs
+++ week-02/day-4/exercise-02-Bubble/Bubble/Bubble/Program.cs
@@ -8,6 +8,26 @@ namespace Bubble
 {
     class Program
     {
+
+        // Fuggveny a lista elemeinek NOVEKVO sorba rendezesere
+        public static List<int> Ascending (List<int> ListElements)
+        {
+            ListElements.Sort();
+            return ListElements;
+        }
+
+
+        // Fuggveny a lista elemeinek CSOKKENO sorba rendezesere
+        public static List<int> Descending (List<int> ListElements, bool boolean)
+        {
+            if (boolean == true)
+            {
+                ListElements.Sort();
+                ListElements.Reverse();
+            }
+            return ListElements;
+        }
+               
         static void Main(string[] args)
         {
 
@@ -18,6 +38,34 @@ namespace Bubble
             // Example
             // input [34, 12, 24, 9, 5]
             // output [5, 9, 12, 24, 34]
+
+
+            //LISTA ELEMEK DEKLARALASA
+            List<int> ListElements = new List<int> { 34, 12, 24, 9, 5 };
+
+
+            // NOVEKVO SORBA RENDEZO FUGGVENY MEGHIVASA
+            Ascending(ListElements);
+                        
+            //LISTA ELEMEINEK MEGJELENITESE
+            foreach (int i in ListElements)
+            {
+                Console.Write("{0} ", i);
+            }
+
+            Console.WriteLine();
+
+
+            // CSOKKENO SORBA RENDEZO FUGGVENY MEGHIVASA
+            Descending(ListElements, true);
+
+            //LISTA ELEMEINEK MEGJELENITESE
+            foreach (int i in ListElements)
+            {
+                Console.Write("{0} ", i);
+            }
+
+            Console.ReadKey();
 
         }
     }
