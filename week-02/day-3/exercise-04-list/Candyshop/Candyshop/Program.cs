2dd32c196bfc0bdcaeff585d36a7a86fe8dda292
Lorant Lajos <trojanosall@gmail.com>
2017-09-17 14:38:38 +0200

    Week 02 - Day 03 - Exercise-04-List-Candyshop - First Step

---
+++ week-02/day-3/exercise-04-list/Candyshop/Candyshop/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Candyshop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var list = new List<object>();
+            list.Add("Cupcake");
+            list.Add(2);
+            list.Add("Brownie");
+            list.Add(false);
+
+            // Accidentally we added "2" and "false" to the list. 
+            // Your task is to change from "2" to "Croissant" and change from "false" to "Ice cream"
+            // No, don't just remove the lines
+
+        }
+    }
+}
