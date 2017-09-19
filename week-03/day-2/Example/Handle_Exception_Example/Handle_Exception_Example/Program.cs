1c94d8d25f8a992b28a6804520793770d4bdd14f
Lorant Lajos <trojanosall@gmail.com>
2017-09-19 12:16:54 +0200

    Week 03 day 02 - Example - Handle the exeption

---
+++ week-03/day-2/Example/Handle_Exception_Example/Handle_Exception_Example/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Handle_Exception_Example
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            try  // Prevents the program breaking when attempting dividing by zero
+            {
+                int divisor = int.Parse(Console.ReadLine());
+                // If the input value for divisor was 0 the program breaks
+                int result = 12 / divisor;
+                // The program doesn't reach this line if the input was 0
+                Console.WriteLine(result);
+            }
+            catch (DivideByZeroException e)
+            {
+                // This line only runs if the input was 0
+                Console.WriteLine("Can't divide by zero!");
+            }
+            catch (Exception e)
+            {
+                // This line only runs if uncatched exception generated
+                Console.WriteLine("uuups, some serious thing happened!");
+            }
+            finally
+            {
+                Console.ReadLine();
+            }
+
+        }
+    }
+}
