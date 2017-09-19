5b42121591eeced9df3ff789f052265a697038be
Lorant Lajos <trojanosall@gmail.com>
2017-09-19 13:32:50 +0200

    Week 03 - Day 02 - Exercise-03-CountLines - First Step

---
+++ week-03/day-2/exercise-03-CountLines/CountLines/CountLines/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CountLines
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // Write a function that takes a filename as string,
+            // then returns the number of lines the file contains.
+            // It should return zero if it can't open the file, and
+            // should not raise any error.
+
+        }
+    }
+}
