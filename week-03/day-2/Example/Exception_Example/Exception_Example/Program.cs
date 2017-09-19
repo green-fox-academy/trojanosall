49c88091af1897df7138b27f34a44829bfabb9b5
Lorant Lajos <trojanosall@gmail.com>
2017-09-19 12:02:56 +0200

    Week-03 Day-02 - Exception_Example

---
+++ week-03/day-2/Example/Exception_Example/Exception_Example/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exception_Example
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int divisor = int.Parse(Console.ReadLine());
+            // If the input value for divisor was 0 the program breaks
+            int result = 12 / divisor;
+            // The program doesn't reach this line if the input was 0
+            Console.WriteLine(result);
+            Console.ReadLine();
+
+        }
+    }
+}
