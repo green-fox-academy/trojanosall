cf5d04f9de1d808ca3ab82706775ac10a8855528
Lorant Lajos <trojanosall@gmail.com>
2017-09-19 13:18:05 +0200

    Week 03 - Day-02 - Exercise-02-PrintingEachLine - First Step

---
+++ week-03/day-2/exercise-2-PrintingEachLine/PrintingEachLine/PrintingEachLine/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrintingEachLine
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // Write a program that opens a file called "my-file.txt", then prints
+            // each of lines form the file.
+            // If the program is unable to read the file (for example it does not exists),
+            // then it should print an error message like: "Unable to read file: my-file.txt"
+
+        }
+    }
+}
