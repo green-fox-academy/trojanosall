d6df20faa409e6e969218b9545c8e587993ad8b1
Lorant Lajos <trojanosall@gmail.com>
2017-09-19 12:58:55 +0200

    Week 03 - Day 2 - Exercise-01-Divided By Zero - Take a number funcion created

---
+++ week-03/day-2/exercise-01-DivideByZero/DividedByZero/DividedByZero/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DividedByZero
+{
+    class Program
+    {
+        public static int TakeNumber()
+        {
+            Console.WriteLine("Please give me a number and I will divide ten with it: ");
+            int TakeNumber = int.Parse(Console.ReadLine());
+            return TakeNumber;
+
+        }
+
+        static void Main(string[] args)
+        {
+
+            // create a function that takes a number
+            // divides ten with it,
+            // and prints the result.
+            // it should print "fail" if the parameter is 0
+
+            TakeNumber();
+
+            Console.ReadKey();
+
+
+        }
+    }
+}
