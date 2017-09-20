ce3d060ce175a6d07db768ee002550675c72deb9
Lorant Lajos <trojanosall@gmail.com>
2017-09-20 11:45:12 +0200

    Week 03 Day 03 Exercise 01 LineInTheMiddle - Done

---
+++ week-03/day-3/Ex-01-LineInTheMiddle/LineInTheMiddle/LineInTheMiddle/MainWindow.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using GreenFox;
+
+namespace LineInTheMiddle
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            var foxDraw = new FoxDraw(canvas);
+
+            //  draw a red horizontal line to the canvas' middle.
+            //  draw a green vertical line to the canvas' middle.
+
+            DrawGreenLines(foxDraw);
+        }
+
+        public static void DrawGreenLines(FoxDraw foxDraw)
+        {
+            foxDraw.StrokeColor(Colors.Red);
+            foxDraw.DrawLine(0, 150, 300, 150);
+
+            foxDraw.StrokeColor(Colors.Green);
+            foxDraw.DrawLine(150, 0, 150, 300);
+                        
+        }
+    }
+}
