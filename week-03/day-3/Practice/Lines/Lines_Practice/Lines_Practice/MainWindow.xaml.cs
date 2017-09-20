830ae75531db52966b036a8cd95eaa6247a0e764
Lorant Lajos <trojanosall@gmail.com>
2017-09-20 10:58:29 +0200

    Week 03 Day 02 Practice Example Lines

---
+++ week-03/day-3/Practice/Lines/Lines_Practice/Lines_Practice/MainWindow.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using GreenFox;
+
+namespace Lines_Practice
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            var foxDraw = new FoxDraw(canvas);
+
+            foxDraw.StrokeColor(Colors.Green);
+            foxDraw.DrawLine(0, 10, 100, 10);
+
+            var startPoint = new Point(0, 20);
+            var endPoint = new Point(100, 20);
+            foxDraw.DrawLine(startPoint, endPoint);
+        }
+    }
+}
