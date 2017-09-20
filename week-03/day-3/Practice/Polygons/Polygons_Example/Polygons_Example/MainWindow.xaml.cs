106c1ee5fb098929d39bf0995cd7412336856975
Lorant Lajos <trojanosall@gmail.com>
2017-09-20 11:27:36 +0200

    Week 03 Day 03 Practice Example Polygons

---
+++ week-03/day-3/Practice/Polygons/Polygons_Example/Polygons_Example/MainWindow.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using GreenFox;
+
+namespace Polygons_Example
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            var foxDraw = new FoxDraw(canvas);
+
+            DrawGreenPolygon(foxDraw);
+        }
+
+        public static void DrawGreenPolygon(FoxDraw foxDraw)
+        {
+            var points = new List<Point>();
+            points.Add(new Point(10, 10));
+            points.Add(new Point(40, 210));
+            points.Add(new Point(170, 190));
+            points.Add(new Point(130, 40));
+
+            foxDraw.FillColor(Colors.Green);
+            foxDraw.DrawPolygon(points);
+        }
+    }
+}
