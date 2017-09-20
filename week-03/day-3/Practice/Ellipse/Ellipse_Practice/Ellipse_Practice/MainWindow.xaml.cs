7ac23ceaacbc7a90c207c2ab07033f0b34c860e1
Lorant Lajos <trojanosall@gmail.com>
2017-09-20 11:17:10 +0200

    Week 03 Day 03 Practice Example Ellipse

---
+++ week-03/day-3/Practice/Ellipse/Ellipse_Practice/Ellipse_Practice/MainWindow.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using GreenFox;
+
+namespace Ellipse_Practice
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            var foxDraw = new FoxDraw(canvas);
+
+            DrawEllipse(foxDraw);
+                        
+        }
+            public static void DrawEllipse(FoxDraw foxDraw)
+            {
+                foxDraw.DrawEllipse(10, 10, 150, 50);
+                foxDraw.FillColor(Colors.Azure);
+            }
+        
+    }
+}
