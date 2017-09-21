9722ddcc361d2ddca42fcb7888ec76a31cebb764
Lorant Lajos <trojanosall@gmail.com>
2017-09-21 10:14:57 +0200

    Week 03 Day-04 - Recursion - Prtactice - Factorial with loop

---
+++ week-03/day-4/Practice/FactorialWithLoop/FactorialWithLoop/FactorialWithLoop/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FactorialWithLoop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 5;
+            
+            Console.WriteLine(FactorialWithLoop(a));
+
+            Console.ReadKey();
+
+        }
+        public static int FactorialWithLoop(int n)
+        {
+            int factorial = 1;
+            for (int i = 1; i < n + 1; i++)
+            {
+                factorial *= i;
+            }
+            return factorial;
+        }
+    }
+    
+}
