11f1c6b37b1442c2df53fd3b33a5bcd22f2a9936
Lorant Lajos <trojanosall@gmail.com>
2017-09-21 10:59:26 +0200

    Week 03 - Day -04 - Exercise-01 - Counter - Done

---
+++ week-03/day-4/Ex-01-Counter/Counter/Counter/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Counter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // Write a recursive function that takes one parameter: n and counts down from n.
+            Console.WriteLine("Plese give me a number and I count down from it: ");
+            int UserInput = int.Parse(Console.ReadLine());
+
+            Counter(UserInput);
+
+            Console.ReadKey();
+        }
+        private static int Counter(int n)
+        {
+
+            if (n == 1)
+            {                
+                return 1;
+            }
+            else
+            {
+                Console.WriteLine(n-1);
+                return Counter(n-1);                
+            }
+        }
+    }
+}
