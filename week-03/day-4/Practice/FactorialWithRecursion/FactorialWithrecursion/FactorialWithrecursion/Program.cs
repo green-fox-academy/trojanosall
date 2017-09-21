827d521555c6247fb699bf3b9c261a960f81ce29
Lorant Lajos <trojanosall@gmail.com>
2017-09-21 10:22:11 +0200

    Week 03 Day 04 - Recursion - Practice - Factorial With Recursion

---
+++ week-03/day-4/Practice/FactorialWithRecursion/FactorialWithrecursion/FactorialWithrecursion/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FactorialWithrecursion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(FactorialWithRecursion(5));
+
+            Console.ReadKey();
+
+        }
+        public static int FactorialWithRecursion(int n)
+        {
+            if (n == 1)
+            {
+                return 1;
+            }
+            else
+            {
+                return n * FactorialWithRecursion(n - 1);
+            }
+        }
+    }
+}
