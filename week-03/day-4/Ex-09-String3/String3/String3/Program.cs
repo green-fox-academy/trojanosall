cb822b5dc94e5d2bc0baff098918f9785da9c2be
Lorant Lajos <trojanosall@gmail.com>
2017-09-22 10:57:48 +0200

    Week 03 - Day 04 - Exercise 09 - String 3 - Done

---
+++ week-03/day-4/Ex-09-String3/String3/String3/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace String3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // Given a string, compute recursively a new string where all the
+            // adjacent chars are now separated by a "*".
+                        
+            string word = "Sajtoskalacstoltelek";
+
+            Console.WriteLine(word);
+
+            Console.WriteLine("\nAfter Separations:\n");
+
+            Console.WriteLine(Separator(word));
+
+            Console.ReadKey();
+
+        }
+
+        private static string Separator(string word)
+        {
+            if (word.Length < 1)
+            { 
+                return word;
+            }
+            else
+            { 
+                return word[0] + "*" + Separator(word.Substring(1));
+            }
+        }
+    }
+}
