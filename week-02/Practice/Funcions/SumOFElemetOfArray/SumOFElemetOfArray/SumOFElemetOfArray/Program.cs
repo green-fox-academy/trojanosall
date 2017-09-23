41be7e08dd375465f6b8ae65ef1605576f9fd2f5
Lorant Lajos <trojanosall@gmail.com>
2017-09-24 01:29:45 +0200

    Practice - Funcions

---
+++ week-02/Practice/Funcions/SumOFElemetOfArray/SumOFElemetOfArray/SumOFElemetOfArray/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SumOFElemetOfArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //Write a program in C# Sharp to calculate the sum of elements in an array.
+
+            int[] NewArray = new int[] { 5, 7, 3, 2, 9};
+
+            Console.WriteLine(SumOfArrayElement(NewArray));
+            
+
+            Console.ReadKey();
+
+        }
+
+
+        private static int SumOfArrayElement(int[] newArray)
+        {
+            int result = 0;
+
+            for (int i = 0; i < newArray.Length; i++)
+            {
+                result += newArray[i];
+            }
+            return result;
+            
+        }
+    }
+}
