4b3edc3069db1dbf551f1fa530a4c303f7b701ad
Lorant Lajos <trojanosall@gmail.com>
2017-09-25 14:19:57 +0200

    Week 04 - Day 01 - Practice - FishTank

---
+++ week-04/day-1/Practice/Practice_FishTank/Practice_FishTank/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practice_FishTank
+{
+    public class FishTank
+    {
+        public static int FishCount = 0;
+
+        public string Status;
+
+        public FishTank()
+        {
+            Status = "There is no fish in the tank!";
+        }
+
+        public static void Add()
+        {
+            FishCount++;
+            Console.WriteLine("There is {0} fish in the tank.", FishCount);
+        }
+
+        public void HowMuchIsTheFish()
+        {
+            Console.WriteLine("Hyper! Hyper!");
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var tank = new FishTank();
+            Console.WriteLine(tank.Status);
+
+            FishTank.Add();
+
+            tank.HowMuchIsTheFish();
+
+            Console.ReadLine();
+        }
+    }
+}
