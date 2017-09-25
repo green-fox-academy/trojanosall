24870c2a710e3d2efe641d6c088ced6d644188df
Lorant Lajos <trojanosall@gmail.com>
2017-09-25 13:24:01 +0200

    Week 04 - Day 01 - Exercise 01 - Struct - Post it(with constructor) - Done

---
+++ week-04/day-1/Ex-01-PostIt/PostIt_V2_Constructor/PostIt_V2_Constructor/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PostIt_V2_Constructor
+{
+    class Program
+    {
+        struct PostIt
+        {
+            public string BackgroundColor;
+            public string Text;
+            public string TextColor;
+
+            public PostIt(string BackgroundColor, string Text, string TextColor) //this is contructor
+            {
+                this.BackgroundColor = BackgroundColor;
+                this.Text = Text;
+                this.TextColor = TextColor;
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            PostIt postIt1 = new PostIt("Orange", "Idea 1", "Blue");
+            PostIt postIt2 = new PostIt("Pink", "Awesome", "Black");
+            PostIt postIt3 = new PostIt("Yellow", "Superb", "Green");
+            
+        }
+    }
+}
