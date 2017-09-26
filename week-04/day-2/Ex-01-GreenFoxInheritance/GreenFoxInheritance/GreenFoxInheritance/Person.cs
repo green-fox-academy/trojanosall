e6f61900593be440dd94763a5b40409383636b26
Lorant Lajos <trojanosall@gmail.com>
2017-09-26 16:47:03 +0200

    Week 04 - Day 02 - Exercise 01 - Green Fox Inheritance - Person Class created

---
+++ week-04/day-2/Ex-01-GreenFoxInheritance/GreenFoxInheritance/GreenFoxInheritance/Person.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GreenFoxInheritance
+{
+    class Person
+    {
+
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public string Gender { get; set; }
+
+        public Person(string name, int age, string gender)
+        {
+            this.Name = name;
+            this.Age = age;
+            this.Gender = gender;
+        }
+
+        public Person()
+        {
+            this.Name = "Jane Doe";
+            this.Age = 30;
+            this.Gender = "female";
+
+        }
+
+        public void Introduce(string name, int age, string gender)
+        {
+            Console.WriteLine("Hi, I'm {0}, a {1} year old {2}.", name, age, gender);
+        }
+
+        public void GetGoal()
+        {
+            Console.WriteLine("My goal is: Live for the moment!");
+        }
+
+    }
+}
