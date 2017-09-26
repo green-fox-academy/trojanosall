a26f1b626821a7d6fdecd09ab6b231ed56469fb8
Lorant Lajos <trojanosall@gmail.com>
2017-09-26 15:35:31 +0200

    Week 04 - Day 02 - Exercise 01 - Green Fox Inheritance - First Steps

--- week-04/day-1/Ex-03-Dominoes/Dominoes/Dominoes/Domino.cs
+++ week-04/day-1/Ex-03-Dominoes/Dominoes/Dominoes/Domino.cs
@@ -6,16 +6,16 @@ using System.Threading.Tasks;
 
 namespace Dominoes
 {
-    public class Domino
+    public class Domino                                   // A Domino nevu class deklaralasa
     {
-        private readonly int[] Values;
+        private readonly int[] Values;                    // A Values egy integer tipusu array. Ez a constructor utan mar constans lesz. Erre jo a readonly.
 
-        public Domino(int valueA, int valueB)
+        public Domino(int valueA, int valueB)             // Constructor ket integer bemenettel
         {
-            this.Values = new int[] { valueA, valueB };
+            this.Values = new int[] { valueA, valueB };   // Values array initialization
         }
 
-        public int[] GetValues()
+        public int[] GetValues()                          // Akkor ez it egy funcion ami integer tipusu array-t ad vissza. Az array pedig a fentebb emlitett readonly-val constans-a tett, integer-eket tartalmazo elem (Values nevu).
         {
             return Values;
         }
--- week-04/day-1/Ex-03-Dominoes/Dominoes/Dominoes/Program.cs
+++ week-04/day-1/Ex-03-Dominoes/Dominoes/Dominoes/Program.cs
@@ -10,15 +10,18 @@ namespace Dominoes
     {
         public static void Main(string[] args)
         {
-            var dominoes = InitializeDominoes();
+            var dominoes = InitializeDominoes(); // ehun meghivom a lenti funcion-t
             // You have the list of Dominoes
             // Order them into one snake where the adjacent dominoes have the same numbers on their adjacent sides
             // Create a function to write the dominous to the console in the following format
             // eg: [2, 4], [4, 3], [3, 5] ...
+            
+            OrderDominoes(dominoes);
+            Console.ReadLine();
 
         }
 
-        public static List<Domino> InitializeDominoes()
+        public static List<Domino> InitializeDominoes() // ez egy funcion, ami legeneralja a dominokat...
         {
             var dominoes = new List<Domino>();
             dominoes.Add(new Domino(5, 2));
@@ -29,5 +32,35 @@ namespace Dominoes
             dominoes.Add(new Domino(7, 1));
             return dominoes;
         }
+        public static void OrderDominoes(List<Domino> listDomino)
+        {
+            var ordered = new List<int[]>();
+            ordered.Add(listDomino[0].GetValues());
+            int[] dominoArray = listDomino[0].GetValues();
+            for (int i = 0; i < listDomino.Count; i++)
+            {
+                for (int j = 0; j < listDomino.Count - 1; j++)
+                {
+                    int[] dominoArray2 = listDomino[j + 1].GetValues();
+                    if (dominoArray[1] == dominoArray2[0])
+                    {
+                        dominoArray = dominoArray2;
+                        ordered.Add(dominoArray2);
+                    }
+                }
+
+            }
+            foreach (var array in ordered)
+            {
+                Console.Write("[");
+                foreach (var numbers in array)
+                {
+                    Console.Write(" " + numbers.ToString() + " ");
+                }
+                Console.Write("] ");
+            }
+            Console.ReadLine();
+        }
+
     }
 }
