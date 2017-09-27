b15c44773158fa869443df919a0c88b268cb694f
Lorant Lajos <trojanosall@gmail.com>
2017-09-27 16:34:10 +0200

    Week 04 - Day 03 - Exercise 03 - Anagram - Done

---
+++ week-04/day-3/Ex-03-Anagram/AnagramMaker/AnagramMaker/Anagram.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AnagramMaker
+{
+    public class Anagram
+    {
+        public static bool AnagramChecker(string firstString, string secondString)
+        {
+            if (firstString.Length != secondString.Length)                            //eleve nem lehet anagramma, ha a ket szo hossza eltero.
+            {
+                return false;
+            }
+            else
+            {
+                Char[] firstStringArray = firstString.ToCharArray();                   // A ket szobol csinaltam ket char tipusu arrayt
+                Char[] secondStringArray = secondString.ToCharArray();
+
+                var result = firstStringArray.Except(secondStringArray);               // Google haverom mondta ezt a tippet. Az elso tombol csinaltam egy olyan tombot, amiben nincsenek benne a masodik tomb elemei. Ha a ket tomb megegyezik, akor az uj tombnek nincs eleme.
+                if (result.Count() == 0)
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+
+                // Anagram
+                //Write a function, that takes two strings and returns a boolean value based on if the two strings are Anagramms or not.
+                //Create a test for that.
+
+            }
+        }
+    }
+}
---
+++ week-04/day-3/Ex-03-Anagram/AnagramMaker/AnagramMakerTest/AnagramTest.cs
@@ -0,0 +1,100 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AnagramMaker;
+
+namespace AnagramMakerTest
+{
+    [TestFixture]
+
+    public class AnagramTest
+    {
+
+       [TestCase]
+        public void TestAnagramMaker_WhenStringsDifferentLenght_ThenFalse()
+        {
+            //arrange
+            string firstString = "kutyamutya";
+            string secondString = "nyuszifulimulike";
+
+            //act
+            bool output = false;
+
+            //assert
+            Assert.AreEqual(output, Anagram.AnagramChecker(firstString, secondString));
+        }
+
+        [TestCase]
+        public void TestAnagramMaker_WhenStringsEqualsLenghtButNotAnagram_ThenFalse()
+        {
+            //arrange
+            string firstString = "kutyamutya";
+            string secondString = "nuoyaluzia";
+
+            //act
+            bool output = false;
+
+            //assert
+            Assert.AreEqual(output, Anagram.AnagramChecker(firstString, secondString));
+        }
+
+        [TestCase]
+        public void TestAnagramMaker_WhenStringsEqualsLenghtAndAnagram_ThenTrue()
+        {
+            //arrange
+            string firstString = "kutyamutya";
+            string secondString = "tyatyamuku";
+
+            //act
+            bool output = true;
+
+            //assert
+            Assert.AreEqual(output, Anagram.AnagramChecker(firstString, secondString));
+        }
+
+        [TestCase]
+        public void TestAnagramMaker_WhenStringsEqualsLenghtWithSpace_ThenFalse()
+        {
+            //arrange
+            string firstString = "kutyamutya";
+            string secondString = "tyaty muku";
+
+            //act
+            bool output = false;
+
+            //assert
+            Assert.AreEqual(output, Anagram.AnagramChecker(firstString, secondString));
+        }
+
+        [TestCase]
+        public void TestAnagramMaker_WhenStringsEqualsLenghtWithUpperLetter_ThenTrue()
+        {
+            //arrange
+            string firstString = "nono";
+            string secondString = "NoNo";
+
+            //act
+            bool output = true;
+
+            //assert
+            Assert.AreEqual(output, Anagram.AnagramChecker(firstString, secondString));
+        }
+
+        [TestCase]
+        public void TestAnagramMaker_WhenOneStringEmpty_ThenFalse()
+        {
+            //arrange
+            string firstString = "nono";
+            string secondString = "";
+
+            //act
+            bool output = false;
+
+            //assert
+            Assert.AreEqual(output, Anagram.AnagramChecker(firstString, secondString));
+        }
+    }
+}
