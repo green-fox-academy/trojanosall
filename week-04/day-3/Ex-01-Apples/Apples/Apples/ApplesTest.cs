0f9d1d37e2ca7b3444d6e0b396222189016afa4d
Lorant Lajos <trojanosall@gmail.com>
2017-09-27 13:17:34 +0200

    Week 04 - Day 03 - Exercise 01 - Apples - Done

---
+++ week-04/day-3/Ex-01-Apples/Apples/Apples/ApplesTest.cs
@@ -0,0 +1,44 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Apples
+{
+    [TestFixture]
+
+    public class ApplesTest
+    {
+        Apples apples = new Apples();
+
+        [TestCase]
+        public void TestApple_WhenApple_ThenApple()
+        {
+            string output = "apple";
+            Assert.AreEqual(output, apples.GetApple());
+        }
+
+        [TestCase]
+        public void TestApple_WhenApples_ThenFailedTest()
+        {
+            string output = "apples";
+            Assert.AreEqual(output, apples.GetApple());            
+        }
+
+        [TestCase]
+        public void TestApple_WhenApple_ThenFailedTest2()
+        {
+            string output = "Apple";
+            Assert.AreEqual(output, apples.GetApple());
+        }
+
+        [TestCase]
+        public void TestApple_WhenNothing_ThenFailedTest3()
+        {
+            string output = "";
+            Assert.AreEqual(output, apples.GetApple());
+        }
+    }
+}
