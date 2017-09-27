7276313f175d7d704242b9f1392f96beb22b3b0b
Lorant Lajos <trojanosall@gmail.com>
2017-09-28 00:29:37 +0200

    Week 04 - Day 03 - Example 5 - Fibonacci - Test - Done

---
+++ week-04/day-3/Ex-05-Fibonacci/FibonacciMaker/FibonacciMaker/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FibonacciMaker
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+
+            // Fibonacci
+
+            // Write a function that computes a member of the fibonacci sequence by a given index
+            // Create tests that covers all types of input(like in the previous workshop exercise)
+
+            Console.WriteLine("I calculate Fibonacci number. Which Fibonacci number would you like to know?");
+            int n = int.Parse(Console.ReadLine());
+
+            Console.WriteLine(Fibonacci(n));
+
+            Console.ReadKey();
+
+        }
+
+        public static int Fibonacci(int n)
+        {
+            int a = 0;
+            int b = 1;
+
+            // In N steps compute Fibonacci sequence iteratively.
+            for (int i = 0; i < n; i++)
+            {
+                int temp = a;
+                a = b;
+                b = temp + b;
+            }
+            return a;
+        }
+
+    }
+}
---
+++ week-04/day-3/Ex-05-Fibonacci/FibonacciMaker/FibonacciMakerTest/Class1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FibonacciMaker;
+using NUnit.Framework;
+
+namespace FibonacciMakerTest
+{
+    [TestFixture]
+
+    public class Class1
+    {       
+        [Test]
+        public void TestFibonacci_WhenGetNumber_ThenGoodOutput()
+        {
+            //arrange
+            int n = 11;
+
+            //act
+            int output = 89;
+
+            //assert
+            Assert.AreEqual(output, Program.Fibonacci(n));
+        }
+
+        [Test]
+        public void TestFibonacci_WhenGetNulla_ThenNullaOutput()
+        {
+            //arrange
+            int n = 0;
+
+            //act
+            int output = 0;
+
+            //assert
+            Assert.AreEqual(output, Program.Fibonacci(n));
+        }
+
+    }
+}
