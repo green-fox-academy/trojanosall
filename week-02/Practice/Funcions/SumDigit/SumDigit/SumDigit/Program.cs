9eb343d90ceb0b363b4d4da172f9de6174f82d52
Lorant Lajos <trojanosall@gmail.com>
2017-10-01 01:13:29 +0200

    Practice - Funcion - SumDigit

---
+++ week-02/Practice/Funcions/SumDigit/SumDigit/SumDigit/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SumDigit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //  Write a program in C# Sharp to create a function to calculate the sum of the individual digits of a given number.
+
+            Console.WriteLine("Give me a number and I will sum of its digit: ");
+            int UserInput = int.Parse(Console.ReadLine());
+
+            SumDigit(UserInput);
+
+            Console.WriteLine("Sum of digits is {0}", SumDigit(UserInput));
+
+            Console.ReadKey();
+
+        }
+
+        private static int SumDigit(int userNumber)
+        {
+            int sum = 0;
+
+            while (userNumber != 0)
+            {
+                sum += userNumber % 10;
+                userNumber /= 10;
+            }
+            return sum;
+        }
+    }
+}
