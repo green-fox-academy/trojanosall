fc8c90038b8e748a66289e451916b8e3c44a4df3
Lorant Lajos <trojanosall@gmail.com>
2017-10-01 12:05:50 +0200

    Week 04 - Day 01 - Exercise 04 - Dice - Done - again - with some comment

---
+++ week-04/day-1/Ex-04-Dice/Dice Again/Dice_v2/Dice_v2/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+
+namespace Dice
+{
+    public class Dice
+    {
+        //    You have a Dice class which has 6 dice
+        //    You can roll all of them with Roll()
+        //    Check the current rolled numbers with GetCurrent()
+        //    You can reroll with Reroll()
+        //    Your task is to get where all dice is a 6
+        static Random RandomValue;
+        static int[] Dices = new int[6];
+
+        public int[] Roll()
+        {
+            for (int i = 0; i < Dices.Length; i++)
+            {
+                Dices[i] = RandomValue.Next(1, 6);
+            }
+            return Dices;
+        }
+
+        public int[] GetCurrent()
+        {
+            return Dices;
+        }
+
+        public int GetCurrent(int i)
+        {
+            return Dices[i];
+        }
+
+        public void Reroll()
+        {
+            for (int i = 0; i < Dices.Length; i++)
+            {
+                Dices[i] = RandomValue.Next(1, 6);
+            }
+        }
+
+        public void Reroll(int k)
+        {
+            Dices[k] = RandomValue.Next(1, 7);
+        }
+
+        public static void Main(string[] args)
+        {
+            RandomValue = new Random();
+            Dice myDice = new Dice();
+            
+
+            int sum = 0;
+
+            while (sum < 36)                            // 36 lesz a kockak osszerteke, ha mindegyik hatost mutat. Vagzis addig kell forgatni a kockakaat, amig mindegyik hatos nem lesz.
+            {
+                sum = 0;
+
+                for (int i = 0; i < 6; i++)
+                {
+                    if (myDice.GetCurrent(i) < 6)
+                    {
+                        myDice.Reroll(i);
+                    }
+                    Console.Write(Dices[i] + " ");
+                    sum += Dices[i];
+                }
+                Console.WriteLine();
+            }
+            myDice.GetCurrent();
+            Console.WriteLine("Vége van");
+            Console.ReadLine();
+        }
+    }
+}
