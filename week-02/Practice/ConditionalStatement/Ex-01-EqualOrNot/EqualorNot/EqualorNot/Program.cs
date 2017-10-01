1bc51070cf0a0bd8b7038a41cc60057d74c55ea1
Lorant Lajos <trojanosall@gmail.com>
2017-10-02 00:35:04 +0200

    Practice - Conditional Statement - Exercise 01 - Equal or Not - Done

---
+++ week-02/Practice/ConditionalStatement/Ex-01-EqualOrNot/EqualorNot/EqualorNot/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EqualorNot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Write a C# Sharp program to accept two integers and check whether they are equal or not.
+
+            Console.WriteLine("Give me the first number: ");
+            int firstUserInput = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Give me the second number: ");
+            int secondUserInput = int.Parse(Console.ReadLine());
+
+            if (firstUserInput == secondUserInput)
+            {
+                Console.WriteLine("They are equal.");
+            }
+            else
+            {
+                Console.WriteLine("They are not equal.");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
