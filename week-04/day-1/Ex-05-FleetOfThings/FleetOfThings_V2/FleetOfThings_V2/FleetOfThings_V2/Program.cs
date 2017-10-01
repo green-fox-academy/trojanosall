acb9afa8fad0a32cc83622e2d83e87baa92bf5f7
Lorant Lajos <trojanosall@gmail.com>
2017-10-01 15:03:30 +0200

    Week 04 - Day 01 - Exercise 05 v2 - Fleet Of Things - Done without comment

---
+++ week-04/day-1/Ex-05-FleetOfThings/FleetOfThings_V2/FleetOfThings_V2/FleetOfThings_V2/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FleetOfThings_V2
+{
+    public class FleetOfThings
+    {
+        public static void Main(string[] args)
+        {
+            // Create a fleet of things to have this output:
+            // 1. [ ] Get milk
+            // 2. [ ] Remove the obstacles
+            // 3. [x] Stand up
+            // 4. [x] Eat lunch
+            // Hint: You have to create a Print method also
+
+            var fleet = new Fleet();
+
+            var thing1 = new Thing("Get milk");
+            var thing2 = new Thing("Remove the obstacles");
+
+            var thing3 = new Thing("Stand up");
+            thing3.Complete();
+
+            var thing4 = new Thing("Eat lunch");
+            thing4.Complete();
+
+            fleet.Add(thing1);
+            fleet.Add(thing2);
+            fleet.Add(thing3);
+            fleet.Add(thing4);
+
+            fleet.Print();
+
+            Console.ReadKey();
+
+        }
+    }
+}
