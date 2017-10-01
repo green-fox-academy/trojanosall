60bf1bdd82790fa4cda0c585f19cfb5eaa48575c
Lorant Lajos <trojanosall@gmail.com>
2017-10-01 18:08:02 +0200

    Week 04 - Day 01 - Exercise 06 - Animal V2- Done - without comment

---
+++ week-04/day-1/Ex-06-Animal/Animal_V2/Animal_V2/Animal_V2/Animal.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Animal_V2
+{
+    class Animal
+    {
+        public string AnaimalType;
+        private int _thirst = 50;
+        private int _hunger = 50;
+
+        public Animal(string animalType, int thirst, int hunger)
+        {
+            this.AnaimalType = animalType;
+            this._thirst = thirst;
+            this._hunger = hunger;
+        }
+
+        public void Eat()
+        {
+            this._hunger--;
+        }
+
+        public void Drink()
+        {
+            this._thirst--;
+        }
+
+        public void Play()
+        {
+            this._hunger++;
+            this._thirst++;
+        }
+
+        public int GetHunger()
+        {
+            return this._hunger;
+        }
+
+        public int GetThirst()
+        {
+            return this._thirst;
+        }
+
+    }
+}
