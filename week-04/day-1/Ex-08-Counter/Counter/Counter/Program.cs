7a88325ca9330acd28994af026ac75fed96cefa4
Lorant Lajos <trojanosall@gmail.com>
2017-10-01 18:46:45 +0200

    Week 04 -Day 01 - Exercise 08 - Counter - Start to work

---
+++ week-04/day-1/Ex-08-Counter/Counter/Counter/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Counter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // Counter
+
+            // Create Counter class
+            // which has an integer property
+            // when creating it should have a default value 0 or we can specify it when creating
+            // we can Add(number) to this counter another whole number
+            // or we can Add() without parameters just increasing the counter's value by one
+            // and we can Get() the current value as string
+            // also we can Reset() the value to the initial value
+
+        }
+    }
+}
