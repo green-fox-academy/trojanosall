257be8ccd61c7af91b79edd2fb69049ba9f24d39
Lorant Lajos <trojanosall@gmail.com>
2017-10-01 18:12:55 +0200

    Week 04 - Day 07 - Exercise 07 - Sharpie - Start to work

---
+++ week-04/day-1/Ex-07-Sharpie/Sharpie/Sharpie/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sharpie
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // Sharpie
+
+            // Create Sharpie class
+            // We should know about each sharpie their Color(which should be a string), Width(which will be a floating point number), InkAmount(another floating point number)
+            // When creating one, we need to specify the Color and the Width
+            // Every sharpie is created with a default 100 as InkAmount
+            // We can Use() the sharpie objects
+            // which decreases inkAmount
+
+        }
+    }
+}
