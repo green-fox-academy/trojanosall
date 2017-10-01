16d2b5cf7695bdc20efbc05e4b14f0fe15ec7516
Lorant Lajos <trojanosall@gmail.com>
2017-10-01 17:34:46 +0200

    Week 04 - Day 01 - Exercise - 06 - Animal - Done with comment

---
+++ week-04/day-1/Ex-06-Animal/Animal/Animal/Animal.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Animal
+{
+    class Animal
+    {
+        public string NameOfAnimal;                             // Field-ek meghatarozasa
+        private int _hunger = 50;                               // hunger es thirst 50-re allitjuk a feladat leiras szerint, mivel private az access ezet also vonas es camel case a kovetendo.
+        private int _thirst = 50;
+
+
+        public Animal(string name, int hunger, int thirst)      // Constructor
+        {
+            NameOfAnimal = name;
+            _hunger = hunger;
+            _thirst = thirst;
+        }
+
+        public void Eat()                                       // Method
+        {
+            this._hunger--;
+        }
+
+        public void Drink()                                     // Method
+        {
+            this._thirst--;
+        }
+
+        public void Play()                                      // Method
+        {
+            this._hunger++;
+            this._thirst++;
+        }
+
+        public int GetHunger()                                  // Mivel a a thrist es a hunger is private access tipusu, ezert a main-ben nem latszana, ha nem get-elnenk.
+        {
+            return this._hunger;
+        }
+
+        public int GetThirst()                                  // Mivel a a thrist es a hunger is private access tipusu, ezert a main-ben nem latszana, ha nem get-elnenk.
+        {
+            return this._thirst;
+        }
+    }
+}
---
+++ week-04/day-1/Ex-06-Animal/Animal/Animal/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Animal
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Every animal has a hunger value, which is a whole number
+            // Every animal has a thirst value, which is a whole number
+            // when creating a new animal object these values are created with the default 50 value
+            // Every animal can eat() which decreases their hunger by one
+            // Every animal can drink() which decreases their thirst by one
+            // Every animal can play() which increases both by one
+
+            var dog = new Animal("Dog", 50, 50);
+            
+            dog.Drink();
+            dog.Eat();
+            dog.Play();
+
+            Console.WriteLine(dog.GetThirst());
+            Console.WriteLine(dog.GetHunger());
+
+            var cat = new Animal("Cat", 50, 50);
+            
+            dog.Play();
+
+            Console.WriteLine(dog.GetThirst());
+            Console.WriteLine(dog.GetHunger());
+
+
+            Console.ReadKey();
+
+
+
+        }
+    }
+}
