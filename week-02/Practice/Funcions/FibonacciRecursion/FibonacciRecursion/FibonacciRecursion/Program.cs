83a07fbee742f28b12dee4d853a33801bb66e94a
Lorant Lajos <trojanosall@gmail.com>
2017-10-02 00:24:30 +0200

    Practice - Funcion - Fibonacci Recursion

---
+++ week-02/Practice/Funcions/FibonacciRecursion/FibonacciRecursion/FibonacciRecursion/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FibonacciRecursion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // Write a program in C# Sharp to create a recursive function to calculate the Fibonacci number of a specific term.
+
+            Console.WriteLine("Which Fibonacci number would you like to know? I will calculate it for you: ");
+            int userinput = int.Parse(Console.ReadLine());
+
+            Fibonacci(userinput);
+
+            Console.WriteLine("The {0}. Fibonacci number is {1}", userinput, Fibonacci(userinput)); 
+
+            Console.ReadKey();
+
+        }
+
+        private static int Fibonacci(int userinput)
+        {
+            if(userinput <= 1)
+            {
+                return 1;
+            }
+            else
+            {
+                return Fibonacci(userinput - 2) + Fibonacci(userinput - 1);
+            }
+        }
+    }
+}
