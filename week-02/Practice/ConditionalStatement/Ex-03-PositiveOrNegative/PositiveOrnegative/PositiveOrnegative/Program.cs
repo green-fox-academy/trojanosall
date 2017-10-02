e206342ed4d5b6284393c38fd503bf991b2fe6d7
Lorant Lajos <trojanosall@gmail.com>
2017-10-02 23:17:38 +0200

    Practice - Conditionals - Ex 03 - Positive or Negative

---
+++ week-02/Practice/ConditionalStatement/Ex-03-PositiveOrNegative/PositiveOrnegative/PositiveOrnegative/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PositiveOrnegative
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Please give me a number and i tell you it positive even or negative: ");
+            int userInput = int.Parse(Console.ReadLine());
+
+            if (userInput < 0)
+            {
+                Console.WriteLine("The {0} is negative.", userInput);
+            }
+            else
+            {
+                Console.WriteLine("The {0} is positive.", userInput);
+            }
+
+            Console.ReadKey();
+
+        }
+    }
+}
