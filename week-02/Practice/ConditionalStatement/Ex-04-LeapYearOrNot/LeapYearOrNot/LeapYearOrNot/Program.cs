940ccc8fb80afb12d3fbe9229235b5b582fb71ec
Lorant Lajos <trojanosall@gmail.com>
2017-10-02 23:24:20 +0200

    Practice - Conditionals - Ex 04 - Leap year or not

---
+++ week-02/Practice/ConditionalStatement/Ex-04-LeapYearOrNot/LeapYearOrNot/LeapYearOrNot/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeapYearOrNot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please give me a number and i tell you it leap year not: ");
+            int userInput = int.Parse(Console.ReadLine());
+
+            if (DateTime.IsLeapYear(userInput))
+            {
+                Console.WriteLine("The {0} is leap year.", userInput);
+            }
+            else
+            {
+                Console.WriteLine("The {0} is not leap year.", userInput);
+            }
+
+            Console.ReadKey();
+
+        }
+    }
+}
