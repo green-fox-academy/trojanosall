11c56d34a52174769e283c5c054f56183d111c75
Lorant Lajos <trojanosall@gmail.com>
2017-10-02 23:14:21 +0200

    Practice - Conditionals - Ex 02 - Even or Odd

---
+++ week-02/Practice/ConditionalStatement/Ex-02-EvenOrOdd/EvenOrOdd/EvenOrOdd/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EvenOrOdd
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please give me a number and i tell you it is even or Odd: ");
+            int userInput = int.Parse(Console.ReadLine());
+
+            if ( userInput % 2 != 0)
+            {
+                Console.WriteLine("The {0} is odd.", userInput);
+            }
+            else
+            {
+                Console.WriteLine("The {0} is even.", userInput);
+            }
+
+            Console.ReadKey();
+
+        }
+    }
+}
