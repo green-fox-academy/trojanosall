2c53a13cbbb7b83d0a5e3343d8392b6d0c069d3e
Lorant Lajos <trojanosall@gmail.com>
2017-10-02 23:46:15 +0200

    Practice - Conditionals - Ex 06 - M and N number and their relations

---
+++ week-02/Practice/ConditionalStatement/Ex-06-NvsM_Number/NvsM_Number/NvsM_Number/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NvsM_Number
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Input the value of m: ");
+            int m = int.Parse(Console.ReadLine());
+            int n;
+
+            if (m > 0)
+            {
+                n = 1;
+                Console.WriteLine("If the value of m is larger than 0 the value of n is 1. The value of m is {0}. So the value of n is: {1}.", m, n);
+            }
+            else if (m == 0)
+            {
+                n = 0;
+                Console.WriteLine("If the value of m is equal 0 the value of n is 0. The value of m is {0}. So the value of n is: {1}.", m, n); 
+            }
+            else
+            {
+                n = -1;
+                Console.WriteLine("If the value of m is less than 0 the value of n is -1. The value of m is {0}. So the value of n is: {1}.", m, n);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
