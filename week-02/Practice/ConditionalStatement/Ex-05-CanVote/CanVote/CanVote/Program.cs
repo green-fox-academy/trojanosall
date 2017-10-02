02e5dadecd0eca377c44a437b42ea87917c91c42
Lorant Lajos <trojanosall@gmail.com>
2017-10-02 23:33:19 +0200

    Practice - Conditionals - Ex 05 - Can Vote or not

---
+++ week-02/Practice/ConditionalStatement/Ex-05-CanVote/CanVote/CanVote/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CanVote
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please give me your age and i tell you whether it is eligible for casting your own vote: ");
+            int userInput = int.Parse(Console.ReadLine());
+
+            if (userInput > 18)
+            {
+                Console.WriteLine("With age of {0} is eligible for casting your own vote.", userInput);
+            }
+            else
+            {
+                Console.WriteLine("With age of {0} is not eligible for casting your own vote.", userInput);
+            }
+
+            Console.ReadKey();
+
+        }
+    }
+}
