6d963852a8f2874bca00076272b8b5e866f05592
Lorant Lajos <trojanosall@gmail.com>
2017-10-05 16:36:30 +0200

    Week 03 - Day 07 - Exercise 06 - Copy File - Done

---
+++ week-03/day-2/exercise-06 -CopyFile/CopyFile/CopyFile/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace CopyFile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Write a function that reads all lines of a file and writes the read lines to an other file (a.k.a copies the file)
+            // It should take the filenames as parameters
+            // It should return a boolean that shows if the copy was successful
+
+            string pathSource = @"./my-file.txt";
+            string pathTarget = @"./my-file2.txt";
+
+            Console.WriteLine(CopyFile(pathSource, pathTarget));
+
+            Console.ReadKey();
+            
+        }
+
+        private static bool CopyFile(string pathSource, string pathTarget)
+        {
+            string text = File.ReadAllText(pathSource);
+
+            try
+            {
+                File.WriteAllText(pathTarget, text);
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Exception");
+                return false;
+            }
+            return true;
+        }
+    }
+}
