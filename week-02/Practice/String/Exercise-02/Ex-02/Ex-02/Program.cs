8093203eedc5848b7dd746c0963487609cc0ff4b
Lorant Lajos <trojanosall@gmail.com>
2017-10-07 11:34:09 +0200

    Practice exercises - 02

---
+++ week-02/Practice/String/Exercise-02/Ex-02/Ex-02/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex_02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string word = " w3resource.com";
+
+            Console.WriteLine("The given word is the following: {0}", word);
+            Console.WriteLine("\nThe lenght of this word is {0}", word.Length);
+
+            Console.ReadLine();
+
+        }
+    }
+}
