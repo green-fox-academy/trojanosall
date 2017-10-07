dcd7100e447efaa442ebac27fe05acc6061a9e60
Lorant Lajos <trojanosall@gmail.com>
2017-10-07 11:29:07 +0200

    Preparatation for basic exam and other exercises

---
+++ week-02/Practice/String/Exercise-01/Ex-01/Ex-01/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex_01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string userInput;
+
+            Console.WriteLine("\n\nAccept a string from keyboard :\n");
+            Console.WriteLine("--------------------------------------");
+            Console.WriteLine("Input the string: ");
+            userInput = Console.ReadLine();
+            Console.WriteLine("The string you entered: {0}\n", userInput);
+            Console.ReadKey();
+        }
+    }
+}
