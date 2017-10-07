61b9be8b1a5d12a43f7344a908ae5354c4143617
Lorant Lajos <trojanosall@gmail.com>
2017-10-07 23:44:05 +0200

    Practice exercises - 05

---
+++ week-02/Practice/String/Exercise-05/Ex-05/Ex-05/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex_05
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\nThis program will count the total number of word in a string.\n");
+            Console.WriteLine("----------------------------------------------\n");
+            Console.WriteLine("Please give me a string: ");
+            string userInput = Console.ReadLine();
+            int counter = 1;
+
+            for (int i = 0; i < userInput.Length; i++)
+            {
+                if (userInput[i]==' ' || userInput[i]=='\n' || userInput[i]=='\t')
+                {
+                    counter++;
+                }
+            }
+
+            Console.WriteLine("The total number of word in the string above is {0}.", counter);
+
+            Console.ReadKey();
+        }
+    }
+}
