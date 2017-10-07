61271a61819bbc606a51e8ff594fa06bc86f8d15
Lorant Lajos <trojanosall@gmail.com>
2017-10-07 11:52:37 +0200

    Practice exercises - 03

---
+++ week-02/Practice/String/Exercise-03/Ex-03/Ex-03/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex_03
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string word = "w3resource.com";
+            int counter = 0;
+
+            Console.WriteLine("The given word which will be separated is {0}\n", word);
+
+            Console.WriteLine("After the separation:\n");
+
+            while (counter < word.Length)
+            {
+                Console.Write(word[counter]);
+                Console.Write(" ");
+                counter++;
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
