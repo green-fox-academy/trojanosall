c23b02c866449df707a0738ed2facee010010b40
Lorant Lajos <trojanosall@gmail.com>
2017-10-09 23:18:34 +0200

    Practice - Exercise 14

---
+++ week-02/Practice/String/Exercise-14/Ex-14/Ex-14/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex_14
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string userInput, testWord;
+            bool check;
+
+            Console.WriteLine("Input the string : ");
+            userInput = Console.ReadLine();
+
+            Console.WriteLine("Input the substring to search : ");
+            testWord = Console.ReadLine();
+
+            check = userInput.Contains(testWord);
+
+            if (check)
+            {
+                Console.WriteLine("The substring exists in the string");
+            }
+            else
+            {
+                Console.WriteLine("The substring does not exist in the string");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
