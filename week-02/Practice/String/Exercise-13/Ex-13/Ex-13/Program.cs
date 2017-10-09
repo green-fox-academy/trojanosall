44fad942419edcb3e4c57d298e87a8f3795901ea
Lorant Lajos <trojanosall@gmail.com>
2017-10-09 23:09:16 +0200

    Practice - Exercise 13

---
+++ week-02/Practice/String/Exercise-13/Ex-13/Ex-13/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex_13
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string userInput, userInputAfterSubstring;
+            int positionToStartExtraction, lenghtOfSubstring;
+
+            Console.WriteLine("Input the string : ");
+            userInput = Console.ReadLine();
+
+            Console.WriteLine("Input the position to start extraction :");
+            positionToStartExtraction = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Input the length of substring :");
+            lenghtOfSubstring = int.Parse(Console.ReadLine());
+
+            userInputAfterSubstring = userInput.Substring(positionToStartExtraction, lenghtOfSubstring);
+
+            Console.WriteLine($"\nThe substring retrieve from the string is : {userInputAfterSubstring}");
+
+            Console.ReadKey();
+
+        }
+    }
+}
