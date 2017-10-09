e511161993290651cdc319492bb2a2e2995dcd09
Lorant Lajos <trojanosall@gmail.com>
2017-10-09 22:32:35 +0200

    Practice - Exercise 11

---
+++ week-02/Practice/String/Exercise-11/Ex-11/Ex-11/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex_11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string userInput;
+
+            Console.WriteLine("Input the string :");
+            userInput = Console.ReadLine();
+
+            char[] userInputChar = userInput.ToCharArray();
+            Array.Sort(userInputChar, StringComparer.InvariantCulture);
+
+            Array.ForEach(userInputChar, x => Console.Write(x));
+
+            Console.ReadKey();
+
+        }
+    }
+}
