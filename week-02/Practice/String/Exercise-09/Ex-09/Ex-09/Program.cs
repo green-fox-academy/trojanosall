9da79f06eda7a469a16969a60881c28e6424d206
Lorant Lajos <trojanosall@gmail.com>
2017-10-09 17:29:41 +0200

    Practice - Exercise 09

--- week-02/Practice/String/Exercise-09/Ex-09/Ex-09/Program.cs
+++ week-02/Practice/String/Exercise-09/Ex-09/Ex-09/Program.cs
@@ -10,6 +10,29 @@ namespace Ex_09
     {
         static void Main(string[] args)
         {
+            string userInput;
+            int vowelNumber = 0, consonantNumber = 0, userInputLenght, i = 0;
+
+            Console.WriteLine("Input the string : ");
+            userInput = Console.ReadLine();
+
+            userInputLenght = userInput.Length;
+
+            while (i < userInputLenght)
+            {
+                if (userInput[i] == 'a' || userInput[i] == 'e' || userInput[i] == 'i' || userInput[i] == 'o' || userInput[i] == 'u' || userInput[i] == 'A' || userInput[i] == 'E' || userInput[i] == 'I' || userInput[i] == 'O' || userInput[i] == 'U')
+                {
+                    vowelNumber++;
+                }
+                else if ((userInput[i] >= 'a' && userInput[i] <= 'z') || (userInput[i] >= 'A' && userInput[i] <= 'Z'))
+                {
+                    consonantNumber++;
+                }
+                i++;
+            }
+
+            Console.WriteLine($"The total number of vowel in the string is : {vowelNumber}\nThe total number of consonant in the string is : {consonantNumber}");
+            Console.ReadKey();
         }
     }
 }
