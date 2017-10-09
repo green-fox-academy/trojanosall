c5740cb1021b8bf0c62d2d470310b2a051d30426
Lorant Lajos <trojanosall@gmail.com>
2017-10-09 16:54:49 +0200

    Practice - Exercise 08 - v1

---
+++ week-02/Practice/String/Exercise-08/Ex-08/Ex-08/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex_08
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string userInput, copiedString;
+            int userInputLenght = 0;
+
+            Console.WriteLine("The First string is : \n\n");
+            userInput = Console.ReadLine();
+
+            userInputLenght = userInput.Length;
+
+            copiedString = userInput;
+
+            Console.WriteLine($"\n\nThe First string is : {userInput}");
+            Console.WriteLine($"The Second string is : {copiedString}");
+
+            Console.WriteLine($"Number of characters copied : {userInputLenght}");
+
+            Console.ReadKey();
+        }
+    }
+}
