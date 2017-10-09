b2f3aeb00a9ba237a9f026912352cf8d1ef3ad93
Lorant Lajos <trojanosall@gmail.com>
2017-10-09 23:52:54 +0200

    Practice - Exercise 16

---
+++ week-02/Practice/String/Exercise-16/Ex-16/Ex-16/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex_16
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string userName, passWord;
+            int counter = 0;
+
+            Console.Write("\n\nCheck username and password :\n");
+            Console.Write("N.B. : Default user name and password is :abcd and 1234\n");
+            Console.Write("------------------------------------------------------\n");
+
+            do
+            {
+                Console.Write("Input the username: ");
+                userName = Console.ReadLine();
+
+                Console.Write("input the password: ");
+                passWord = Console.ReadLine();
+
+                if (userName != "abcd" || passWord != "1234")
+                {
+                    counter++;
+                }
+                else
+                {
+                    counter = 1;
+                }
+
+            }
+            while ((userName != "abcd" || passWord != "1234") && counter != 3);
+
+            if (counter==3)
+            {
+                Console.Write("\nLogin attemp three or more times. Try later!\n\n");
+            }
+            else
+            {
+                Console.Write("\nThe password entered successfully!\n\n");
+            }
+
+            Console.ReadKey();
+
+        }
+    }
+}
