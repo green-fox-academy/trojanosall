17ad0114915e315c23f3dae54911ab8b8e871061
Lorant Lajos <trojanosall@gmail.com>
2017-10-09 23:39:51 +0200

    Practice - Exercise 15

---
+++ week-02/Practice/String/Exercise-15/Ex-15/Ex-15/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex_15
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string userInput;
+            int lenght;
+            char ch;
+
+            Console.WriteLine("Input the string : ");
+            userInput = Console.ReadLine();
+
+            char[] userInputChar = userInput.ToCharArray();
+            lenght = userInputChar.Length;
+
+
+            Console.WriteLine("After the conversation : ");
+
+            for (int i = 0; i < lenght; i++)
+            {
+                ch = userInputChar[i];
+                if (Char.IsLower(ch))
+                {
+                    Console.Write(Char.ToUpper(ch));
+                }
+                else
+                {
+                    Console.Write(Char.ToLower(ch));
+                }
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
