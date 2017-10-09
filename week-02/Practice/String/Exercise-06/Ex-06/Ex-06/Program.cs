6c810de5f6af4343cc3f0e55abf63d1e240f8d6b
Lorant Lajos <trojanosall@gmail.com>
2017-10-09 16:14:24 +0200

    Practice - Exercise 06

---
+++ week-02/Practice/String/Exercise-06/Ex-06/Ex-06/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex_06
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string firstString, secondString;
+            int firstStringLenght, secondStringLenght, checkCounter = 0;
+
+            Console.WriteLine("\nInput the 1st string: ");
+            firstString = Console.ReadLine();
+
+            Console.WriteLine("\nInput the 2nd string: ");
+            secondString = Console.ReadLine();
+
+            firstStringLenght = firstString.Length;
+            secondStringLenght = secondString.Length;
+
+
+            if (firstStringLenght == secondStringLenght)
+            {
+                for (int i = 0; i < firstStringLenght; i++)
+                {
+                    if (firstString[i] == secondString[i])
+                    {
+                        checkCounter++;
+                    }
+                    
+                } 
+            }
+
+            if (checkCounter == firstStringLenght)
+            {
+                Console.WriteLine("The two string are equal.");
+            }
+            else
+            {
+                Console.WriteLine("The two string are not equal.");
+            }
+
+            Console.ReadKey();
+
+        }
+    }
+}
