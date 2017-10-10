3a139fdc00e3e9e4385fef5bb644f4d649bfca20
Lorant Lajos <trojanosall@gmail.com>
2017-10-10 22:10:34 +0200

    Week 03 - Day 02 - Exercise 03 - Count Lines Again - count then lines

---
+++ week-03/day-2/exercise-03-CountLines/CountLinesAgain/CountLinesAgain/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace CountLinesAgain
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string fileName = @"my-file.txt";
+            int counter;
+
+            using (StreamReader str = File.OpenText(fileName))
+            {
+                string content;
+                counter = 0;
+
+                while ((content = str.ReadLine()) != null)
+                {
+                    Console.WriteLine(content);
+                    counter++;
+                }
+
+                Console.WriteLine($"\nThe number of lines is {counter}");
+            }
+
+            Console.ReadKey();
+
+        }
+    }
+}
