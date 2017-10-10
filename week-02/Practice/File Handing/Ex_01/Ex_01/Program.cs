42ab12e19f2b282ffb380d8e44c141fe5f40d4a5
Lorant Lajos <trojanosall@gmail.com>
2017-10-10 13:49:26 +0200

    Practice - File Handing - Exercise 01

---
+++ week-02/Practice/File Handing/Ex_01/Ex_01/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Ex_01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string fileName = @"mytest.txt";
+
+            try
+            {
+                Console.WriteLine("\n\nCreate a file named mytest.txt in the disk :\n");
+                Console.WriteLine("------------------------------------------------------");
+
+                using (FileStream fs = File.Create(fileName))
+                {
+                    Console.WriteLine("A file created with name mytest.txt\n\n");
+                }
+            }
+            catch (Exception myException)
+            {
+                Console.WriteLine(myException.ToString());
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
