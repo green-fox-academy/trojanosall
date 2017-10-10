c9bafb75448bcb94e198eeef8ab1fb1ab09ce544
Lorant Lajos <trojanosall@gmail.com>
2017-10-10 22:35:43 +0200

    Week 03 - Day 02 - Exercise 04 - Write Single Line

---
+++ week-03/day-2/exercise-04-WriteSingleLine/WriteSingleLineAgain/WriteSingleLineAgain/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace WriteSingleLineAgain
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string fileName = @"my-file.txt";
+
+            try
+            {
+                using (StreamWriter stw = File.CreateText(fileName))
+                {
+                    stw.WriteLine("Lajos Lorant");
+                }
+            }
+            catch (Exception MyExcept)
+            {
+                Console.WriteLine($"Unable to write file: {fileName}");
+            }
+
+            Console.ReadKey();
+
+        }
+    }
+}
