15030af439a21509f6bfeca7d66c927f518aae16
Lorant Lajos <trojanosall@gmail.com>
2017-10-10 23:02:32 +0200

    Week 03 - Day 02 - Exercise 06 - Copy File again

---
+++ week-03/day-2/exercise-06 -CopyFile/CopyFileAgain/CopyFileAgain/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace CopyFileAgain
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string pathSource = @"my-file.txt";
+            string pathTarget = @"my-file2.txt";
+
+            Console.WriteLine(CopyFile(pathSource, pathTarget));
+
+            Console.ReadKey();
+        }
+
+        private static bool CopyFile(string pathSource, string pathTarget)
+        {
+            string text = File.ReadAllText(pathSource);
+
+            try
+            {
+                File.WriteAllText(pathTarget, text);
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Exception");
+                return false;
+            }
+            return true;
+        }
+    }
+}
