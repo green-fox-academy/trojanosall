f3f61bf194c22286e783e212aaae9430092f4642
Lorant Lajos <trojanosall@gmail.com>
2017-10-10 22:51:15 +0200

    Week 03 - Day 02 - Exercise 05 - Write Multiple Lines Again

---
+++ week-03/day-2/exercise-05-WriteMultipleLines/WriteMultipleLinesAgain/WriteMultipleLinesAgain/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace WriteMultipleLinesAgain
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string fileName = @"myfile.txt";
+            string content = "apple";
+            int numberOfLines = 5;
+
+            WriteMultipleLines(fileName, content, numberOfLines);
+
+        }
+
+        public static void WriteMultipleLines(string fileName, string content, int numberOfLines)
+        {
+            using (StreamWriter stw = File.CreateText(fileName))
+            {
+                for (int i = 0; i < numberOfLines; i++)
+                {
+                    stw.WriteLine(content);
+                }
+            }
+        }
+    }
+}
