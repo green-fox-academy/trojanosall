c22ebb74e1c59a64cbe85a91bed32f333dc48512
Lorant Lajos <trojanosall@gmail.com>
2017-10-10 14:22:59 +0200

    Practice - File Handing - Exercise 02

---
+++ week-02/Practice/File Handing/Ex_02/Ex_02/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Ex_02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string fileName = @"mytest.txt";
+
+            Console.Write("\n\n Remove a file from the disk (at first create the file ) :\n");
+            Console.Write("--------------------------------------------------------------\n");
+
+            Console.WriteLine(File.Exists(fileName) ? "File exists." : "File does not exist.");
+
+            if (File.Exists(fileName))
+            {
+                File.Delete(fileName);
+                Console.WriteLine($"The file {fileName} deleted successfully");
+            }
+            else
+            {
+                Console.WriteLine($"File {fileName} does not exist.");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
