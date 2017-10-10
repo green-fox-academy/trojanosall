401db668e04fe366eb504058f54cab734a53c4d9
Lorant Lajos <trojanosall@gmail.com>
2017-10-10 15:00:35 +0200

    Practice - File Handing - Exercise 05

---
+++ week-02/Practice/File Handing/Ex_05/Ex_05/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Ex_05
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string fileName = @"mytest.txt";
+
+            try
+            {
+                if (File.Exists(fileName))
+                {
+                    File.Delete(fileName);
+                }
+                Console.Write("\n\n Create a file with text and read the file  :\n");
+                Console.Write("-------------------------------------------------\n");
+
+                using (StreamWriter stw = File.CreateText(fileName))
+                {
+                    stw.WriteLine(" Hello and Welcome");
+                    stw.WriteLine(" It is the first content");
+                    stw.WriteLine(" of the text file mytest.txt");
+                }
+
+                using (StreamReader str = File.OpenText(fileName))
+                {
+                    string content = "";
+                    Console.WriteLine($"Here is the content of the file {fileName}: ");
+                    while ((content=str.ReadLine()) != null)
+                    {
+                        Console.WriteLine(content);
+                    }
+                }
+
+            }
+            catch (Exception MyExcept)
+            {
+                Console.WriteLine(MyExcept.ToString());
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
