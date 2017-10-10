c3e51aa82ee1b2d9d0d0e09fad71ba4a4470fa02
Lorant Lajos <trojanosall@gmail.com>
2017-10-10 18:43:06 +0200

    Practice - File Handing - Exercise 11

---
+++ week-02/Practice/File Handing/Ex_11/Ex_11/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Ex_11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string fileName = @"mytest.txt";
+
+            try
+            {
+                if (File.Exists(fileName))
+                {
+                    File.Delete(fileName);
+                }
+
+                Console.Write("\n\n Read the first line from a file  :\n");
+                Console.Write("---------------------------------------\n");
+
+                using (StreamWriter stw = File.CreateText(fileName))
+                {
+                    stw.WriteLine(" test line 1");
+                    stw.WriteLine(" test line 2");
+                    stw.WriteLine(" test line 3");
+                }
+
+                using (StreamReader str = File.OpenText(fileName))
+                {
+                    string content;
+
+                    Console.WriteLine(" Here is the content of the file mytest.txt : ");
+
+                    while ((content = str.ReadLine()) != null)
+                    {
+                        Console.WriteLine(content);
+                    }
+                }
+
+                Console.WriteLine("\n The content of the first line of the file is :\n");
+
+                if (File.Exists(fileName))
+                {
+                    string[] fileArray = File.ReadAllLines(fileName);
+                    Console.WriteLine(fileArray[0]);
+                }
+            }
+            catch (Exception MyExcept)
+            {
+                Console.WriteLine(MyExcept.ToString());
+            }
+
+            Console.ReadKey();
+
+        }
+    }
+}
