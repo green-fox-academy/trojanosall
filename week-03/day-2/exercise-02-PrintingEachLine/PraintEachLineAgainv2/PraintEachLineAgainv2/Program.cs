abd98028f634609f7fc755a50fa3aa1f3d989636
Lorant Lajos <trojanosall@gmail.com>
2017-10-10 22:03:39 +0200

    Week 03 - Day 02 - Exercise 02 - Print Each Line Again v2

---
+++ week-03/day-2/exercise-02-PrintingEachLine/PraintEachLineAgainv2/PraintEachLineAgainv2/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace PraintEachLineAgainv2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string fileName = @"my-file.txt";
+
+            try
+            {
+                string content = File.ReadAllText(fileName);
+                Console.WriteLine(content);
+
+            }
+            catch (Exception MyExcept)
+            {
+                Console.WriteLine($"Unable to read file: {fileName}");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
