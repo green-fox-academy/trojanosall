9d6669f8d73a3c276f1fc24e09c01c35d7647382
Lorant Lajos <trojanosall@gmail.com>
2017-10-11 22:32:00 +0200

    Practice - OOP - Fleet Of Things again print list without completed signal

---
+++ week-04/Practice/FleetOfThigsAgain/FleetOfThigsAgain/Fleet.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FleetOfThigsAgain
+{
+    public class Fleet
+    {
+        private List<Thing> Things;
+
+        public Fleet()
+        {
+            Things = new List<Thing>();
+        }
+
+        public void Add(Thing thing)
+        {
+            Things.Add(thing);
+        }
+
+        public void Print()
+        {
+            foreach (Thing item in Things)
+            {
+                Console.WriteLine((item.IsComplete() ? "[X] " : "[ ] ") + item.GetName());
+            }
+        }
+    }
+}
---
+++ week-04/Practice/FleetOfThigsAgain/FleetOfThigsAgain/Thing.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FleetOfThigsAgain
+{
+    public class Thing
+    {
+        private string Name;
+        private bool Completed;
+
+        public Thing(string name)
+        {
+            this.Name = name;
+        }
+
+        public void Complete()
+        {
+            this.Completed = true;
+        }
+
+        public string GetName()
+        {
+            return this.Name;
+        }
+
+        public bool IsComplete()
+        {
+            return this.Completed;
+        }
+    }
+}
