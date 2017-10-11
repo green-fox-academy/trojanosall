bc195c01c6afed820066bf8b84c6d07d57b99c60
Lorant Lajos <trojanosall@gmail.com>
2017-10-11 22:12:35 +0200

    Practice - OOP - Dices again

---
+++ week-04/Practice/DiceAgain/DiceAgain/Dice.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DiceAgain
+{
+    public class Dice
+    {
+        Random rnd = new Random();
+
+        public int[] Dices = new int[6];
+
+        public int[] Roll()
+        {
+            for (int i = 0; i < Dices.Length; i++)
+            {
+                Dices[i] = rnd.Next(1, 6);
+            }
+            return Dices;
+        }
+
+        public int[] GetCurrent()
+        {
+            return Dices;
+        }
+
+        public int GetCurrent(int i)
+        {
+            return Dices[i];
+        }
+
+        public void Reroll()
+        {
+            for (int i = 0; i < Dices.Length; i++)
+            {
+                Dices[i] = rnd.Next(1, 6);
+            }
+        }
+
+        public void Reroll(int k)
+        {
+            Dices[k] = rnd.Next(1, 7);
+        }
+    }
+}
---
+++ week-04/Practice/DiceAgain/DiceAgain/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DiceAgain
+{
+    class Program
+    {
+        
+            public static void Main(string[] args)
+            {
+            
+            Dice myDice = new Dice();
+
+
+                int sum = 0;
+
+                while (sum < 36
+                ) 
+                {
+                    sum = 0;
+
+                    for (int i = 0; i < 6; i++)
+                    {
+                        if (myDice.GetCurrent(i) < 6)
+                        {
+                            myDice.Reroll(i);
+                        }
+                        Console.Write(myDice.Dices[i] + " ");
+                        sum += myDice.Dices[i];
+                    }
+                    Console.WriteLine();
+                }
+                myDice.GetCurrent();
+                Console.WriteLine("Vége van");
+                Console.ReadLine();
+            }
+        
+    }
+}
