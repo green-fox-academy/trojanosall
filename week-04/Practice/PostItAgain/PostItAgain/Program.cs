33f88e7f617c8713b570be75fd62fbe311fe14f5
Lorant Lajos <trojanosall@gmail.com>
2017-10-11 17:55:28 +0200

    Practice - OOP - Post It again

---
+++ week-04/Practice/PostItAgain/PostItAgain/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PostItAgain
+{
+    class Program
+    {
+        struct PostIt
+        {
+            public string BackgroundColor;
+            public string Text;
+            public string TextColor;
+
+            public PostIt(string backgroundColor, string text, string textColor)
+            {
+                this.BackgroundColor = backgroundColor;
+                this.Text = text;
+                this.TextColor = textColor;
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            PostIt postIt1 = new PostIt("Orange", "Idea 1", "Blue");
+            PostIt postIt2 = new PostIt("Pink", "Awesome", "Black");
+            PostIt postIt3 = new PostIt("Yellow", "Superb", "Green");
+
+            Console.WriteLine(postIt1.BackgroundColor + ", " + postIt1.Text + ", " + postIt1.TextColor);
+            Console.WriteLine(postIt2.BackgroundColor + ", " + postIt2.Text + ", " + postIt2.TextColor);
+            Console.WriteLine(postIt3.BackgroundColor + ", " + postIt3.Text + ", " + postIt3.TextColor);
+
+            Console.ReadKey();
+        }
+    }
+}
