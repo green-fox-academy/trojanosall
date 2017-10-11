758a221469d362aabc47fb197a521a9ec7a37119
Lorant Lajos <trojanosall@gmail.com>
2017-10-11 23:41:55 +0200

    Practice - OOP - Counter again

---
+++ week-04/Practice/CounterAgain/CounterAgain/Counter.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CounterAgain
+{
+    class Counter
+    {
+        private int Basenumber = 0;
+
+        public Counter()
+        {
+            this.Basenumber = 0;
+        }
+
+        public Counter(int basenumber)
+        {
+            this.Basenumber = basenumber;
+        }
+
+        public void Add(int number)
+        {
+            Basenumber += number;
+        }
+
+        public void Add()
+        {
+            Basenumber+=1;
+        }
+
+        public string GetValue()
+        {
+            return Basenumber.ToString();
+        }
+
+        public void Reset()
+        {
+            Basenumber = 0;
+        }
+
+    }
+}
---
+++ week-04/Practice/CounterAgain/CounterAgain/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CounterAgain
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var mycounter1 = new Counter();
+
+            mycounter1.Add(5);
+
+            Console.WriteLine(mycounter1.GetValue());
+
+            var mycounter2 = new Counter(5);
+
+            mycounter2.Add(5);
+
+            Console.WriteLine(mycounter2.GetValue());
+
+            var mycounter3 = new Counter(5);
+
+            mycounter3.Add();
+
+            Console.WriteLine(mycounter3.GetValue());
+
+            mycounter3.Reset();
+
+            Console.WriteLine(mycounter3.GetValue());
+
+            Console.ReadKey();
+
+        }
+    }
+}
