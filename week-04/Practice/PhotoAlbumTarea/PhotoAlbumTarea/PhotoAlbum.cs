88cf3a4f97783282e2368faf04c9414ce04cfffe
Lorant Lajos <trojanosall@gmail.com>
2017-10-11 16:28:06 +0200

    Practice - OOP - Photo Album

---
+++ week-04/Practice/PhotoAlbumTarea/PhotoAlbumTarea/PhotoAlbum.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PhotoAlbumTarea
+{
+    class PhotoAlbum
+    {
+        protected int numberOfPages;
+
+        public PhotoAlbum()
+        {
+            this.numberOfPages = 16;
+        }
+
+        public PhotoAlbum(int numberOfPages)
+        {
+            this.numberOfPages = numberOfPages;
+        }
+
+        public int GetNumberOfPages()
+        {
+            return numberOfPages;
+        }
+    }
+}
---
+++ week-04/Practice/PhotoAlbumTarea/PhotoAlbumTarea/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PhotoAlbumTarea
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var myAlbum1 = new PhotoAlbum();
+            Console.WriteLine(myAlbum1.GetNumberOfPages());
+
+            var  myAlbum2 = new PhotoAlbum(24);
+            Console.WriteLine(myAlbum2.GetNumberOfPages());
+
+            var myAlbum3 = new BigPhotoAlbum();
+            Console.WriteLine(myAlbum3.GetNumberOfPages());
+
+            Console.ReadKey();
+        }
+    }
+}
