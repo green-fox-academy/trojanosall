038640cd104fc081ebd1e15d5dfffa0eef8b0b47
Lorant Lajos <trojanosall@gmail.com>
2017-10-11 23:06:44 +0200

    Practice - OOP - Animal - refactored

---
+++ week-04/Practice/AnimalAgain/AnimalAgain/Animal.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AnimalAgain
+{
+    class Animal
+    {
+        private string AnimalType;
+        private int Hunger = 50;
+        private int Thirst = 50;
+
+        public Animal(string animalType)
+        {
+            this.AnimalType = animalType;
+        }
+
+        public void eat()
+        {
+            Hunger--;
+        }
+
+        public void drink()
+        {
+            Thirst--;
+        }
+
+        public void play()
+        {
+            Hunger++;
+            Thirst++;
+        }
+
+        public int GetHunger()
+        {
+            return this.Hunger;
+        }
+
+        public int GetThirst()
+        {
+            return this.Thirst;
+        }
+
+    }
+}
---
+++ week-04/Practice/AnimalAgain/AnimalAgain/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AnimalAgain
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var dog = new Animal("dog");
+
+            dog.drink();
+            dog.eat();
+            dog.eat();
+            dog.eat();
+            dog.play();
+
+            Console.WriteLine(dog.GetHunger());
+            Console.WriteLine(dog.GetThirst());
+
+            Console.ReadKey();
+        }
+    }
+}
