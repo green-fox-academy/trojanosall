32db362ce04b22c6c43e328723150a862c6e4f79
Lorant Lajos <trojanosall@gmail.com>
2017-10-11 23:25:26 +0200

    Practice - OOP - Sharpie again

---
+++ week-04/Practice/SharpieAgain/SharpieAgain/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SharpieAgain
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var mysharpie = new Sharpie("blue", 100);
+
+            mysharpie.Use();
+
+            Console.WriteLine(mysharpie.GetInkAmount());
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ week-04/Practice/SharpieAgain/SharpieAgain/Sharpie.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SharpieAgain
+{
+    public class Sharpie
+    {
+        private string Color;
+        private float Width;
+        private float InkAmount = 100;
+
+        public Sharpie(string color, float width)
+        {
+            this.Color = color;
+            this.Width = width;
+            this.InkAmount = 100;
+        }
+
+        public void Use()
+        {
+            InkAmount--;
+        }
+
+        public float GetInkAmount()
+        {
+            return this.InkAmount;
+        }
+    }
+}
