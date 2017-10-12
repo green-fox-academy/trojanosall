76dcb7f3352ca9b2d7f04ffc2ac50ea5f3ee30f2
Lorant Lajos <trojanosall@gmail.com>
2017-10-12 18:49:40 +0200

    Practice - OOP - Garden Again - Plants class created

---
+++ week-04/Practice/GardenAgain/GardenAgain/Plants.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GardenAgain
+{
+    class Plants
+    {
+        protected string TypeOfPlan;
+        protected string Color;
+        protected double CurrentWaterLevel;
+        protected double ExpectedWaterLevel;
+        protected double AbsorbPercentage;
+
+        public Plants(string color, string typeOfPlan)
+        {
+            this.Color = color;
+            this.TypeOfPlan = typeOfPlan;
+        }
+
+        public virtual void NeedsWaterOrNot()
+        {
+            if (CurrentWaterLevel >= ExpectedWaterLevel)
+            {
+                Console.WriteLine($"The {Color} {TypeOfPlan} does not need water");
+            }
+            else
+            {
+                Console.WriteLine($"The {Color} {TypeOfPlan} need water");
+            }
+        }
+
+        public virtual void Watering(int waterAmount)
+        {
+            if (CurrentWaterLevel < ExpectedWaterLevel)
+            {
+                CurrentWaterLevel = CurrentWaterLevel + waterAmount * AbsorbPercentage / 100;
+            }
+        }
+    }
+}
