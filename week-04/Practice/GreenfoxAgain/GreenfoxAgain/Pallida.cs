0578894d6cd534043da399d63e95797c522da633
Lorant Lajos <trojanosall@gmail.com>
2017-10-12 17:56:57 +0200

    Practice - OOP - Greenfox Again - Pallida class done

---
+++ week-04/Practice/GreenfoxAgain/GreenfoxAgain/Pallida.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GreenfoxAgain
+{
+    class Pallida
+    {
+        private string className;
+        private List<Student> students = new List<Student>();
+        private List<Mentor> mentors = new List<Mentor>();
+
+        public Pallida(string className)
+        {
+            this.className = className;
+            List<Student> students = null;
+            List<Mentor> mentors = null;
+        }
+
+        public void AddStudent(Student student)
+        {
+            students.Add(student);
+        }
+
+        public void AddMentor(Mentor mentor)
+        {
+            mentors.Add(mentor);
+        }
+
+        public void Info()
+        {
+            Console.WriteLine($"{className} class has {students.Count} students and {mentors.Count} mentors.");
+        }
+
+    }
+}
