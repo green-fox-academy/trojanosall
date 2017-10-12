cc90ff76676f609da3588088aa26cf04d4fbdbe2
Lorant Lajos <trojanosall@gmail.com>
2017-10-12 22:16:22 +0200

    Practice - Array - Ex-03

---
+++ week-02/Practice/Array/Ex-03/Ex-03/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex_03
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] myArray = new int[100];
+            int i, n, sum = 0;
+
+
+            Console.Write("\n\nFind sum of all elements of array:\n");
+            Console.Write("--------------------------------------\n");
+
+            Console.Write("Input the number of elements to be stored in the array :");
+            n = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Input {0} elements in the array :\n", n);
+            for (i = 0; i < n; i++)
+            {
+                Console.Write("element - {0} : ", i);
+                myArray[i] = Convert.ToInt32(Console.ReadLine());
+            }
+
+            for (i = 0; i < n; i++)
+            {
+                sum += myArray[i];
+            }
+
+            Console.Write("Sum of all elements stored in the array is : {0}\n\n", sum);
+
+            Console.ReadKey();
+        }
+    }
+}
