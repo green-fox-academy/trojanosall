7432a88ae5b21adcb37c75a10932e57d66225247
Lorant Lajos <trojanosall@gmail.com>
2017-10-12 10:41:21 +0200

    Practice - OO - Student and Teacher again

---
+++ week-04/Practice/TeacherAndStudentAgain/TeacherAndStudentAgain/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TeacherAndStudentAgain
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var myStudent = new Student();
+            var myTeacher = new Teacher();
+
+            myTeacher.teach();
+            myStudent.learn();
+
+            Console.WriteLine();
+
+            myStudent.question();
+            myTeacher.answer();
+
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ week-04/Practice/TeacherAndStudentAgain/TeacherAndStudentAgain/Student.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TeacherAndStudentAgain
+{
+    class Student
+    {
+
+        public void learn()
+        {
+            Console.WriteLine("Student learn");
+        }
+
+        public void question()
+        {
+            Console.WriteLine("Student ask teacher");
+        }
+
+    }
+}
