d32d8daadc439f2c42a8e512e2fd7c6147cb9a04
Lorant Lajos <trojanosall@gmail.com>
2017-10-12 21:54:51 +0200

    Practice - Array - Ex-01

---
+++ week-02/Practice/Array/Ex_01/Ex_01/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex_01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] myArray = new int[10];
+
+            for (int i = 0; i < myArray.Length; i++)
+            {
+                myArray[i] = i + 1;
+            }
+
+            int count = 0;
+
+            foreach (int element in myArray)
+            {
+                count += 1;
+                Console.WriteLine("Element #{0}: {1}", count, element);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
