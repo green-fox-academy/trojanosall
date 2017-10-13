77c934af6082227ad6153da7d018fa8b9858abec
Lorant Lajos <trojanosall@gmail.com>
2017-10-13 22:32:26 +0200

    Basic Exam - TakeMeToThe - One Sulution but it does not work based on the description ü with some little modification

--- week-06/Basic Exam Again/TakeMeToThe/TakeMeToThe/CandyShop.cs
+++ week-06/Basic Exam Again/TakeMeToThe/TakeMeToThe/CandyShop.cs
@@ -1,10 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace CandyShopApp
+namespace TakeMeToThe
 {
     public class CandyShop
     {
