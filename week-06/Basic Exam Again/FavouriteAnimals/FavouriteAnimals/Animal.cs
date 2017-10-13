7cd9d51eab0738f4e37949a8c2ff134c1638255d
Lorant Lajos <trojanosall@gmail.com>
2017-10-13 19:19:27 +0200

    Basic Exam - Favourite Animals

---
+++ week-06/Basic Exam Again/FavouriteAnimals/FavouriteAnimals/Animal.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace FavouriteAnimals
+{
+    class Animal
+    {
+        string fileName = @"./favourites.txt";
+        List<string> animals;
+
+        public void PrintUsage()
+        {
+            Console.WriteLine("\n\n```C# FavouriteAnimals [animal] [animal]```\n\nYou should add your favourite animals as string after./ FavouriteAnimals.exe[animal][animal]");
+        }
+
+        public void AddAnimal(string[] inputAnimals)
+        {
+            animals = File.ReadAllLines(fileName).ToList<string>();
+            foreach (var newAnimal in inputAnimals)
+            {
+                if (animals.Contains(newAnimal))
+                {
+                    Console.WriteLine($"Sorry, but the file has already contained {newAnimal} animal.");
+                }
+                else
+                {
+                    using (StreamWriter writer = File.AppendText(fileName))
+                    {
+                        writer.WriteLine(newAnimal);
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ week-06/Basic Exam Again/FavouriteAnimals/FavouriteAnimals/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace FavouriteAnimals
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Animal myAnimal = new Animal();
+
+            if (args.Length == 0)
+            {
+                myAnimal.PrintUsage();
+            }
+            else
+            {
+                myAnimal.AddAnimal(args);
+            }
+        }
+    }
+}
