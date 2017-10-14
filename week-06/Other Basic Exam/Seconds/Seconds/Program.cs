db56db81400b2efcb0dd347ba23338d2c98deb29
Lorant Lajos <trojanosall@gmail.com>
2017-10-14 23:14:24 +0200

    Other Basic Exam - Seconds - Done

---
+++ week-06/Other Basic Exam/Seconds/Seconds/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Seconds
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Create a function that takes a list as a parameter,
+            // and returns a new list with every second element from the orignal list
+            // example: [1, 2, 3, 4, 5] should produce [2, 4] - print this result
+
+            List<int> myList = new List<int>(new int []{ 1, 2, 3, 4, 5 });
+
+            Seconds(myList);
+
+            for (int i = 0; i < Seconds(myList).Count; i++)
+            {
+                Console.Write("[" + Seconds(myList)[i] + "]");
+            }
+
+            Console.ReadKey();
+
+        }
+
+        public static List<int> Seconds(List<int> myList)
+        {
+            List<int> secondsList = new List<int>();
+
+            for (int i = 0; i < myList.Count; i++)
+            {
+                if (i % 2 != 0)
+                {
+                    secondsList.Add(myList[i]);
+                }
+            }
+            return secondsList;
+        }
+    }
+}
