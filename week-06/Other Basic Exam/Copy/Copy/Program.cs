db20af8d372e78c3c14fe00fac32c60fec59a168
Lorant Lajos <trojanosall@gmail.com>
2017-10-14 16:01:57 +0200

    Other Basic Exam - Copy - Copy is copying

---
+++ week-06/Other Basic Exam/Copy/Copy/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Copy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // This should be the basic replica of the 'cp' command
+            // If ran from the command line without arguments
+            // It should print out the usage:
+            // copy [source] [destination]
+            // When just one argument is provided print out
+            // No destination provided
+            // When both arguments provided and the source is a file
+            // Read all contents from it and write it to the destination
+
+            //sourcefile.txt destinationfile.txt
+
+            if (args.Length == 0)
+            {
+                Console.WriteLine("\n\n```COPY```\n\nPlease follow the below instructions and I will copy the contain from the source to the destination\n\ncopy[source][destination]");
+            }
+            else if (args.Length == 1)
+            {
+                Console.WriteLine("No destination provided");
+            }
+            else
+            {
+                try
+                {
+                    string[] content = File.ReadAllLines(args[0]);
+                    File.WriteAllLines(args[1], content);
+                    Console.WriteLine("Copy was successful");
+                }
+                catch (Exception myException)
+                {
+                    Console.WriteLine("I'm sorry, I am unable to read the file");
+                }
+            }
+        }
+    }
+}
