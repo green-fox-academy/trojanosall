9d2152e7c6f2555895b578546bea2f8ff7561ab5
Lorant Lajos <trojanosall@gmail.com>
2017-10-18 16:32:31 +0200

    Week 07 - Day 03 - Exercise 08 - LINQ - UpperCase - Query syntax done

---
+++ week-07/Day 03/Ex-08/Ex_08_LINQ/Ex_08_LINQ/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex_08_LINQ
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Write a LINQ Expression to find the uppercase characters in a string!
+
+            string something = "Knezić Károly, Nagysándor József, Damjanich János, Aulich Lajos, Lahner György, Poeltenberg Ernő, Leiningen-Westerburg Károly, Török Ignác, Vécsey Károly, Kiss Ernő, Schweidel József, Dessewffy Arisztid, Lázár Vilmos";
+
+            //Query Syntax
+            var charQuery = from character in something
+                where char.IsUpper(character)
+                select character;
+
+            Console.WriteLine("Upper case characters with query syntax are the followings: ");
+            foreach (var character in charQuery)
+            {
+                Console.Write(character + " ");
+            }
+
+            Console.ReadKey();
+
+        }
+    }
+}
