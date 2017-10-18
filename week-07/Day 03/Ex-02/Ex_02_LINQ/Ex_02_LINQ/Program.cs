3334c1ebb04fb05a317c500ba671e3126477c0d8
Lorant Lajos <trojanosall@gmail.com>
2017-10-18 11:52:21 +0200

    Week 07 - Day 03 - Exercise 03 - LINQ - Oddd average - Query syntax done

---
+++ week-07/Day 03/Ex-02/Ex_02_LINQ/Ex_02_LINQ/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex_02_LINQ
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Write a LINQ Expression to get the average value of the odd numbers from the following array:
+            //int[] n = { 1, 3, -2, -4, -7, -3, -8, 12, 19, 6, 9, 10, 14 };
+
+            double[] numbers = { 1, 3, -2, -4, -7, -3, -8, 12, 19, 6, 9, 10, 14 };
+
+            //Query Syntax
+            var numberSyntax = from num in numbers
+                where num % 2 != 0
+                select num;
+
+            var oddAverageSyntax = numberSyntax.Average();
+
+            Console.WriteLine(oddAverageSyntax);
+
+            Console.ReadKey();
+
+        }
+    }
+}
