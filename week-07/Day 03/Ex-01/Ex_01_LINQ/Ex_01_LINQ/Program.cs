703fb6fb86603889627cb5ca11fb03ec031b7437
Lorant Lajos <trojanosall@gmail.com>
2017-10-18 11:26:00 +0200

    Week 07 - Day 03 - Exercise 03 - LINQ - Even number - Method syntax also done

--- week-07/Day 03/Ex-01/Ex_01_LINQ/Ex_01_LINQ/Program.cs
+++ week-07/Day 03/Ex-01/Ex_01_LINQ/Ex_01_LINQ/Program.cs
@@ -16,15 +16,25 @@ namespace Ex_01_LINQ
             int[] numbers = { 1, 3, -2, -4, -7, -3, -8, 12, 19, 6, 9, 10, 14 };
 
             // Query Syntax
-            var number = from num in numbers
+            var numberSyntax = from num in numbers
                 where num % 2 == 0
                 orderby num
                 select num;
 
             Console.WriteLine("Output of Query Syntax");
-            foreach (var n in number)
+            foreach (var n in numberSyntax)
             {
-                Console.WriteLine(n);
+                Console.Write(n + " ");
+            }
+
+
+            // Method Syntax
+            var numberMethod = numbers.Where(x => (x % 2 == 0)).OrderBy(x => x);
+
+            Console.WriteLine("\n\nOutput of Method Syntax");
+            foreach (var n in numberMethod)
+            {
+                Console.Write(n + " ");
             }
 
             Console.ReadKey();
