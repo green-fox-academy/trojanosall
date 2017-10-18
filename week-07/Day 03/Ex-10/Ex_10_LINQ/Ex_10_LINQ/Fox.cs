60cabba57a2064b56b38fd1c186cf7bd0f2e80dc
Lorant Lajos <trojanosall@gmail.com>
2017-10-18 17:11:16 +0200

    Week 07 - Day 03 - Exercise 10 - LINQ - Green fox - Generate class

---
+++ week-07/Day 03/Ex-10/Ex_10_LINQ/Ex_10_LINQ/Fox.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex_10_LINQ
+{
+    class Fox
+    {
+        public string Name;
+        public string Type;
+        public string Color;
+
+        public Fox(string name, string type, string color)
+        {
+            this.Name = name;
+            this.Type = type;
+            this.Color = color;
+        }
+    }
+}
---
+++ week-07/Day 03/Ex-10/Ex_10_LINQ/Ex_10_LINQ/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex_10_LINQ
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Create a Fox class with 3 properties(name, type, color) 
+            // Fill a list with at least 5 foxes, it's up to you how you name/create them! 
+            // Write a LINQ Expression to find the foxes with green color! 
+            // Write a LINQ Expression to find the foxes with green color and pallida type!
+        }
+    }
+}
