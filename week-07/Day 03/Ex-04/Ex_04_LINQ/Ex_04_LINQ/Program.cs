c2ff6dac44aae18339d59f2d88bb7a6e5bcefee8
Lorant Lajos <trojanosall@gmail.com>
2017-10-18 14:09:46 +0200

    Week 07 - Day 03 - Exercise 03 - LINQ - Square larger than 20 - Method syntax done

---
+++ week-07/Day 03/Ex-04/Ex_04_LINQ/Ex_04_LINQ/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex_04_LINQ
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Write a LINQ Expression to find which number squared value is more then 20 from the following array:
+            //int[] n = new[] { 3, 9, 2, 8, 6, 5 };
+
+            int[] numbers = new[] { 3, 9, 2, 8, 6, 5 };
+
+            //Query Syntax
+            var numbersSyntax = from num in numbers
+                where (num * num) > 20
+                orderby num
+                select num;
+
+            Console.WriteLine("Output of Query Syntax");
+            foreach (var n in numbersSyntax)
+            {
+                Console.Write(n + " ");
+            }
+
+
+            // Method Syntax
+            var numbersMethod = numbers.Where(x => (x * x > 20)).OrderBy(x => x);
+
+            Console.WriteLine("\n\nOutput of Method Syntax");
+            foreach (var n in numbersMethod)
+            {
+                Console.Write(n + " ");
+            }
+
+
+            Console.ReadKey();
+        }
+    }
+}
