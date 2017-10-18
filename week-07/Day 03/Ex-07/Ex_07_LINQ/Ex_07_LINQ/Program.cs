56a7edc65458bd67fe5a6caae2c9c7050a7281c5
Lorant Lajos <trojanosall@gmail.com>
2017-10-18 15:59:57 +0200

    Week 07 - Day 03 - Exercise 07 - LINQ - Cities  - Query syntax done

---
+++ week-07/Day 03/Ex-07/Ex_07_LINQ/Ex_07_LINQ/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex_07_LINQ
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Write a LINQ Expression to find the strings which starts with A and ends with I in the following array:
+            //string[] cities = { "ROME", "LONDON", "NAIROBI", "CALIFORNIA", "ZURICH", "NEW DELHI", "AMSTERDAM", "ABU DHABI", "PARIS" };
+
+            string[] cities = { "ROME", "LONDON", "NAIROBI", "CALIFORNIA", "ZURICH", "NEW DELHI", "AMSTERDAM", "ABU DHABI", "PARIS" };
+            string startingCharacter = "A";
+            string endingCharacter = "I";
+
+            //Query Syntax
+            var cityQuery = from city in cities
+                where city.StartsWith(startingCharacter)
+                where city.EndsWith(endingCharacter)
+                select city;
+
+            foreach (var city in cityQuery)
+            {
+                Console.WriteLine($"Output of Query Syntax: {city}");
+            }
+            
+
+            Console.ReadKey();
+
+        }
+    }
+}
