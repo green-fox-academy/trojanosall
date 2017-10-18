e7af50da3d5bd407db9efe47e5d85639d8e7f0e7
Lorant Lajos <trojanosall@gmail.com>
2017-10-18 14:20:20 +0200

    Week 07 - Day 03 - Exercise 05 - LINQ - Frequency - Query syntax done

---
+++ week-07/Day 03/Ex-05/Ex_05_LINQ/Ex_05_LINQ/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex_05_LINQ
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Write a LINQ Expression to find the frequency of numbers in the following array:
+            //int[] n = new int[] { 5, 9, 1, 2, 3, 7, 5, 6, 7, 3, 7, 6, 8, 5, 4, 9, 6, 2 };
+
+            int[] numbers = new int[] { 5, 9, 1, 2, 3, 7, 5, 6, 7, 3, 7, 6, 8, 5, 4, 9, 6, 2 };
+
+            //Query Syntax
+            var numberQuery = from num in numbers
+                group num by num
+                into newNum
+                select newNum;
+
+            Console.WriteLine("Output of Query Syntax");
+            foreach (var  numDictionary in numberQuery)
+            {
+                Console.WriteLine("Number " + numDictionary.Key + " appears " + numDictionary.Count() + " times");
+            }
+
+            Console.ReadKey();
+
+        }
+    }
+}
