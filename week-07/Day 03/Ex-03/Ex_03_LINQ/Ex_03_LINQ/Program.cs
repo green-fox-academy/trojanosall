7ab8472aea5c9f5c9eeb457760b1785d3b859a20
Lorant Lajos <trojanosall@gmail.com>
2017-10-18 13:22:33 +0200

    Week 07 - Day 03 - Exercise 03 - LINQ - Square - Query yntax done

---
+++ week-07/Day 03/Ex-03/Ex_03_LINQ/Ex_03_LINQ/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex_03_LINQ
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Write a LINQ Expression to get the squared value of the positive numbers from the following array:
+            //int[] n = { 1, 3, -2, -4, -7, -3, -8, 12, 19, 6, 9, 10, 14 };
+
+            int[] numbers = { 1, 3, -2, -4, -7, -3, -8, 12, 19, 6, 9, 10, 14 };
+
+            //Query Syntax
+            var numbersSyntax = from num in numbers
+                where num > 0
+                orderby num
+                select (num * num);
+
+            Console.WriteLine("Output of Query Syntax");
+            foreach (var n in numbersSyntax)
+            {
+                Console.Write(n + " ");
+            }
+
+            Console.ReadKey();
+
+
+
+
+        }
+    }
+}
