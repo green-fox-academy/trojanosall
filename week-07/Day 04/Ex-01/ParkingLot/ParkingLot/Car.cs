2adcb022751a52951d8a7f26f86ed224a660b183
Lorant Lajos <trojanosall@gmail.com>
2017-10-19 11:20:59 +0200

    Week 07 - Day 04 - Exercise 01 - ParkingLot - Car class created

---
+++ week-07/Day 04/Ex-01/ParkingLot/ParkingLot/Car.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParkingLot
+{
+    class Car
+    {
+        public string Types;
+        public string Color;
+        Random rnd = new Random();
+
+        public Car(string types, string color)
+        {
+            Types = rnd.Next(0, Enum.GetNames(typeof(Types)).Length + 1).ToString();
+            Color = rnd.Next(0, Enum.GetNames(typeof(Color)).Length + 1).ToString();
+        }
+    }
+
+    public enum Types
+    {
+        Audi,
+        Porsche,
+        BMW,
+        Lexus,
+        Subaru,
+        Kia,
+        Mazda,
+        Tesla,
+        Honda,
+        Buick,
+        Toyota,
+        Hyundai,
+        Acura,
+        Volvo,
+        Lincoln,
+        Infiniti,
+        Chevrolet,
+        Cadilac,
+        Crysler,
+        MercedesBenz,
+        Suzuki
+    }
+
+    public enum Color
+    {
+        Black,
+        White,
+        Blue,
+        Red,
+        Green,
+        Brown,
+        Yellow,
+        Pink,
+        Grey,
+        Purple
+    }
+}
---
+++ week-07/Day 04/Ex-01/ParkingLot/ParkingLot/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParkingLot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Create a Car class with 2 enum properties
+            //    enum of car types
+            //    The types of these cars or vehicles is up to you
+            //    enum of colors
+            // Create 256 Vehicles randomly and put them into a List
+            // Count and Print the number of same vehicles for each type
+            // Count and Print the number of same vehicles for each color
+            // Print the most frequently occurring vehicle
+
+        }
+    }
+}
