e4a3c93ab26be45f1f08b58b25595dddb61e18c1
Lorant Lajos <trojanosall@gmail.com>
2017-10-19 10:58:38 +0200

    Week 07 - Day 04 - TestExample - Enum

---
+++ week-07/Day 04/TestExampleEnum/TestExampleEnum/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestExampleEnum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Gender.Male);
+            Console.WriteLine((int)Gender.Male);
+
+            Console.ReadLine();
+        }
+
+        public enum Gender
+        {
+            Male,
+            Female,
+            Alien
+        }
+    }
+}
