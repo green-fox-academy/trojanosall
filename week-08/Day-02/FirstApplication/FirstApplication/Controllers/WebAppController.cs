cac747232567006e64559da40e580a3784554a43
Lorant Lajos <trojanosall@gmail.com>
2017-10-25 00:20:59 +0200

    Week 08 - Day 02 - First Web Application - say hello in different language and in different color and in different style done

--- week-08/Day-02/FirstApplication/FirstApplication/Controllers/WebAppController.cs
+++ week-08/Day-02/FirstApplication/FirstApplication/Controllers/WebAppController.cs
@@ -9,29 +9,13 @@ using FirstApplication.Models;
 
 namespace FirstApplication.Controllers
 {
-    [Route("webapp")]
+    [Route("web")]
     public class WebAppController : Controller
     {
-        string[] GreetingArray = {"Mirëdita", "Ahalan", "Parev", "Zdravei", "Nei Ho", "Dobrý den", "Ahoj", "Goddag", "Goede dag, Hallo",
-            "Hello", "Saluton", "Hei", "Bonjour",
-            "Guten Tag", "Gia'sou", "Aloha", "Shalom", "Namaste", "Namaste", "Jó napot", "Halló", "Helló",
-            "Góðan daginn", "Halo", "Aksunai", "Qanuipit", "Dia dhuit",
-            "Salve", "Ciao", "Kon-nichiwa", "An-nyong Ha-se-yo", "Salvëte", "Ni hao", "Dzien' dobry", "Olá",
-            "Bunã ziua", "Zdravstvuyte", "Hola", "Jambo", "Hujambo", "Hej",
-            "Sa-wat-dee", "Merhaba", "Selam", "Vitayu", "Xin chào", "Hylo", "Sut Mae", "Sholem Aleychem", "Sawubona"};
-
-        [Route("greeting")]
-        public IActionResult Greeting()
+        [Route("greetingall")]
+        public IActionResult GreetingAll()
         {
-            var myGreeting = new GreetingsWebApp()
-            {
-            for (int i = 0; i < ; i++)
-            {
-                Console.WriteLine(myGreeting.Content[i]);
-            }
-            };
-            
-        return new JsonResult(myGreeting);
+           return View(Greeting.hellos);
 
         }
     }
