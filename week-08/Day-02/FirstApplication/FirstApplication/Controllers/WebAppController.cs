78f8aeb0191dfbe97d303c9906248ab4e1fb395b
Lorant Lajos <trojanosall@gmail.com>
2017-10-24 17:43:43 +0200

    Week 08 - Day 02 - First Web Application - Web App array trying to find new way... but soooo far the end...

--- week-08/Day-02/FirstApplication/FirstApplication/Controllers/WebAppController.cs
+++ week-08/Day-02/FirstApplication/FirstApplication/Controllers/WebAppController.cs
@@ -12,18 +12,26 @@ namespace FirstApplication.Controllers
     [Route("webapp")]
     public class WebAppController : Controller
     {
+        string[] GreetingArray = {"Mirëdita", "Ahalan", "Parev", "Zdravei", "Nei Ho", "Dobrý den", "Ahoj", "Goddag", "Goede dag, Hallo",
+            "Hello", "Saluton", "Hei", "Bonjour",
+            "Guten Tag", "Gia'sou", "Aloha", "Shalom", "Namaste", "Namaste", "Jó napot", "Halló", "Helló",
+            "Góðan daginn", "Halo", "Aksunai", "Qanuipit", "Dia dhuit",
+            "Salve", "Ciao", "Kon-nichiwa", "An-nyong Ha-se-yo", "Salvëte", "Ni hao", "Dzien' dobry", "Olá",
+            "Bunã ziua", "Zdravstvuyte", "Hola", "Jambo", "Hujambo", "Hej",
+            "Sa-wat-dee", "Merhaba", "Selam", "Vitayu", "Xin chào", "Hylo", "Sut Mae", "Sholem Aleychem", "Sawubona"};
+
         [Route("greeting")]
         public IActionResult Greeting()
         {
-            var myGreeting = new GreetingsWebApp();
-
-
-            for (int i = 0; i < myGreeting.Content.Length; i++)
+            var myGreeting = new GreetingsWebApp()
+            {
+            for (int i = 0; i < ; i++)
             {
                 Console.WriteLine(myGreeting.Content[i]);
             }
-
-            return new JsonResult(myGreeting);
+            };
+            
+        return new JsonResult(myGreeting);
 
         }
     }
--- week-08/Day-02/FirstApplication/FirstApplication/Models/GreetingsWebApp.cs
+++ week-08/Day-02/FirstApplication/FirstApplication/Models/GreetingsWebApp.cs
@@ -7,16 +7,8 @@ namespace FirstApplication.Models
 {
     public class GreetingsWebApp
     {
-        public string[] Content = new[]
-        {
-            "Mirëdita", "Ahalan", "Parev", "Zdravei", "Nei Ho", "Dobrý den", "Ahoj", "Goddag", "Goede dag, Hallo",
-            "Hello", "Saluton", "Hei", "Bonjour",
-            "Guten Tag", "Gia'sou", "Aloha", "Shalom", "Namaste", "Namaste", "Jó napot", "Halló", "Helló",
-            "Góðan daginn", "Halo", "Aksunai", "Qanuipit", "Dia dhuit",
-            "Salve", "Ciao", "Kon-nichiwa", "An-nyong Ha-se-yo", "Salvëte", "Ni hao", "Dzien' dobry", "Olá",
-            "Bunã ziua", "Zdravstvuyte", "Hola", "Jambo", "Hujambo", "Hej",
-            "Sa-wat-dee", "Merhaba", "Selam", "Vitayu", "Xin chào", "Hylo", "Sut Mae", "Sholem Aleychem", "Sawubona"
-        };
 
+        public string Content { get; set; }
+        
     }
 }
