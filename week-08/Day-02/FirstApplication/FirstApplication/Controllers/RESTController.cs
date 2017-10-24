4f1adeb5dede26a3ec23c433c92b2e633bb6de14
Lorant Lajos <trojanosall@gmail.com>
2017-10-24 14:17:55 +0200

    Week 08 - Day 02 - First Web Application - Hello World REST done

--- week-08/Day-02/FirstApplication/FirstApplication/Controllers/RESTController.cs
+++ week-08/Day-02/FirstApplication/FirstApplication/Controllers/RESTController.cs
@@ -3,17 +3,26 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using FirstApplication.Models;
 
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
 namespace FirstApplication.Controllers
 {
+    [Route("api")]
     public class RESTController : Controller
     {
-        // GET: /<controller>/
-        public IActionResult Index()
+
+        [Route("greeting")]
+        public IActionResult Greeting(string name)
         {
-            return View();
+            Greeting myGreeting = new Greeting()
+            {
+                Id = 1,
+                Content = "Hello World!"
+            };
+
+            return new JsonResult(myGreeting);
         }
     }
 }
--- week-08/Day-02/FirstApplication/FirstApplication/Models/Greeting.cs
+++ week-08/Day-02/FirstApplication/FirstApplication/Models/Greeting.cs
@@ -7,5 +7,7 @@ namespace FirstApplication.Models
 {
     public class Greeting
     {
+        public long Id { get; set; }
+        public string Content { get; set; }
     }
 }
