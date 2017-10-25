27b767fd857765b6c3276b39db5709b3351f2ac9
Lorant Lajos <trojanosall@gmail.com>
2017-10-25 13:14:56 +0200

    Week 08 - Day 03 - Bank of Simba -  show the fields of the BankAccount - Part I - done

--- week-08/Day-03/bankofsimba/bankofsimba/Controllers/HomeController.cs
+++ week-08/Day-03/bankofsimba/bankofsimba/Controllers/HomeController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using bankofsimba.Models;
 using Microsoft.AspNetCore.Mvc;
 
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
@@ -10,10 +11,16 @@ namespace bankofsimba.Controllers
 {
     public class HomeController : Controller
     {
-        [Route("")]
+        [Route("Simba")]
         public IActionResult Index()
         {
-            return Content("Simba");
+            var BankAccount = new BankAccount()
+            {
+                Name = "Simba",
+                Balance = 2000,
+                AnimalType = "Lion"
+            };
+            return View(BankAccount);
         }
     }
 }
