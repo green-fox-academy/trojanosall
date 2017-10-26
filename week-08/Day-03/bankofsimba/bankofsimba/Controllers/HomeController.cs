e92a8b385599f775ae660d8503298f6d4b34aab1
Lorant Lajos <trojanosall@gmail.com>
2017-10-27 00:51:54 +0200

    Week 08 - Day 03 - Razor Pages - Bank of Simba - Refactored again HomeController.cs file

--- week-08/Day-03/bankofsimba/bankofsimba/Controllers/HomeController.cs
+++ week-08/Day-03/bankofsimba/bankofsimba/Controllers/HomeController.cs
@@ -26,19 +26,14 @@ namespace bankofsimba.Controllers
         [Route("Animals")]
         public IActionResult Animals()
         {
-            List<BankAccount> animalsList = new List<BankAccount>();
-
-            //var animal1 = new BankAccount("Mufasza", 4000, "Lion", true, true);
-            //var animal2 = new BankAccount("Rafiki", 500, "Monkey", true, true);
-            //var animal3 = new BankAccount("Zazu", 750, "Parrot", true, true);
-            //var animal4 = new BankAccount("Pumbaa", 10000, "Wildpig", true, true);
-            //var animal5 = new BankAccount("Timon", 20, "Rat", true, true);
-
-            animalsList.Add(new BankAccount("Mufasza", 4000, "Lion", true, true));
-            animalsList.Add(new BankAccount("Rafiki", 500, "Monkey", true, true));
-            animalsList.Add(new BankAccount("Zazu", 750, "Parrot", true, true));
-            animalsList.Add(new BankAccount("Pumbaa", 10000, "Wildpig", true, true));
-            animalsList.Add(new BankAccount("Timon", 20, "Rat", true, true));
+            List<BankAccount> animalsList = new List<BankAccount>
+            {
+                new BankAccount("Mufasza", 4000, "Lion", true, true),
+                new BankAccount("Rafiki", 500, "Monkey", true, true),
+                new BankAccount("Zazu", 750, "Parrot", true, true),
+                new BankAccount("Pumbaa", 10000, "Wildpig", true, true),
+                new BankAccount("Timon", 20, "Rat", true, true)
+            };
 
             return View(animalsList);
         }
