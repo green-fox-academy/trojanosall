bdc3c67adb25beca6cdcf42bbfeeb4dcd4d66d5a
Lorant Lajos <trojanosall@gmail.com>
2017-10-26 13:24:06 +0200

    Week 08 - Day 04 - Dependency Injection - Exercise 01 - Counter - Counter class created (within Models folder)

---
+++ week-08/Day-04/Dependency Injection/Ex-01/Counter/Counter/Models/Counter.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Counter.Models
+{
+    public class Counter
+    {
+        public int Number { get; set; } = 1;
+
+        public void Increase()
+        {
+            Number++;
+        }
+    }
+}
