992f29b93fa8847552e7ce22f3ae08fd2ddcacd4
Lorant Lajos <trojanosall@gmail.com>
2017-10-31 13:19:34 +0100

    Week 09 - Day 02 - Database Integration -  List of ToDo Apps - Todo class added and set

---
+++ week-09/day-02/Ex-01/ListingTodosApp/ListingTodosApp/Models/Todo.cs
@@ -0,0 +1,10 @@
+﻿namespace ListingTodosApp.Models
+{
+    public class Todo
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public bool IsUrgent { get; set; } = false;
+        public bool IsDone { get; set; } = false;
+    }
+}
