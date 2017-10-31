70cdc8a29d31711945a6eb92102c2a0252cf5758
Lorant Lajos <trojanosall@gmail.com>
2017-10-31 14:21:41 +0100

    Week 09 - Day 02 - Database Integration -  List of ToDo Apps - the repository injected into the controller

--- week-09/day-02/Ex-01/ListingTodosApp/ListingTodosApp/Controllers/TodoController.cs
+++ week-09/day-02/Ex-01/ListingTodosApp/ListingTodosApp/Controllers/TodoController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using ListingTodosApp.Repositories;
+using Microsoft.AspNetCore.Mvc;
 
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -6,6 +7,13 @@ namespace ListingTodosApp.Controllers
 {
     public class TodoController : Controller
     {
+        TodoRepository TodoRepository;
+
+        public TodoController(TodoRepository todoRepository)
+        {
+            TodoRepository = todoRepository;
+        }
+
         [Route("")]
         [Route("/list")]
         public IActionResult List()
