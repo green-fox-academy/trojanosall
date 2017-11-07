7d07e522db84c78301e085551fbd8b773f4094a6
Lorant Lajos <trojanosall@gmail.com>
2017-11-07 17:30:43 +0100

    Week 10 - Day 02 - Rest Backend - Until - Two respond from four is correct

---
+++ week-10/Ex_05_Do_Until/FrontendEx/FrontendEx/Controllers/HomeController.cs
@@ -0,0 +1,99 @@
+﻿using FrontendEx.Models;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace FrontendEx.Controllers
+{
+    public class HomeController : Controller
+    {
+        [Route("")]
+        public IActionResult Index()
+        {
+            return File("index.html", "text/html");
+        }
+
+        [HttpGet]
+        [Route("/doubling")]
+        public IActionResult Doubling(int? input)
+        {
+            if (input == null)
+            {
+                return Json(new { error = "Please provide an input!" });
+            }
+            return Json(new { received = input, result = input * 2 });
+        }
+
+        [HttpGet]
+        [Route("/greeter")]
+        public IActionResult Greeter(string name, string title)
+        {
+            if (name == null)
+            {
+                return Json(new { error = "Please provide a name!" });
+            }
+            else if (title == null)
+            {
+                return Json(new { error = "Please provide a title!" });
+            }
+            else
+            {
+                return Json(new { welcome_message = $"Oh, hi there {name}, my dear {title}!" });
+            }
+        }
+
+        [HttpGet]
+        [Route("/appenda/{appendable}")]
+        public IActionResult AppendA(string appendable)
+        {
+            return Json(new { appended = $"{appendable}a" });
+        }
+
+        [HttpGet]
+        [Route("/appenda/")]
+        public IActionResult Append()
+        {
+            return NotFound();
+        }
+
+        [HttpPost]
+        [Route("/dountil/{what}")]
+        public IActionResult DoUntil([FromBody] DoUntilClass myDoUntilClass, string what)
+        {
+            if (what == "factor")
+            {
+                return Json(new { result = myDoUntilClass.Factor() });
+            }
+
+            if (what == "sum")
+            {
+                return Json(new { result = myDoUntilClass.Sum() });
+            }
+
+            if (myDoUntilClass == null)
+            {
+                return Json(new { error = "Please provide a number!" });
+            }
+
+            if (myDoUntilClass.Until == null)
+            {
+                return Json(new { error = "Please provide a number!" });
+            }
+
+            if (string.IsNullOrEmpty(what))
+            {
+                return Json(new { error = "Please provide a number!" });
+            }
+
+            if (what == null)
+            {
+                return Json(new { error = "Please provide a number!" });
+            }
+
+            else
+            {
+                return Json(new { error = "Please provide a number!" });
+            }
+        }
+    }
+}
---
+++ week-10/Ex_05_Do_Until/FrontendEx/FrontendEx/Models/DoUntilClass.cs
@@ -0,0 +1,31 @@
+﻿namespace FrontendEx.Models
+{
+    public class DoUntilClass
+    {
+        public int? Until { get; set; }
+
+        public DoUntilClass()
+        {
+        }
+
+        public int Sum()
+        {
+            int SumValue = 0;
+            for (int i = 0; i <= Until; i++)
+            {
+                SumValue += i;
+            }
+            return SumValue;
+        }
+
+        public int Factor()
+        {
+            int FactValue = 1;
+            for (int i = 1; i <= Until; i++)
+            {
+                FactValue *= i;
+            }
+            return FactValue;
+        }
+    }
+}
