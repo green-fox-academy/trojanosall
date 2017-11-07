572cf2803f7611c027435c7998b1c6e294543524
Lorant Lajos <trojanosall@gmail.com>
2017-11-07 13:49:53 +0100

    Week 10 - Day 02 - Rest Backend - Doubling - endpoint is created, two respond are created

---
+++ week-10/Ex_02_Doubling/FrontendEx/FrontendEx/Controllers/HomeController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace FrontendEx.Controllers
+{
+    public class HomeController : Controller
+    {
+        [Route("")]
+        public IActionResult Index()
+        {
+            return File("index.html", "text/html");
+        }
+
+        [HttpGet]
+        [Route("/doubling")]
+        public IActionResult Index(int? input)
+        {
+            if (input == null)
+            {
+                return Json(new { error = "Please provide an input!" });
+            }
+            return Json(new { received = input, result = input * 2 });
+        }
+    }
+}
