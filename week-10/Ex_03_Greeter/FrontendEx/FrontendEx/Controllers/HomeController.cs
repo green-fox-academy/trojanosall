f69bc075b99083c990f063aead4d64eb400dc5a1
Lorant Lajos <trojanosall@gmail.com>
2017-11-07 14:19:41 +0100

    Week 10 - Day 02 - Rest Backend - Greetet - endpoint is created, three respond are created

---
+++ week-10/Ex_03_Greeter/FrontendEx/FrontendEx/Controllers/HomeController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace FrontendEx.Controllers
+{
+    public class HomeController : Controller
+    {
+        [Route("")]
+        public IActionResult Index()
+        {
+            return File("index.html", "text/html");
+        }
+
+        [HttpGet]
+        [Route("/doubling")]
+        public IActionResult Index(int? input)
+        {
+            if (input == null)
+            {
+                return Json(new { error = "Please provide an input!" });
+            }
+            return Json(new { received = input, result = input * 2 });
+        }
+
+        [HttpGet]
+        [Route("/greeter")]
+        public IActionResult Index(string name, string title)
+        {
+            if (name == null)
+            {
+                return Json(new { error = "Please provide a name!" });
+            }
+            else if (title == null)
+            {
+                return Json(new { error = "Please provide a title!" });
+            }
+            else
+            {
+                return Json(new { welcome_message = $"Oh, hi there {name}, my dear {title}!" });
+            }
+        }
+    }
+}
