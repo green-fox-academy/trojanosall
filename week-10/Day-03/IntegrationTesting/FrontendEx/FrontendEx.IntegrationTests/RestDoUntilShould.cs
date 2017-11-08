80eb3e4330e092190d65fa9a6d9b10dbbc4b373f
Lorant Lajos <trojanosall@gmail.com>
2017-11-08 14:26:22 +0100

    Week 10 - Day 03 - Integration Testing - ReturnOkStatus for dountil is tested based on Suvis solution

---
+++ week-10/Day-03/IntegrationTesting/FrontendEx/FrontendEx.IntegrationTests/RestDoUntilShould.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.TestHost;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace FrontendEx.IntegrationTests
+{
+    public class RestDoUntilShould
+    {
+        private readonly TestServer Server;
+        private readonly HttpClient Client;
+
+        public RestDoUntilShould()
+        {
+            Server = new TestServer(new WebHostBuilder().UseStartup<Startup>());
+            Client = Server.CreateClient();
+        }
+
+        [Fact]
+        public async Task ReturnOkStatus()
+        {
+            var response = await Client.GetAsync("/dountil");
+
+            Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+        }
+    }
+}
