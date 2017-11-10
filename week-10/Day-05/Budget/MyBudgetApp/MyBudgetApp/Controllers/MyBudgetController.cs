34921595743a69f92a731c7e6d36e41533b42135
Lorant Lajos <trojanosall@gmail.com>
2017-11-10 10:38:20 +0100

    Week 10 - Day 05 - MyBudgetApp for partner demo - within  MyBudgetController class constructor is created

--- week-10/Day-05/Budget/MyBudgetApp/MyBudgetApp/Controllers/MyBudgetController.cs
+++ week-10/Day-05/Budget/MyBudgetApp/MyBudgetApp/Controllers/MyBudgetController.cs
@@ -1,8 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
+using MyBudgetApp.Repositories;
 
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -10,7 +7,13 @@ namespace MyBudgetApp.Controllers
 {
     public class MyBudgetController : Controller
     {
-        // GET: /<controller>/
+        MyBudgetRepository MyBudgetRepository;
+
+        public MyBudgetController(MyBudgetRepository myBudgetRepository)
+        {
+            MyBudgetRepository = myBudgetRepository;
+        }
+
         public IActionResult Index()
         {
             return View();
