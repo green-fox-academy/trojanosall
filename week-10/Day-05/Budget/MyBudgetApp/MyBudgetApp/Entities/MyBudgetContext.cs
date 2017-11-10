bfcf12d00d50a767db841f4f37920a4647227133
Lorant Lajos <trojanosall@gmail.com>
2017-11-10 09:42:42 +0100

    Week 10 - Day 05 - MyBudgetApp for partner demo - MyBudgetContext class is added

---
+++ week-10/Day-05/Budget/MyBudgetApp/MyBudgetApp/Entities/MyBudgetContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MyBudgetApp.Models;
+
+namespace MyBudgetApp.Entities
+{
+    public class MyBudgetContext : DbContext
+    {
+        public MyBudgetContext(DbContextOptions<MyBudgetContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Expense> Expenses { get; set; }
+    }
+}
