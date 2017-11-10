4d02a2b8d508b9cbce689df9ab570f8131567be1
Lorant Lajos <trojanosall@gmail.com>
2017-11-10 10:16:02 +0100

    Week 10 - Day 05 - MyBudgetApp for partner demo - MyBudgetRepositor class is added and AddExpense and GetList methods are created

---
+++ week-10/Day-05/Budget/MyBudgetApp/MyBudgetApp/Repositories/MyBudgetRepository.cs
@@ -0,0 +1,36 @@
+﻿using MyBudgetApp.Entities;
+using MyBudgetApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace MyBudgetApp.Repositories
+{
+    public class MyBudgetRepository
+    {
+        MyBudgetContext MyBudgetContext;
+
+        public MyBudgetRepository(MyBudgetContext myBudgetContext)
+        {
+            MyBudgetContext = myBudgetContext;
+        }
+
+        public List<Expense> GetList()
+        {
+            return MyBudgetContext.Expenses.ToList();
+        }
+
+        public void AddExpense(string title, DateTime date, ExpenseCategoryEnum category)
+        {
+            var expense = new Expense()
+            {
+                Title = title,
+                Date = date,
+                Category = category,
+            };
+
+            MyBudgetContext.Expenses.Add(expense);
+            MyBudgetContext.SaveChanges();
+        }
+    }
+}
