9919959786adacc9582786ec9c95163699fb526e
Lorant Lajos <trojanosall@gmail.com>
2017-11-10 11:09:18 +0100

    Week 10 - Day 05 - MyBudgetApp for partner demo - within  MyBudgetController class basic, List, Add, deletre, update, edit endpoints are added

--- week-10/Day-05/Budget/MyBudgetApp/MyBudgetApp/Controllers/MyBudgetController.cs
+++ week-10/Day-05/Budget/MyBudgetApp/MyBudgetApp/Controllers/MyBudgetController.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using MyBudgetApp.Models;
 using MyBudgetApp.Repositories;
+using System;
 
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -14,9 +16,50 @@ namespace MyBudgetApp.Controllers
             MyBudgetRepository = myBudgetRepository;
         }
 
-        public IActionResult Index()
+        [Route("")]
+        [HttpGet]
+        public IActionResult List()
+        {
+            return View(MyBudgetRepository.GetList());
+        }
+
+        [Route("/add")]
+        [HttpGet]
+        public IActionResult Add()
         {
             return View();
+        }
+
+        [Route("/add")]
+        [HttpPost]
+        public IActionResult Add(string title, DateTime date, ExpenseCategoryEnum category)
+        {
+            MyBudgetRepository.AddExpense(title, date, category);
+            return RedirectToAction("List");
+        }
+
+        [Route("/{id}/delete")]
+        [HttpPost]
+        public IActionResult Delete(int id)
+        {
+            MyBudgetRepository.Delete(id);
+            return RedirectToAction("List");
+        }
+
+        [Route("/{id}/update")]
+        [HttpPost]
+        public IActionResult Update(int id)
+        {
+            var expense = MyBudgetRepository.Updating(id);
+            return View(expense);
+        }
+
+        [Route("/{id}/edit")]
+        [HttpPost]
+        public IActionResult Edit(Expense expense)
+        {
+            MyBudgetRepository.UpdateExpense(expense);
+            return RedirectToAction("List");
         }
     }
 }
