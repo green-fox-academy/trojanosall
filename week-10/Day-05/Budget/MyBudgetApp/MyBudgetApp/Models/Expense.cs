2744505a4e0f8f91041fe11c5e1fc697a6f47ff0
Lorant Lajos <trojanosall@gmail.com>
2017-11-10 09:27:38 +0100

    Week 10 - Day 05 - MyBudgetApp for partner demo - MyBudgetController class is added

--- week-10/Day-05/Budget/MyBudgetApp/MyBudgetApp/Models/Expense.cs
+++ week-10/Day-05/Budget/MyBudgetApp/MyBudgetApp/Models/Expense.cs
@@ -10,82 +10,82 @@ namespace MyBudgetApp.Models
         public ExpenseCategoryEnum Category { get; set; }
 
 
-        public string GetExpenseCategory(ExpenseCategoryEnum Category)
-        {
-            switch (Category)
-            {
-                case ExpenseCategoryEnum.InternetAndTV:
-                    return "Internet and TV";
+        //public string GetExpenseCategory(ExpenseCategoryEnum Category)
+        //{
+        //    switch (Category)
+        //    {
+        //        case ExpenseCategoryEnum.InternetAndTV:
+        //            return "Internet and TV";
 
-                case ExpenseCategoryEnum.GasUtilityService:
-                    return "Gas utility service";
+        //        case ExpenseCategoryEnum.GasUtilityService:
+        //            return "Gas utility service";
 
-                case ExpenseCategoryEnum.WaterUtilityService:
-                    return "Water utility service";
+        //        case ExpenseCategoryEnum.WaterUtilityService:
+        //            return "Water utility service";
 
-                case ExpenseCategoryEnum.WasteUtilityService:
-                    return "Waste utility service";
+        //        case ExpenseCategoryEnum.WasteUtilityService:
+        //            return "Waste utility service";
 
-                case ExpenseCategoryEnum.ElectricityUtilityService:
-                    return "Electricity utility service";
+        //        case ExpenseCategoryEnum.ElectricityUtilityService:
+        //            return "Electricity utility service";
 
-                case ExpenseCategoryEnum.LunchTinus:
-                    return "Lunch for Tinus";
+        //        case ExpenseCategoryEnum.LunchTinus:
+        //            return "Lunch for Tinus";
 
-                case ExpenseCategoryEnum.LunchLoci:
-                    return "Lunch for Loci";
+        //        case ExpenseCategoryEnum.LunchLoci:
+        //            return "Lunch for Loci";
 
-                case ExpenseCategoryEnum.MobileServiceFee:
-                    return "Mobile service fee";
+        //        case ExpenseCategoryEnum.MobileServiceFee:
+        //            return "Mobile service fee";
 
-                case ExpenseCategoryEnum.Cigarette:
-                    return "Cigarette";
+        //        case ExpenseCategoryEnum.Cigarette:
+        //            return "Cigarette";
 
-                case ExpenseCategoryEnum.Shopping:
-                    return "Shopping";
+        //        case ExpenseCategoryEnum.Shopping:
+        //            return "Shopping";
 
-                case ExpenseCategoryEnum.Fuel:
-                    return "Fuel";
+        //        case ExpenseCategoryEnum.Fuel:
+        //            return "Fuel";
 
-                case ExpenseCategoryEnum.PublicTransportFee:
-                    return "Public transport fee";
+        //        case ExpenseCategoryEnum.PublicTransportFee:
+        //            return "Public transport fee";
 
-                case ExpenseCategoryEnum.Clothes:
-                    return "Clothes";
+        //        case ExpenseCategoryEnum.Clothes:
+        //            return "Clothes";
 
-                case ExpenseCategoryEnum.Recreation:
-                    return "Recreation";
+        //        case ExpenseCategoryEnum.Recreation:
+        //            return "Recreation";
 
-                case ExpenseCategoryEnum.Car:
-                    return "Car";
+        //        case ExpenseCategoryEnum.Car:
+        //            return "Car";
 
-                case ExpenseCategoryEnum.BankLoan:
-                    return "Bank loan";
+        //        case ExpenseCategoryEnum.BankLoan:
+        //            return "Bank loan";
 
-                case ExpenseCategoryEnum.Savings:
-                    return "Savings";
+        //        case ExpenseCategoryEnum.Savings:
+        //            return "Savings";
 
-                case ExpenseCategoryEnum.BankCharges:
-                    return "Bank charges";
+        //        case ExpenseCategoryEnum.BankCharges:
+        //            return "Bank charges";
 
-                case ExpenseCategoryEnum.HealthCare:
-                    return "Health care";
+        //        case ExpenseCategoryEnum.HealthCare:
+        //            return "Health care";
 
-                case ExpenseCategoryEnum.Gifts:
-                    return "Gifts";
+        //        case ExpenseCategoryEnum.Gifts:
+        //            return "Gifts";
 
-                case ExpenseCategoryEnum.Childs:
-                    return "Childs";
+        //        case ExpenseCategoryEnum.Childs:
+        //            return "Childs";
 
-                case ExpenseCategoryEnum.Learning:
-                    return "Learning";
+        //        case ExpenseCategoryEnum.Learning:
+        //            return "Learning";
 
-                case ExpenseCategoryEnum.Other:
-                    return "Other";
+        //        case ExpenseCategoryEnum.Other:
+        //            return "Other";
 
-                case ExpenseCategoryEnum.Travel:
-                    return "Travel";
-            }
-        }
+        //        case ExpenseCategoryEnum.Travel:
+        //            return "Travel";
+        //    }
+        //}
     }
 }
