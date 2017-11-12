2e4e276eabae87be1f9db7307b5766420e4a7bf8
Lorant Lajos <trojanosall@gmail.com>
2017-11-12 17:51:06 +0100

    Week 08 - Day 03 - AnagrammWebApp - Constructor is added to HomeController class

--- week-08/Day-04/Dependency Injection/Ex-03/AnagrammWebApp/AnagrammWebApp/Controllers/HomeController.cs
+++ week-08/Day-04/Dependency Injection/Ex-03/AnagrammWebApp/AnagrammWebApp/Controllers/HomeController.cs
@@ -1,7 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using AnagrammWebApp.Models;
 using Microsoft.AspNetCore.Mvc;
 
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
@@ -10,6 +7,13 @@ namespace AnagrammWebApp.Controllers
 {
     public class HomeController : Controller
     {
+        AnagramChecker MyAnagramchecker;
+
+        public HomeController(AnagramChecker myAnagramchecker)
+        {
+            MyAnagramchecker = myAnagramchecker;
+        }
+
         // GET: /<controller>/
         public IActionResult Index()
         {
