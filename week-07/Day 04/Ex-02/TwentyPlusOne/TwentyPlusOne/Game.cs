96abfbe28b47aa412bc0329eac32c902d1513bcc
Lorant Lajos <trojanosall@gmail.com>
2017-11-12 12:23:43 +0100

    Week 07 - Day 04 - TwentyPlusOne - Game Class - Game class completed but it is not ready for using because I got null reference exception... I will have to fix it later...

--- week-07/Day 04/Ex-02/TwentyPlusOne/TwentyPlusOne/Game.cs
+++ week-07/Day 04/Ex-02/TwentyPlusOne/TwentyPlusOne/Game.cs
@@ -5,12 +5,18 @@ namespace TwentyPlusOne
 {
     public class Game
     {
-        public Deck GameDeck;
+        public List<Card> GameDeck;
+        public Deck MyDeck;
         public List<Card> PlayerCards;
-        public int OponentScore;
+        public int OponentScore { get; set; }
         public int BlackJackValue = 21;
         public int MinOponenPoints = 15;
         static readonly Random RandomNumber = new Random();
+
+        public Game()
+        {
+            GameDeck = GetGameDeck();
+        }
 
         public int GetOponentScore()
         {
@@ -19,19 +25,38 @@ namespace TwentyPlusOne
             return OponentScore;
         }
 
-        //public List<Card> GetActualGameDeck()
-        //{
-        //    List<Card> actualGameDeck = GameDeck.ShuffleDeck(GameDeck.GenerateMyDeck());
-        //    return actualGameDeck;
-        //}
+        public List<Card> GetGameDeck()
+        {
+            List<Card> GameDeck = MyDeck.ShuffleDeck(MyDeck.GenerateMyDeck());
+
+            return GameDeck;
+
+        }
+
+        public List<Card> GetNewCard(List<Card> playerCards, List<Card> gameDeck)
+        {
+            playerCards.Add(MyDeck.PullRandom(gameDeck));
+
+            return playerCards;
+        }
+
+        public int GetTotalValueOfPlayerCards(List<Card> playerCards)
+        {
+            int TotalValueOfPlayerCards = 0;
+            foreach (var cards in playerCards)
+            {
+                TotalValueOfPlayerCards += cards.CardValue;
+            }
+            return TotalValueOfPlayerCards;
+        }
 
 
-        public List<Card> GetInitialPlayerCards(List<Card> actualGameDeck)
+        public List<Card> GetInitialPlayerCards(List<Card> GameDeck)
         {
             PlayerCards = new List<Card>();
-            Card FirstPlayerCard = GameDeck.PullFirst(actualGameDeck);
+            Card FirstPlayerCard = MyDeck.PullFirst(GameDeck);
             PlayerCards.Add(FirstPlayerCard);
-            Card LastPlayerCard = GameDeck.PullLast(actualGameDeck);
+            Card LastPlayerCard = MyDeck.PullLast(GameDeck);
             PlayerCards.Add(LastPlayerCard);
 
             return PlayerCards;
@@ -58,15 +83,72 @@ namespace TwentyPlusOne
         {
             Console.WriteLine($"Welcome to My Black Jack Game!");
 
-            List<Card> actualGameDeck = GameDeck.ShuffleDeck(GameDeck.GenerateMyDeck());
+            List<Card> actualGameDeck = GameDeck;
 
             List<Card> PlayerCards = GetInitialPlayerCards(actualGameDeck);
 
             PrintActualPlayerCardsAndPoints(PlayerCards);
 
-            Console.WriteLine($"\n The value of cards of Bank is the following: {GetOponentScore()} ");
-
         }
 
+        public void PlayGame()
+        {
+            OponentScore = GetOponentScore();
+            Console.WriteLine($"\n The value of cards of Bank is the following: {OponentScore}");
+
+            Console.WriteLine("Do you want to hit or stand? Please type H or S!");
+
+            string input = Console.ReadLine().ToLower();
+
+            if (input == "s")
+            {
+                GameOver();
+            }
+            else if (input == "h")
+            {
+                GetNewCard(PlayerCards, GameDeck);
+                PrintActualPlayerCardsAndPoints(PlayerCards);
+                LoseOrContinue();
+            }
+            else
+                Console.WriteLine("You typed a wrong character, please type H or S!");
+            PlayGame();
+        }
+
+        public void LoseOrContinue()
+        {
+            if (GetTotalValueOfPlayerCards(PlayerCards) <= 21)
+            {
+                PlayGame();
+            }
+            else if (GetTotalValueOfPlayerCards(PlayerCards) > 21)
+            {
+                GameOver();
+            }
+        }
+
+        private void GameOver()
+        {
+            int TotalValueOfPlayerCards = 0;
+            foreach (var cards in PlayerCards)
+            {
+                TotalValueOfPlayerCards += cards.CardValue;
+            }
+
+            if ((TotalValueOfPlayerCards <= 21) && (TotalValueOfPlayerCards > OponentScore))
+            {
+                Console.WriteLine("You won!!!");
+            }
+            else if ((TotalValueOfPlayerCards < OponentScore) || (TotalValueOfPlayerCards > 21))
+            {
+                Console.WriteLine("You lost!!!");
+            }
+            else if (TotalValueOfPlayerCards == OponentScore)
+            {
+                Console.WriteLine("It's a score draw!");
+            }
+        }
     }
+
 }
+
