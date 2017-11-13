01a55ddf0b753ee0ca7edea446fc3eabfff30905
Lorant Lajos <trojanosall@gmail.com>
2017-11-13 13:52:07 +0100

    Week 09 - Day 05 - Reddit Web App - RedditContext class is added

--- week-09/day-05/Reddit/RedditWebApp/RedditWebApp/Controllers/RedditController.cs
+++ week-09/day-05/Reddit/RedditWebApp/RedditWebApp/Controllers/RedditController.cs
@@ -1,8 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
+using RedditWebApp.Models;
 
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -10,7 +7,13 @@ namespace RedditWebApp.Controllers
 {
     public class RedditController : Controller
     {
-        // GET: /<controller>/
+        Reddit myReddit;
+
+        public RedditController(Reddit myReddit)
+        {
+            this.myReddit = myReddit;
+        }
+
         public IActionResult Index()
         {
             return View();
---
+++ week-09/day-05/Reddit/RedditWebApp/RedditWebApp/Entities/RedditContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RedditWebApp.Models;
+
+namespace RedditWebApp.Entities
+{
+    public class RedditContext : DbContext
+    {
+        public RedditContext(DbContextOptions<RedditContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Reddit> Reddits { get; set; }
+    }
+}
