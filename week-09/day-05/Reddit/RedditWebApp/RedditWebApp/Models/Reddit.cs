65bf569e18abf68994722a7c9338b65968cd685e
Lorant Lajos <trojanosall@gmail.com>
2017-11-13 15:45:05 +0100

    Week 09 - Day 05 - Reddit Web App - Post endpont is visible + data is loaded by manual + the content of post endpoints is under creation, but up and down voting is not created and add button currently does not have funcionality

--- week-09/day-05/Reddit/RedditWebApp/RedditWebApp/Models/Reddit.cs
+++ week-09/day-05/Reddit/RedditWebApp/RedditWebApp/Models/Reddit.cs
@@ -5,5 +5,16 @@
         public int Id { get; set; }
         public string Content { get; set; }
         public int Score { get; set; }
+
+        //public Reddit()
+        //{
+        //}
+
+        //public Reddit(int id, string content, int score)
+        //{
+        //    Id = id;
+        //    Content = content;
+        //    Score = score;
+        //}
     }
 }
