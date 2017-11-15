d9a3e0afda6ba3f93622980577b4e5c5bc9d0466
Lorant Lajos <trojanosall@gmail.com>
2017-11-15 12:36:20 +0100

    Week 11 - Day 03 - Quiz class under construction

--- week-11/Last Year Exam/GreenFoxQuizAPI/GreenFoxQuizAPI/Entities/QuizContext.cs
+++ week-11/Last Year Exam/GreenFoxQuizAPI/GreenFoxQuizAPI/Entities/QuizContext.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using GreenFoxQuizAPI.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace GreenFoxQuizAPI.Entities
 {
--- week-11/Last Year Exam/GreenFoxQuizAPI/GreenFoxQuizAPI/Models/Quiz.cs
+++ week-11/Last Year Exam/GreenFoxQuizAPI/GreenFoxQuizAPI/Models/Quiz.cs
@@ -1,11 +1,24 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace GreenFoxQuizAPI.Models
 {
     public class Quiz
     {
+        public int Id { get; set; }
+        public string Question { get; set; }
+        public string Answer { get; set; }
+        public Dictionary<string, string> QuestionAndAnswer { get; set; } = new Dictionary<string, string>()
+        {
+            {"When did your course start?","2017.03.13"},
+            {"What type of dog Barbi has?","Whippet"},
+            {"What is HerrNorbert's favourite color?","Green"},
+            {"How many classes are learning at Green Fox Academy at this moment?","4"},
+            {"How many mentors teach at Green Fox at this moment?","16"},
+            {"What was the name of the first Green Fox class?","Vulpes"},
+            {"How many likes do we have on facebook?","~3300"},
+            {"What is Tojas's horoscope?","~Libra"},
+        };
+        static readonly Random RandomNumber = new Random();
     }
 }
