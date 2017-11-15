8840129a6ed9f8be4199986b85ec0e594e22c49d
Lorant Lajos <trojanosall@gmail.com>
2017-11-15 14:37:36 +0100

    Week 11 - Day 03 - question endpoint with json is ready but the json currently contain just the question BUT the id is missing

--- week-11/Last Year Exam/GreenFoxQuizAPI/GreenFoxQuizAPI/Repositories/QuizRepository.cs
+++ week-11/Last Year Exam/GreenFoxQuizAPI/GreenFoxQuizAPI/Repositories/QuizRepository.cs
@@ -17,7 +17,38 @@ namespace GreenFoxQuizAPI.Repositories
             this.quizContext = quizContex;
         }
 
-        public List<Quiz> GetRandomQuestions()
+        //public List<Quiz> GetRandomQuestions()
+        //{
+        //    List<Quiz> listOfQuestions = (from quizes in quizContext.Quizes
+        //                                  orderby quizes.Id ascending
+        //                                  select quizes).ToList();
+
+        //    var randomQuiz1 = listOfQuestions[RandomNumber.Next(listOfQuestions.Count)];
+        //    listOfQuestions.Remove(randomQuiz1);
+
+        //    var randomQuiz2 = listOfQuestions[RandomNumber.Next(listOfQuestions.Count)];
+        //    listOfQuestions.Remove(randomQuiz2);
+
+        //    var randomQuiz3 = listOfQuestions[RandomNumber.Next(listOfQuestions.Count)];
+        //    listOfQuestions.Remove(randomQuiz3);
+
+        //    var randomQuiz4 = listOfQuestions[RandomNumber.Next(listOfQuestions.Count)];
+        //    listOfQuestions.Remove(randomQuiz4);
+
+        //    var randomQuiz5 = listOfQuestions[RandomNumber.Next(listOfQuestions.Count)];
+
+        //    var listOfQuiz = new List<Quiz>();
+
+        //    listOfQuiz.Add(randomQuiz1);
+        //    listOfQuiz.Add(randomQuiz2);
+        //    listOfQuiz.Add(randomQuiz3);
+        //    listOfQuiz.Add(randomQuiz4);
+        //    listOfQuiz.Add(randomQuiz5);
+
+        //    return listOfQuiz;
+        //}
+
+        public List<string> GetRandomQuestions()
         {
             List<Quiz> listOfQuestions = (from quizes in quizContext.Quizes
                                           orderby quizes.Id ascending
@@ -37,15 +68,14 @@ namespace GreenFoxQuizAPI.Repositories
 
             var randomQuiz5 = listOfQuestions[RandomNumber.Next(listOfQuestions.Count)];
 
-            var listOfQuiz = new List<Quiz>();
+            List<string> RandomQuestions = new List<string>();
+            RandomQuestions.Add(randomQuiz1.Question);
+            RandomQuestions.Add(randomQuiz2.Question);
+            RandomQuestions.Add(randomQuiz3.Question);
+            RandomQuestions.Add(randomQuiz4.Question);
+            RandomQuestions.Add(randomQuiz5.Question);
 
-            listOfQuiz.Add(randomQuiz1);
-            listOfQuiz.Add(randomQuiz2);
-            listOfQuiz.Add(randomQuiz3);
-            listOfQuiz.Add(randomQuiz4);
-            listOfQuiz.Add(randomQuiz5);
-
-            return listOfQuiz;
+            return RandomQuestions;
         }
     }
 }
