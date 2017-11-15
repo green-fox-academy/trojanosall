30512a9cd9908a1fe1d2030eec4e5b59eb8bc2d9
Lorant Lajos <trojanosall@gmail.com>
2017-11-15 14:24:33 +0100

    Week 11 - Day 03 - Get 5 random question is ready and the multiplication of the questions is solved

--- week-11/Last Year Exam/GreenFoxQuizAPI/GreenFoxQuizAPI/Repositories/QuizRepository.cs
+++ week-11/Last Year Exam/GreenFoxQuizAPI/GreenFoxQuizAPI/Repositories/QuizRepository.cs
@@ -19,15 +19,24 @@ namespace GreenFoxQuizAPI.Repositories
 
         public List<Quiz> GetRandomQuestions()
         {
-            var listOfQuestions = (from quizes in quizContext.Quizes
-                                   orderby quizes.Id ascending
-                                   select quizes).ToList();
+            List<Quiz> listOfQuestions = (from quizes in quizContext.Quizes
+                                          orderby quizes.Id ascending
+                                          select quizes).ToList();
 
             var randomQuiz1 = listOfQuestions[RandomNumber.Next(listOfQuestions.Count)];
+            listOfQuestions.Remove(randomQuiz1);
+
             var randomQuiz2 = listOfQuestions[RandomNumber.Next(listOfQuestions.Count)];
+            listOfQuestions.Remove(randomQuiz2);
+
             var randomQuiz3 = listOfQuestions[RandomNumber.Next(listOfQuestions.Count)];
+            listOfQuestions.Remove(randomQuiz3);
+
             var randomQuiz4 = listOfQuestions[RandomNumber.Next(listOfQuestions.Count)];
+            listOfQuestions.Remove(randomQuiz4);
+
             var randomQuiz5 = listOfQuestions[RandomNumber.Next(listOfQuestions.Count)];
+
             var listOfQuiz = new List<Quiz>();
 
             listOfQuiz.Add(randomQuiz1);
