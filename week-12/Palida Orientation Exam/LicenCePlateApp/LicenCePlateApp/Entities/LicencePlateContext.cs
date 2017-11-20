99c37a722093b84c93628293a37c12e0e933e837
Lorant Lajos <trojanosall@gmail.com>
2017-11-20 14:24:50 +0100

    Licence Plate App - Entities folder is created + LicencePlateContext.cs is added with constructor and Dbset

---
+++ week-12/Palida Orientation Exam/LicenCePlateApp/LicenCePlateApp/Entities/LicencePlateContext.cs
@@ -0,0 +1,14 @@
+﻿using LicenCePlateApp.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace LicenCePlateApp.Entities
+{
+    public class LicencePlateContext : DbContext
+    {
+        public LicencePlateContext(DbContextOptions<LicencePlateContext> options) : base(options)
+        {
+        }
+
+        public DbSet<LicencePlate> LicencePlates { get; set; }
+    }
+}
