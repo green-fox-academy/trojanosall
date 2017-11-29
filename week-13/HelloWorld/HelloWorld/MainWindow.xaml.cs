1fc87278c260e0f6fac9fa5e2273d0e72640845c
Lorant Lajos <trojanosall@gmail.com>
2017-11-29 15:00:45 +0100

    WPF - Tutorial - Routed Events practice

--- week-13/HelloWorld/HelloWorld/MainWindow.xaml.cs
+++ week-13/HelloWorld/HelloWorld/MainWindow.xaml.cs
@@ -12,5 +12,19 @@ namespace HelloWorld
             InitializeComponent();
 
         }
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            txt1.Text = "Button is Clicked";
+        }
+
+        private void StackPanel_Click(object sender, RoutedEventArgs e)
+        {
+            txt2.Text = "Click event is bubbled to Stack Panel";
+        }
+
+        private void Window_Click(object sender, RoutedEventArgs e)
+        {
+            txt3.Text = "Click event is bubbled to Window";
+        }
     }
 }
