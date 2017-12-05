277dc15c23f4a982f8c5ccbeb28cb684ee4cfacf
Lorant Lajos <trojanosall@gmail.com>
2017-12-05 11:28:47 +0100

    MVVM – Practice - ViewModel is created

---
+++ week-14/WPF_MVVM_Implementation/WPF_MVVM_Implementation/ViewModels/PersonViewModel.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections.ObjectModel;
+using System.ComponentModel;
+using WPF_MVVM_Implementation.Models;
+
+namespace WPF_MVVM_Implementation.ViewModels
+{
+    public class PersonViewModel : INotifyPropertyChanged
+    {
+        private Person _person;
+
+        public Person Person
+        {
+            get { return _person; }
+            set { _person = value; NotifyPropertyChanged("Person"); }
+        }
+
+        private ObservableCollection<Person> _persons;
+
+        public ObservableCollection<Person> Persons
+        {
+            get
+            {
+                return _persons;
+            }
+            set
+            {
+                _persons = value;
+                NotifyPropertyChanged("Persons");
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void NotifyPropertyChanged(string propertyName)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+    }
+}
