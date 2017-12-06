d72567355211cd64209f5657cb5460175ca189b4
Lorant Lajos <trojanosall@gmail.com>
2017-12-06 15:26:46 +0100

    WPF Walkthrought - Home.xaml.cs - adding code to handle event

--- week-13/Windows Presentation Foundation tutorial/ExpenseIt/ExpenseIt/ExpenseItHome.xaml.cs
+++ week-13/Windows Presentation Foundation tutorial/ExpenseIt/ExpenseIt/ExpenseItHome.xaml.cs
@@ -1,17 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
+﻿using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace ExpenseIt
 {
@@ -23,6 +11,12 @@ namespace ExpenseIt
         public ExpenseItHome()
         {
             InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            ExpenseReportPage expenseReportPage = new ExpenseReportPage();
+            this.NavigationService.Navigate(expenseReportPage);
         }
     }
 }
