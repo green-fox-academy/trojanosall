4a5c7f4d55886eb69040a9badb7f92c449a9e2a1
Lorant Lajos <trojanosall@gmail.com>
2018-01-15 10:14:16 +0100

    Final mini Execrcise and other liitle modiofications in last weeks work

---
+++ Final Exam/FinalExamNumber5/FinalExamNumber5/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace FinalExamNumber5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string inputWord = "duck";
+            Console.WriteLine("How many letters are in the word duck: \n");
+            Console.WriteLine(HowManyLetters(inputWord));
+            Console.ReadKey();
+        }
+
+        public static int HowManyLetters(string input)
+        {
+            int numberOfLetters = 0;
+            foreach (char letter in input)
+            {
+                numberOfLetters++;
+            }
+            return numberOfLetters;
+        }
+    }
+}
---
+++ Final Exam/Other/FinalExamNumber5/FinalExamNumber5/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+namespace FinalExamNumber5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] p1 = { 1, 2, 3 };
+            int[] p2 = { 2, 3, 6, 9 };
+
+            Console.WriteLine("Are the arrays the same? ");
+            Console.WriteLine(AreTheySame(p1, p2));
+
+            CompareTheArrays(p1, p2);
+
+            Console.ReadKey();
+        }
+
+        private static bool AreTheySame(int[] p1, int[] p2)
+        {
+            if (p1.Length != p2.Length)
+            {
+                return false;
+            }
+            for (int i = 0; i < p1.Length; i++)
+            {
+                if (p1[i] != p2[i])
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        private static void CompareTheArrays(int[] p1, int[] p2)
+        {
+            bool equal = false;
+
+            for (int i = 0; i < p1.Length; i++)
+            {
+                for (int j = 0; j < p2.Length; j++)
+                {
+                    if (p1[i] == p2[j])
+                    {
+                        equal = true;
+                        Console.WriteLine("\nFirst array element " + p1[i] + " was found in Second array");
+                    }
+                }
+                if (equal == false)
+                {
+                    Console.WriteLine("\nFirst array element " + p1[i] + " was NOT found in Second array");
+                }
+                equal = false;
+            };
+        }
+    }
+}
