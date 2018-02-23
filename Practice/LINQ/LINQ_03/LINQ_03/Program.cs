d5c1854c4ea5c3ec4f0c4801453ae3f427af024e
Lorant Lajos <trojanosall@gmail.com>
2018-02-23 15:46:19 +0100

    Linq practice 03

---
+++ Practice/LINQ/LINQ_03/LINQ_03/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Linq;
+
+namespace LINQ_03
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var arr1 = new[] { 3, 9, 2, 8, 6, 5 };
+
+            SquareMakerByQuerySyn(arr1);
+
+            Console.WriteLine("\n");
+
+            SquareMakerByMethodSyn(arr1);
+
+            Console.ReadKey();
+
+        }
+
+        private static void SquareMakerByQuerySyn(int[] arr1)
+        {
+            var querySyn = from int Number in arr1
+                           let SqrNum = Number * Number
+                           where SqrNum > 20
+                           select new { Number, SqrNum };
+
+            foreach (var item in querySyn)
+            {
+                Console.WriteLine(item);
+            }
+        }
+
+        private static void SquareMakerByMethodSyn(int[] arr1)
+        {
+            var methodSyn = arr1.Select(Number => new { Number = Number, SqrNum = Number * Number }).Where(x => x.SqrNum > 20).Select(z => z);
+
+            foreach (var item in methodSyn)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
