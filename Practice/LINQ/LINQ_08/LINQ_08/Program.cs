26bfc0901608bd55123734698bab8d56e0a32b03
Lorant Lajos <trojanosall@gmail.com>
2018-03-01 16:40:16 +0100

    Linq practice 08

---
+++ Practice/LINQ/LINQ_08/LINQ_08/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Linq;
+
+namespace LINQ_08
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] cities =
+            {
+                "ROME","LONDON","NAIROBI","CALIFORNIA","ZURICH","NEW DELHI","AMSTERDAM","ABU DHABI", "PARIS"
+            };
+
+            CitiSelectorByQuerySyn(cities);
+
+            Console.WriteLine("\n");
+
+            CitiSelectorByMethSyn(cities);
+
+            Console.ReadKey();
+        }
+
+        private static void CitiSelectorByQuerySyn(string[] cities)
+        {
+            var querySyn = from city in cities
+                           where city.StartsWith("A")
+                           where city.EndsWith("M")
+                           select city;
+
+            foreach (var item in querySyn)
+            {
+                Console.WriteLine(item);
+            }
+        }
+
+        private static void CitiSelectorByMethSyn(string[] cities)
+        {
+            var methodSyn = cities.Where(x => x.StartsWith("A")).Where(x => x.EndsWith("M")).Select(z => z);
+
+            foreach (var item in methodSyn)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
