b6f7b08607d8fd2ece9ac81270f83c2ac55b7c03
Lorant Lajos <trojanosall@gmail.com>
2018-03-01 15:02:07 +0100

    Linq practice 06

---
+++ Practice/LINQ/LINQ_06/LINQ_06/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Linq;
+
+namespace LINQ_06
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] dayWeek = { "Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday" };
+
+            DayWeekPrinterByQuerySyn(dayWeek);
+
+            Console.WriteLine("\n");
+
+            DayWeekPrinterByMethSyn(dayWeek);
+
+            Console.ReadKey();
+        }
+
+        private static void DayWeekPrinterByQuerySyn(string[] dayWeek)
+        {
+            var querySyn = from day in dayWeek
+                           select day;
+
+            foreach (var day in querySyn)
+            {
+                Console.WriteLine(day);
+            }
+        }
+
+        private static void DayWeekPrinterByMethSyn(string[] dayWeek)
+        {
+            var methodSyn = dayWeek.Select(x => x);
+
+            foreach (var day in methodSyn)
+            {
+                Console.WriteLine(day);
+            }
+        }
+
+    }
+}
