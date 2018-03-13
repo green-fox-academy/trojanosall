6781f33f5dc8e025d9f52b35125a2775aac6e96b
Lorant Lajos <trojanosall@gmail.com>
2018-03-13 11:54:24 +0100

    Linq practice 09

---
+++ Practice/LINQ/LINQ_09/LINQ_09/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LINQ_09
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> tempList = new List<int>()
+            {55,
+            200,
+            740,
+            76,
+            230,
+            482,
+            95};
+
+            GreaterThanEigthyQuerySyn(tempList);
+
+            Console.WriteLine("\n");
+
+            GreaterThanEigthyMethSyn(tempList);
+
+            Console.ReadKey();
+
+        }
+
+        private static void GreaterThanEigthyQuerySyn(List<int> tempList)
+        {
+            var querySyn = from numbers in tempList
+                           where numbers > 80
+                           select numbers;
+
+            foreach (var item in querySyn)
+            {
+                Console.WriteLine(item);
+            }
+        }
+
+        private static void GreaterThanEigthyMethSyn(List<int> tempList)
+        {
+            var methodSyn = tempList.Where(x => x > 80).ToList();
+
+            foreach (var item in methodSyn)
+            {
+                Console.WriteLine(item);
+            }
+
+            Console.WriteLine("\n");
+
+            var otherMethodSyn = tempList.FindAll(x => x > 80);
+
+            foreach (var item in otherMethodSyn)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
