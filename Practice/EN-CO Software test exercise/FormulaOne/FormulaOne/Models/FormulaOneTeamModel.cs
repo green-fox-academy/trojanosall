1440d97dee2625ead4de7e88b5d5dad05ce0c228
Lorant Lajos <trojanosall@gmail.com>
2018-04-11 13:03:57 +0200

    Properties of FormulaOneTeammodel class are created

---
+++ Practice/EN-CO Software test exercise/FormulaOne/FormulaOne/Models/FormulaOneTeamModel.cs
@@ -0,0 +1,40 @@
+﻿namespace FormulaOne.Models
+{
+    public class FormulaOneTeamModel
+    {
+        private int id;
+        public int Id
+        {
+            get { return id; }
+            set { id = value; }
+        }
+
+        private string name;
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+        private int yearOfFundation;
+        public int YearOfFundation
+        {
+            get { return yearOfFundation; }
+            set { yearOfFundation = value; }
+        }
+
+        private int numberOfChampionshipTitles;
+        public int NumberOfChampionshipTitles
+        {
+            get { return numberOfChampionshipTitles; }
+            set { numberOfChampionshipTitles = value; }
+        }
+
+        private bool isEntryFeePaid;
+        public bool IsEntryFeePaid
+        {
+            get { return isEntryFeePaid; }
+            set { isEntryFeePaid = value; }
+        }
+    }
+}
