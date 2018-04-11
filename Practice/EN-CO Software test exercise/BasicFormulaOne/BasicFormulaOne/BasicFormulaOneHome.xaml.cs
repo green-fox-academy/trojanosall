ef76f89e2198c6f40ce681e7c75a37ff5dc92f23
Lorant Lajos <trojanosall@gmail.com>
2018-04-11 13:53:26 +0200

    BasicFormulaOne.xaml as new window is added to the Project

--- Practice/EN-CO Software test exercise/BasicFormulaOne/BasicFormulaOne/BasicFormulaOneHome.xaml.cs
+++ Practice/EN-CO Software test exercise/BasicFormulaOne/BasicFormulaOne/BasicFormulaOneHome.xaml.cs
@@ -10,6 +10,7 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 namespace BasicFormulaOne
@@ -17,7 +18,7 @@ namespace BasicFormulaOne
     /// <summary>
     /// Interaction logic for BasicFormulaOneHome.xaml
     /// </summary>
-    public partial class BasicFormulaOneHome : Window
+    public partial class BasicFormulaOneHome : Page
     {
         public BasicFormulaOneHome()
         {
